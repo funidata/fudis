54958fc8d65428bf12654f0f43c712e22021d645
joonashak <hakkijoo@gmail.com>
2022-08-10 18:08:42 +0300

    Install angular material

--- ngx-fudis/projects/dev/src/app/app.module.ts
+++ ngx-fudis/projects/dev/src/app/app.module.ts
@@ -1,16 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatButtonModule } from '@angular/material/button';
+import { NgxFudisModule } from '../../../ngx-fudis/src/public-api';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, NgxFudisModule, MatButtonModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
