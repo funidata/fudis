df7ead24f94d2e3c099ec79e16a41261146febaa
joonashak <hakkijoo@gmail.com>
2022-08-15 08:55:41 +0300

    Bump version

--- ngx-fudis/projects/dev/src/app/app.module.ts
+++ ngx-fudis/projects/dev/src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { MatButtonModule } from '@angular/material/button';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgxFudisModule } from '../../../ngx-fudis/src/public-api';
-
 import { AppComponent } from './app.component';
 import { DialogTestComponent } from './dialog-test/dialog-test.component';
 import { DialogTestContentComponent } from './dialog-test/dialog-test-content/dialog-test-content.component';
--- ngx-fudis/projects/dev/src/app/dialog-test/dialog-test-content/dialog-test-content.component.ts
+++ ngx-fudis/projects/dev/src/app/dialog-test/dialog-test-content/dialog-test-content.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-dialog-test-content',
   template: `
+  <h2 mat-dialog-title>Otsikko jee</h2>
     <p>
       dialog-test-content works!
     </p>
