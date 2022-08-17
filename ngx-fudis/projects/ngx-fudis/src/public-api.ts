e533ea5af6d7504e5a8e483dca8a01d513f44974
joonashak <hakkijoo@gmail.com>
2022-08-17 08:21:12 +0300

    Export dialog service

--- ngx-fudis/projects/ngx-fudis/src/public-api.ts
+++ ngx-fudis/projects/ngx-fudis/src/public-api.ts
@@ -2,6 +2,6 @@
  * Public API Surface of ngx-fudis
  */
 
-export * from './lib/ngx-fudis.service';
 export * from './lib/ngx-fudis.component';
 export * from './lib/ngx-fudis.module';
+export * from './lib/fudis-dialog/fudis-dialog.service';
