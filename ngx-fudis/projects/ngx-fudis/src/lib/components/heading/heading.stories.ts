0e6e7f8dc9d7c1cb482927ceb7b4abe352801045
videoeero <eero.suvanto@gmail.com>
2022-10-06 14:37:06 +0300

    Fix heading stories and public-api export

--- ngx-fudis/projects/ngx-fudis/src/lib/components/heading/heading.stories.ts
+++ ngx-fudis/projects/ngx-fudis/src/lib/components/heading/heading.stories.ts
@@ -1,11 +1,11 @@
 // also exported from '@storybook/angular' if you can deal with breaking changes in 6.1
 import { Story, Meta } from '@storybook/angular/types-6-0';
-import { Heading } from './heading.component';
+import { HeadingComponent } from './heading.component';
 
 // More on default export: https://storybook.js.org/docs/angular/writing-stories/introduction#default-export
 export default {
   title: 'Components/Heading',
-  component: Heading,
+  component: HeadingComponent,
   // More on argTypes: https://storybook.js.org/docs/angular/api/argtypes
   argTypes: {
     backgroundColor: { control: 'color' },
@@ -13,7 +13,7 @@ export default {
 } as Meta;
 
 // More on component templates: https://storybook.js.org/docs/angular/writing-stories/introduction#using-args
-const Template: Story<Heading> = (args: Heading) => ({
+const Template: Story<HeadingComponent> = (args: HeadingComponent) => ({
   props: args,
 });
 
