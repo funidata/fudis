3181c9dfa28faa81381da2aba3bb21fe08651bb8
riinakuukso <riina.kuukso@funidata.fi>
2022-11-10 11:26:27 +0200

    DS-54: Add button click unit tests

--- ngx-fudis/projects/ngx-fudis/src/lib/components/button/button.component.spec.ts
+++ ngx-fudis/projects/ngx-fudis/src/lib/components/button/button.component.spec.ts
@@ -2,41 +2,66 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ButtonComponent } from './button.component';
 
 describe('ButtonComponent', () => {
-	let component: ButtonComponent;
-	let fixture: ComponentFixture<ButtonComponent>;
+  let component: ButtonComponent;
+  let fixture: ComponentFixture<ButtonComponent>;
 
-	beforeEach(async () => {
-		await TestBed.configureTestingModule({
-			declarations: [ButtonComponent],
-		}).compileComponents();
-	});
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ButtonComponent],
+    }).compileComponents();
+  });
 
-	beforeEach(() => {
-		fixture = TestBed.createComponent(ButtonComponent);
-		component = fixture.componentInstance;
-		fixture.detectChanges();
-	});
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ButtonComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
 
-	function getButton(): HTMLElement {
-		return fixture.nativeElement.querySelector('button') as HTMLElement;
-	}
+  function getButton(): HTMLElement {
+    return fixture.nativeElement.querySelector('button') as HTMLElement;
+  }
 
-	function assertButtonHasClasses(...classes: string[]): void {
-		const buttonClasses = getButton()?.className ?? '';
-		expect(buttonClasses.split(' ').sort()).toEqual([...classes].sort());
-	}
+  function assertButtonHasClasses(...classes: string[]): void {
+    const buttonClasses = getButton()?.className ?? '';
+    expect(buttonClasses.split(' ').sort()).toEqual([...classes].sort());
+  }
 
-	describe('button CSS classes', () => {
-		it('should contain classes for medium sized primary button by default', () => {
-			assertButtonHasClasses('fudis-button', 'fudis-button--primary', 'fudis-button--medium');
-		});
+  describe('button CSS classes', () => {
+    it('should contain classes for medium sized primary button by default', () => {
+      assertButtonHasClasses('fudis-button', 'fudis-button--primary', 'fudis-button--medium');
+    });
 
-		it('should map the given inputs to the corresponding CSS classes', () => {
-			component.size = 'small';
-			component.variant = 'secondary';
-			fixture.detectChanges();
+    it('should map the given inputs to the corresponding CSS classes', () => {
+      component.size = 'small';
+      component.variant = 'secondary';
+      fixture.detectChanges();
 
-			assertButtonHasClasses('fudis-button', 'fudis-button--small', 'fudis-button--secondary');
-		});
-	});
+      assertButtonHasClasses('fudis-button', 'fudis-button--small', 'fudis-button--secondary');
+    });
+  });
+
+  describe('button clicked', () => {
+    it('should emit events when the button is enabled', () => {
+      let clicked = false;
+      component.handleClick.subscribe(() => {
+        clicked = true;
+      });
+
+      getButton()?.click();
+      expect(clicked).withContext('No click event received').toEqual(true);
+    });
+
+    it('should not emit events when the button is disabled', () => {
+      let clicked = false;
+      component.handleClick.subscribe(() => {
+        clicked = true;
+      });
+
+      component.disabled = true;
+      fixture.detectChanges();
+
+      getButton()?.click();
+      expect(clicked).withContext('Unexpected click event received').toEqual(false);
+    });
+  });
 });
