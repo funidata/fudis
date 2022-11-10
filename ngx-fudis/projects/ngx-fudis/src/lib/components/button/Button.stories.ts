13658cbdf104f22a9638beece2a02a84444d4c6b
videoeero <eero.suvanto@gmail.com>
2022-11-10 18:07:13 +0200

    Add mixins and fix linting

--- ngx-fudis/projects/ngx-fudis/src/lib/components/button/Button.stories.ts
+++ ngx-fudis/projects/ngx-fudis/src/lib/components/button/Button.stories.ts
@@ -4,7 +4,6 @@ import { moduleMetadata } from '@storybook/angular';
 import { MatButtonModule } from '@angular/material/button';
 import { ButtonComponent } from './button.component';
 
-// More on default export: https://storybook.js.org/docs/angular/writing-stories/introduction#default-export
 export default {
 	title: 'Components/Button',
 	component: ButtonComponent,
@@ -13,17 +12,14 @@ export default {
 			imports: [MatButtonModule],
 		}),
 	],
-	// More on argTypes: https://storybook.js.org/docs/angular/api/argtypes
 	argTypes: {},
 } as Meta;
 
-// More on component templates: https://storybook.js.org/docs/angular/writing-stories/introduction#using-args
 const Template: Story<ButtonComponent> = (args: ButtonComponent) => ({
 	props: args,
 });
 
 export const Primary = Template.bind({});
-// More on args: https://storybook.js.org/docs/angular/writing-stories/args
 Primary.args = {
 	variant: 'primary',
 	label: 'Button',
