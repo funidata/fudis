6ee44d1336ee57ce8724df5f1ce7e4384bed1457
videoeero <eero.suvanto@gmail.com>
2022-11-21 16:50:20 +0200

    DS-57: Add Fudis class to heading and button

--- ngx-fudis/projects/ngx-fudis/src/lib/components/button/button.component.spec.ts
+++ ngx-fudis/projects/ngx-fudis/src/lib/components/button/button.component.spec.ts
@@ -28,7 +28,7 @@ describe('ButtonComponent', () => {
 
 	describe('button CSS classes', () => {
 		it('should contain classes for medium sized primary button by default', () => {
-			assertButtonHasClasses('fudis-button', 'fudis-button__primary', 'fudis-button__medium');
+			assertButtonHasClasses('fudis', 'fudis-button', 'fudis-button__primary', 'fudis-button__medium');
 		});
 
 		it('should map the given inputs to the corresponding CSS classes', () => {
@@ -36,7 +36,7 @@ describe('ButtonComponent', () => {
 			component.variant = 'secondary';
 			fixture.detectChanges();
 
-			assertButtonHasClasses('fudis-button', 'fudis-button__small', 'fudis-button__secondary');
+			assertButtonHasClasses('fudis', 'fudis-button', 'fudis-button__small', 'fudis-button__secondary');
 		});
 	});
 
