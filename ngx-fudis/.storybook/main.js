c34fad8078e6f677c6b93afda69fc87df03d4d09
riinakuukso <riina.kuukso@funidata.fi>
2022-11-21 11:43:44 +0200

    Add theme toolbar option for Storybook

--- ngx-fudis/.storybook/main.js
+++ ngx-fudis/.storybook/main.js
@@ -1,23 +1,24 @@
 module.exports = {
-  stories: [
-    "../projects/ngx-fudis/src/lib/components/**/*.stories.ts",
-    "../projects/ngx-fudis/src/lib/foundations/**/*.stories.mdx",
-    "../projects/documentation/**/*.stories.mdx",
-  ],
-  addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-    {
-      name: "@storybook/addon-docs",
-      options: {
-        transcludeMarkdown: true,
-      },
-    },
-  ],
-  framework: "@storybook/angular",
-  core: {
-    builder: "@storybook/builder-webpack5",
-  },
-  staticDirs: ['./../projects/ngx-fudis/src/lib/assets/fonts/fira/woff2'],
+	stories: [
+		'../projects/ngx-fudis/src/lib/components/**/*.stories.ts',
+		'../projects/ngx-fudis/src/lib/foundations/**/*.stories.mdx',
+		'../projects/documentation/**/*.stories.mdx',
+	],
+	addons: [
+		'@storybook/addon-links',
+		'@storybook/addon-essentials',
+		'@storybook/addon-interactions',
+		'@storybook/addon-toolbar',
+		{
+			name: '@storybook/addon-docs',
+			options: {
+				transcludeMarkdown: true,
+			},
+		},
+	],
+	framework: '@storybook/angular',
+	core: {
+		builder: '@storybook/builder-webpack5',
+	},
+	staticDirs: ['./../projects/ngx-fudis/src/lib/assets/fonts/fira/woff2'],
 };
