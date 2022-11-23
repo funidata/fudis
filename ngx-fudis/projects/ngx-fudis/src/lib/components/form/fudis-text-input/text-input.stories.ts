30db1446660e44173c2249856ce8142079fc9099
riinakuukso <riina.kuukso@funidata.fi>
2022-11-23 13:48:40 +0200

    DS-49: Add two new stories to text-input for test purposes

--- ngx-fudis/projects/ngx-fudis/src/lib/components/form/fudis-text-input/text-input.stories.ts
+++ ngx-fudis/projects/ngx-fudis/src/lib/components/form/fudis-text-input/text-input.stories.ts
@@ -2,6 +2,7 @@ import { Story, Meta } from '@storybook/angular/types-6-0';
 import { moduleMetadata } from '@storybook/angular';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormControl, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { TextInputComponent } from './text-input.component';
 
 export default {
@@ -9,7 +10,7 @@ export default {
 	component: TextInputComponent,
 	decorators: [
 		moduleMetadata({
-			imports: [MatInputModule, MatFormFieldModule],
+			imports: [MatInputModule, MatFormFieldModule, ReactiveFormsModule, FormsModule],
 		}),
 	],
 	argTypes: {},
@@ -23,3 +24,35 @@ export const TextInput = Template.bind({});
 TextInput.args = {
 	label: 'Is this the label?',
 };
+
+const control = new FormControl('', Validators.required);
+export const WithErrorMessage: Story = (args) => ({
+	props: {
+		...args,
+		control,
+	},
+	template: `
+		<mat-form-field class="fudis-form-field">
+			<label>Inputin label</label>
+			<input matInput type="text" [formControl]="control" class="fudis-text-input" required />
+			<mat-error *ngIf="control.hasError('required')">Tää on pakollinen</mat-error>
+		</mat-form-field>
+    `,
+});
+WithErrorMessage.storyName = 'with error message';
+WithErrorMessage.parameters = {
+	controls: {
+		exclude: ['alignHint'],
+	},
+};
+
+export const WithFudis: Story = (args) => ({
+	props: {
+		...args,
+		control,
+	},
+	template: `
+		<fudis-text-input required></fudis-text-input>
+    `,
+});
+WithFudis.storyName = 'with fudis';
