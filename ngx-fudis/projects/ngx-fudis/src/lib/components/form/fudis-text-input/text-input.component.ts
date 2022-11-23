4cff21b9968f9abf3fedb1bbd4cdcdef46e9f734
riinakuukso <riina.kuukso@funidata.fi>
2022-11-23 13:46:50 +0200

    DS-49: Clean up text-input component for now

--- ngx-fudis/projects/ngx-fudis/src/lib/components/form/fudis-text-input/text-input.component.ts
+++ ngx-fudis/projects/ngx-fudis/src/lib/components/form/fudis-text-input/text-input.component.ts
@@ -1,6 +1,5 @@
 import { Component, Input } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
-// import { TouchedErrorStateMatcher } from '../touched-error-state.matcher';
 
 @Component({
 	selector: 'fudis-text-input',
@@ -8,31 +7,11 @@ import { FormControl, Validators } from '@angular/forms';
 	styleUrls: ['./text-input.component.scss'],
 })
 export class TextInputComponent {
-	@Input() id: string = 'munInput';
+	@Input() id: string;
 
 	@Input() label: string;
 
-	@Input() type: 'email' | 'number' | 'password' | 'tel' | 'text' | 'url' = 'email';
+	@Input() type: 'email' | 'number' | 'password' | 'tel' | 'text' | 'url' = 'text';
 
-	// @Input() required: boolean;
-
-	// @Input() disabled: boolean;
-
-	// @Input() control: FormControl;
-
-	emailFormControl = new FormControl('', [Validators.required, Validators.email]);
-
-	// matcher = new TouchedErrorStateMatcher();
-
-	// public get classes(): string[] {
-	// 	return ['fudis-text-input', `fudis-text-input__${this.type}`];
-	// }
-
-	getErrorMessage() {
-		if (this.emailFormControl.hasError('required')) {
-			return 'You must enter a value';
-		}
-
-		return this.emailFormControl.hasError('email') ? 'Not a valid email' : '';
-	}
+	control = new FormControl('', Validators.required);
 }
