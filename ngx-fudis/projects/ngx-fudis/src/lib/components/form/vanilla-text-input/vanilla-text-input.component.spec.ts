51b94d9b79df3df80c9ca40b2c29f950e66cb9d5
videoeero <eero.suvanto@gmail.com>
2022-12-07 22:44:17 +0200

    Fix linting

--- ngx-fudis/projects/ngx-fudis/src/lib/components/form/vanilla-text-input/vanilla-text-input.component.spec.ts
+++ ngx-fudis/projects/ngx-fudis/src/lib/components/form/vanilla-text-input/vanilla-text-input.component.spec.ts
@@ -3,23 +3,22 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { VanillaTextInputComponent } from './vanilla-text-input.component';
 
 describe('VanillaTextInputComponent', () => {
-  let component: VanillaTextInputComponent;
-  let fixture: ComponentFixture<VanillaTextInputComponent>;
+	let component: VanillaTextInputComponent;
+	let fixture: ComponentFixture<VanillaTextInputComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ VanillaTextInputComponent ]
-    })
-    .compileComponents();
-  });
+	beforeEach(async () => {
+		await TestBed.configureTestingModule({
+			declarations: [VanillaTextInputComponent],
+		}).compileComponents();
+	});
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(VanillaTextInputComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+	beforeEach(() => {
+		fixture = TestBed.createComponent(VanillaTextInputComponent);
+		component = fixture.componentInstance;
+		fixture.detectChanges();
+	});
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+	it('should create', () => {
+		expect(component).toBeTruthy();
+	});
 });
