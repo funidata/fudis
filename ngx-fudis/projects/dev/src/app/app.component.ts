3712490cb8621a71c2b3684f1e81c038eadb59b4
riinakuukso <riina.kuukso@funidata.fi>
2022-12-27 17:45:00 +0200

    DS-51: Clean and rearrange sandbox

--- ngx-fudis/projects/dev/src/app/app.component.ts
+++ ngx-fudis/projects/dev/src/app/app.component.ts
@@ -6,70 +6,44 @@ import { Component } from '@angular/core';
 		<!--The content below is only a placeholder and can be replaced.-->
 		<div style="text-align:center" class="content">
 			<h1>Welcome to {{ title }}!</h1>
-			<!-- <app-dialog-test></app-dialog-test> -->
-			<fudis-checkbox color="primary" data-theme="sisu">Tämä on fudis checkbox</fudis-checkbox>
-			<!-- <fudis-heading size="xl" tag="h2"></fudis-heading>
-			<fudis-body-text size="l-regular">Haloo</fudis-body-text>
-			<fudis-body-text size="m-regular">Haloo</fudis-body-text>
-			<fudis-body-text size="s-regular">Haloo</fudis-body-text>
-			<fudis-body-text size="l-light">Haloo</fudis-body-text>
-			<fudis-body-text size="m-light">Haloo</fudis-body-text> -->
 		</div>
-		<form
-			class="basic-flex-box"
-			action="
-		">
-			<fudis-text-input
-				data-theme="sisu"
-				label="ei pakollinen tekstikenttä"
-				helpText="Tähän saa kirjoittaa, mutta ei oo pakko"
-				characterLimitIndicatorValue="50"
-				[maxLength]="50"></fudis-text-input>
-			<fudis-text-input
-				required
-				data-theme="sisu"
-				label="pakollinen email"
-				helpText="Tähän on pakko kirjoittaa sähköposti"
-				type="email"></fudis-text-input>
-			<fudis-text-input data-theme="sisu" label="vapaaehtoinen email" type="email"></fudis-text-input>
-			<fudis-text-input data-theme="sisu" label="numerot" type="number"></fudis-text-input>
-			<fudis-vanilla-text-input data-theme="sisu" [required]="true"></fudis-vanilla-text-input>
-			<fudis-native-checkbox data-theme="sisu" label="Uusi label" [required]="true"></fudis-native-checkbox>
-			<fudis-vanilla-text-input data-theme="sisu" [required]="true"></fudis-vanilla-text-input>
-		</form>
-		<fudis-icon icon="chevron" rotate="cw-90"></fudis-icon>
-		<fudis-icon icon="achievement" color="success"></fudis-icon>
-
 		<form class="basic-flex-box">
-			<fudis-vanilla-text-input
-				data-theme="sisu"
-				label="Vanilla text input"
-				[required]="true"
-				helpText="Voit kirjoittaa tähän yhden kivan jutun"></fudis-vanilla-text-input>
+			<h2>Text input ja checbox</h2>
+			<fudis-vanilla-text-input [required]="true" label="Vanilla text input"></fudis-vanilla-text-input>
+			<fudis-native-checkbox data-theme="sisu" label="Checkboxin label" [required]="true"></fudis-native-checkbox>
+			<fudis-vanilla-text-input label="Vanilla text input 2"></fudis-vanilla-text-input>
+		</form>
+		<form class="basic-flex-box">
+			<h2>Textarea ja button</h2>
 			<fudis-vanilla-text-area
 				label="Pakollinen vanilla textarea, tällä on myös aika pitkä label"
 				[maxLength]="20"
-				size="m"
-				helpText="Voit kirjoittaa tähän monia kivoja juttuja"
+				helpText="Voit kirjoittaa tähän monia kivoja juttuja, mutta max 20 kirjainta"
 				[required]="true">
 			</fudis-vanilla-text-area>
-			<fudis-vanilla-text-area label="Vanilla textarea, basic"> </fudis-vanilla-text-area>
+			<fudis-vanilla-text-area label="Vanilla textarea, basic" size="m"> </fudis-vanilla-text-area>
 			<fudis-button data-theme="sisu" type="submit" label="Lähetä"></fudis-button>
 		</form>
-		<fudis-badge variant="accent" content="accent"></fudis-badge>
-		<fudis-badge variant="danger">danger</fudis-badge>
-		<fudis-badge data-theme="sisu" variant="primary" content="sisu primary"></fudis-badge>
-		<fudis-badge data-theme="into" variant="primary" content="into primary"></fudis-badge>
-		<fudis-badge variant="secondary" content="secondary"></fudis-badge>
-		<fudis-badge variant="success" content="success"></fudis-badge>
 		<form class="basic-flex-box">
-			<fudis-dropdown data-theme="sisu" [multipleOption]="true" label="Valitse kurssit"></fudis-dropdown>
+			<h2>Dropdown</h2>
+			<fudis-dropdown [multipleOption]="true" label="Multi select"></fudis-dropdown>
 			<fudis-dropdown
-				data-theme="sisu"
 				[required]="true"
-				label="Valitse yksi kurssi"
+				label="Single select"
 				helpText="Tästä pitäisi valita kiinnostavin kurssi, ole hyvä"></fudis-dropdown>
 		</form>
+		<div class="basic-flex-box2">
+			<fudis-icon icon="chevron" rotate="cw-90"></fudis-icon>
+			<fudis-icon icon="achievement" color="success"></fudis-icon>
+		</div>
+		<div class="basic-flex-box2">
+			<fudis-badge variant="accent" content="accent"></fudis-badge>
+			<fudis-badge variant="danger">danger</fudis-badge>
+			<fudis-badge data-theme="sisu" variant="primary" content="sisu primary"></fudis-badge>
+			<fudis-badge data-theme="into" variant="primary" content="into primary"></fudis-badge>
+			<fudis-badge variant="secondary" content="secondary"></fudis-badge>
+			<fudis-badge variant="success" content="success"></fudis-badge>
+		</div>
 	`,
 	styleUrls: ['./app.scss'],
 })
