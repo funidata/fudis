7622c4c6f2bf264ea5c72e69ab8033ec63d7d0cc
Marjut Kinnari <marjut.kinnari@funidata.fi>
2022-12-28 15:39:21 +0200

    DS-53 Created radio button story

---
+++ ngx-fudis/projects/ngx-fudis/src/lib/components/form/radio-button-group/radio-button-group.component.stories.ts
@@ -0,0 +1,18 @@
+import { Story, Meta } from '@storybook/angular/types-6-0';
+import { RadioButtonGroupComponent } from './radio-button-group.component';
+
+export default {
+	title: 'Components/Form/FudisRadioButtonGroup',
+	component: RadioButtonGroupComponent,
+} as Meta;
+
+const Template: Story<RadioButtonGroupComponent> = (args: RadioButtonGroupComponent) => ({
+	props: args,
+});
+
+export const RadioButtonGroup = Template.bind({});
+RadioButtonGroup.args = {
+	disabled: false,
+	required: true,
+	label: 'Hedelmät',
+};
