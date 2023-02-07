5a7ab51ab374ab285141df4e6803d185166a0057
riinakuukso <riina.kuukso@funidata.fi>
2023-02-07 09:16:29 +0200

    DS-79: Remove unnecessary styles and broken code from sandbox

--- ngx-fudis/projects/dev/src/app/app.component.ts
+++ ngx-fudis/projects/dev/src/app/app.component.ts
@@ -8,12 +8,6 @@ import { Component } from '@angular/core';
 			<h1>Welcome to {{ title }}!</h1>
 		</div>
 		<form class="basic-flex-box">
-			<h2>Text input ja checbox</h2>
-			<fudis-text-input [required]="true" label="Fudis text input"></fudis-text-input>
-			<!-- <fudis-checkbox data-theme="sisu" label="Checkboxin label" [required]="true"></fudis-checkbox> -->
-			<fudis-text-input label="Fudis text input 2"></fudis-text-input>
-		</form>
-		<form class="basic-flex-box">
 			<h2>Textarea ja button</h2>
 			<fudis-text-area
 				label="Pakollinen textarea, tällä on myös aika pitkä label"
@@ -41,19 +35,6 @@ import { Component } from '@angular/core';
 				label="Single select"
 				helpText="Tästä pitäisi valita kiinnostavin kurssi, ole hyvä"></fudis-dropdown>
 		</form>
-		<div class="basic-flex-box2">
-			<fudis-icon icon="chevron" rotate="cw-90"></fudis-icon>
-			<fudis-icon icon="achievement" color="success"></fudis-icon>
-		</div>
-
-		<!-- <div class="basic-flex-box2">
-			<fudis-badge variant="accent" content="accent"></fudis-badge>
-			<fudis-badge variant="danger">danger</fudis-badge>
-			<fudis-badge data-theme="sisu" variant="primary" content="sisu primary"></fudis-badge>
-			<fudis-badge data-theme="into" variant="primary" content="into primary"></fudis-badge>
-			<fudis-badge variant="secondary" content="secondary"></fudis-badge>
-			<fudis-badge variant="success" content="success"></fudis-badge>
-		</div> -->
 	`,
 	styleUrls: ['./app.scss'],
 })
