9af8ef81eb3df645d893e22248879c61e290c749
videoeero <eero.suvanto@gmail.com>
2023-02-09 16:32:56 +0200

    DS-77: Fix tests

--- ngx-fudis/projects/ngx-fudis/src/lib/components/form/dropdown/dropdown.component.spec.ts
+++ ngx-fudis/projects/ngx-fudis/src/lib/components/form/dropdown/dropdown.component.spec.ts
@@ -1,9 +1,14 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule, UntypedFormControl } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSelectModule } from '@angular/material/select';
+import { MockComponent } from 'ng-mocks';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { DropdownComponent } from './dropdown.component';
+import { LabelComponent } from '../label/label.component';
+import { ErrorMessageComponent } from '../error-message/error-message.component';
+
+const dropdownControl: UntypedFormControl = new UntypedFormControl('');
 
 describe('DropdownComponent', () => {
 	let component: DropdownComponent;
@@ -11,7 +16,7 @@ describe('DropdownComponent', () => {
 
 	beforeEach(async () => {
 		await TestBed.configureTestingModule({
-			declarations: [DropdownComponent],
+			declarations: [DropdownComponent, MockComponent(ErrorMessageComponent), MockComponent(LabelComponent)],
 			imports: [ReactiveFormsModule, MatFormFieldModule, MatSelectModule, BrowserAnimationsModule],
 		}).compileComponents();
 	});
@@ -19,6 +24,8 @@ describe('DropdownComponent', () => {
 	beforeEach(() => {
 		fixture = TestBed.createComponent(DropdownComponent);
 		component = fixture.componentInstance;
+		component.label = 'Label for testing purposes';
+		component.control = dropdownControl;
 		fixture.detectChanges();
 	});
 
--- ngx-fudis/projects/ngx-fudis/src/lib/components/form/label/label.component.spec.ts
+++ ngx-fudis/projects/ngx-fudis/src/lib/components/form/label/label.component.spec.ts
@@ -3,21 +3,20 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { LabelComponent } from './label.component';
 
 describe('LabelComponent', () => {
-  let component: LabelComponent;
-  let fixture: ComponentFixture<LabelComponent>;
+	let component: LabelComponent;
+	let fixture: ComponentFixture<LabelComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ LabelComponent ]
-    })
-    .compileComponents();
+	beforeEach(async () => {
+		await TestBed.configureTestingModule({
+			declarations: [LabelComponent],
+		}).compileComponents();
 
-    fixture = TestBed.createComponent(LabelComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+		fixture = TestBed.createComponent(LabelComponent);
+		component = fixture.componentInstance;
+		fixture.detectChanges();
+	});
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+	it('should create', () => {
+		expect(component).toBeTruthy();
+	});
 });
