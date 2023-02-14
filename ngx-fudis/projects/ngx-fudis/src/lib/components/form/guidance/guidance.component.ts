3c86104dd5ed45a4e3861ac4f92a172149fd42ac
videoeero <eero.suvanto@gmail.com>
2023-02-14 12:12:35 +0200

    Add guidance-component

---
+++ ngx-fudis/projects/ngx-fudis/src/lib/components/form/guidance/guidance.component.ts
@@ -0,0 +1,48 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { IFudisErrorMessages, IFudisErrorSummaryItem } from '../../../types/forms';
+
+@Component({
+	selector: 'fudis-guidance',
+	templateUrl: './guidance.component.html',
+	styleUrls: ['./guidance.component.scss'],
+})
+export class GuidanceComponent {
+	@Input() id: string;
+
+	@Input() control: FormControl;
+
+	@Input() helpText: string | undefined;
+
+	@Input() maxLength: number | undefined;
+
+	@Input() errorMsg: IFudisErrorMessages;
+
+	@Output() errorOutput: EventEmitter<IFudisErrorSummaryItem> = new EventEmitter<IFudisErrorSummaryItem>();
+
+	showError: boolean = false;
+
+	errorMsgToShow: string[] = [];
+
+	checkErrors(): boolean {
+		this.errorMsgToShow = [];
+		if (this.control.touched && this.control.errors) {
+			this.showError = true;
+
+			Object.keys(this.control.errors).forEach((item) => {
+				const message = this.errorMsg[item as keyof IFudisErrorMessages];
+				if (message) {
+					this.errorMsgToShow.push(message);
+					this.getErrorOutput(this.id, message);
+				}
+			});
+			return true;
+		}
+		this.showError = false;
+		return false;
+	}
+
+	getErrorOutput(id: string, error: string) {
+		this.errorOutput.emit({ id, message: error });
+	}
+}
