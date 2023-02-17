88862c046fcbee885a38fe4bb5f4340cace25f8d
riinakuukso <riina.kuukso@funidata.fi>
2023-02-17 11:45:45 +0200

    DS-80: Mock components in test file

--- ngx-fudis/projects/ngx-fudis/src/lib/components/form/datepicker/datepicker.component.spec.ts
+++ ngx-fudis/projects/ngx-fudis/src/lib/components/form/datepicker/datepicker.component.spec.ts
@@ -1,4 +1,11 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MockComponent } from 'ng-mocks';
+import { IconComponent } from 'ngx-fudis';
+import { GuidanceComponent } from '../guidance/guidance.component';
+import { LabelComponent } from '../label/label.component';
 
 import { DatepickerComponent } from './datepicker.component';
 
@@ -8,7 +15,13 @@ describe('DatepickerComponent', () => {
 
 	beforeEach(async () => {
 		await TestBed.configureTestingModule({
-			declarations: [DatepickerComponent],
+			declarations: [
+				DatepickerComponent,
+				MockComponent(LabelComponent),
+				MockComponent(IconComponent),
+				MockComponent(GuidanceComponent),
+			],
+			imports: [MatFormFieldModule, MatDatepickerModule, MatNativeDateModule],
 		}).compileComponents();
 
 		fixture = TestBed.createComponent(DatepickerComponent);
