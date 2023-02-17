9d028bb8113a1ac5daa52a39226d84c592dd3f78
riinakuukso <riina.kuukso@funidata.fi>
2023-02-17 13:49:32 +0200

    DS-80: Fix Injectable warning

--- ngx-fudis/projects/ngx-fudis/src/lib/components/form/datepicker/datepicker-custom-date-adapter/datepicker-custom-date-adapter.ts
+++ ngx-fudis/projects/ngx-fudis/src/lib/components/form/datepicker/datepicker-custom-date-adapter/datepicker-custom-date-adapter.ts
@@ -1,5 +1,7 @@
+import { Injectable } from '@angular/core';
 import { NativeDateAdapter } from '@angular/material/core';
 
+@Injectable()
 export class DatepickerCustomDateAdapter extends NativeDateAdapter {
 	// eslint-disable-next-line class-methods-use-this
 	override getFirstDayOfWeek(): number {
