90eeb873524e352936188eb41ed17a043c11d1fc
riinakuukso <riina.kuukso@funidata.fi>
2023-02-17 15:09:59 +0200

    DS-80: Fix test imports

--- ngx-fudis/projects/ngx-fudis/src/lib/components/form/datepicker/datepicker.component.spec.ts
+++ ngx-fudis/projects/ngx-fudis/src/lib/components/form/datepicker/datepicker.component.spec.ts
@@ -1,13 +1,16 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ReactiveFormsModule, FormControl } from '@angular/forms';
 import { MatNativeDateModule } from '@angular/material/core';
 import { MatDatepickerModule } from '@angular/material/datepicker';
-import { MatFormFieldModule } from '@angular/material/form-field';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MockComponent } from 'ng-mocks';
 import { IconComponent } from 'ngx-fudis';
 import { GuidanceComponent } from '../guidance/guidance.component';
 import { LabelComponent } from '../label/label.component';
 
 import { DatepickerComponent } from './datepicker.component';
+
+const datepickerControl: FormControl = new FormControl('');
 
 describe('DatepickerComponent', () => {
 	let component: DatepickerComponent;
@@ -21,11 +24,14 @@ describe('DatepickerComponent', () => {
 				MockComponent(IconComponent),
 				MockComponent(GuidanceComponent),
 			],
-			imports: [MatFormFieldModule, MatDatepickerModule, MatNativeDateModule],
+			imports: [ReactiveFormsModule, MatDatepickerModule, MatNativeDateModule, BrowserAnimationsModule],
 		}).compileComponents();
+	});
 
+	beforeEach(() => {
 		fixture = TestBed.createComponent(DatepickerComponent);
 		component = fixture.componentInstance;
+		component.control = datepickerControl;
 		fixture.detectChanges();
 	});
 
