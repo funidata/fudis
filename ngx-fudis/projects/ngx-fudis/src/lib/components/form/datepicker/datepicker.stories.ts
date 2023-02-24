58b81db770e0e448a734bc0cb0c5b2e1ff6cfc9e
riinakuukso <riina.kuukso@funidata.fi>
2023-02-24 14:40:43 +0200

    DS-80: Refactor datepicker Story

--- ngx-fudis/projects/ngx-fudis/src/lib/components/form/datepicker/datepicker.stories.ts
+++ ngx-fudis/projects/ngx-fudis/src/lib/components/form/datepicker/datepicker.stories.ts
@@ -1,91 +1,73 @@
 import { Story, Meta } from '@storybook/angular/types-6-0';
 import { moduleMetadata } from '@storybook/angular';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatDatepickerModule } from '@angular/material/datepicker';
-import { MatInputModule } from '@angular/material/input';
-import { Component } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 import { DatepickerComponent } from './datepicker.component';
-import { IFudisErrorMessages } from '../../../types/forms';
+// import { DatepickerCustomHeaderComponent } from './datepicker-custom-header/datepicker-custom-header.component';
 import readme from './readme.mdx';
-
-@Component({
-	selector: 'example-datepicker-with-form-control',
-	template: `
-		<form [formGroup]="mainFormGroup">
-			<fudis-datepicker
-				[control]="mainFormGroup.controls['datepickerBasic']"
-				[id]="'unique-datepicker-id-1'"
-				requiredText="Required"
-				[errorMsg]="validatorMessages"
-				label="Select a date"
-				helpText="Please select your favourite date."></fudis-datepicker>
-			<fudis-datepicker
-				[control]="mainFormGroup.controls['datepickerWithMinMax']"
-				[id]="'unique-datepicker-id-2'"
-				requiredText="Required"
-				[minDate]="myMinDate"
-				[maxDate]="myMaxDate"
-				[errorMsg]="validatorMessages"
-				label="Datepicker with min and max dates"
-				helpText="Please select a date inside the allowed range."></fudis-datepicker>
-			<fudis-datepicker
-				[control]="mainFormGroup.controls['datepickerDisabled']"
-				[id]="'unique-datepicker-id-3'"
-				[disabled]="true"
-				label="This is disabled datepicker"></fudis-datepicker>
-		</form>
-	`,
-})
-class DatepickerWithFormControlExampleComponent {
-	validatorsForDatepicker = [Validators.required];
-
-	validatorMessages: IFudisErrorMessages = {
-		required: 'This is required field.',
-		matDatepickerMin: 'Too low date!',
-		matDatepickerMax: 'Too high date',
-	};
-
-	mainFormGroup: FormGroup = this.formBuilder.group({
-		datepickerBasic: new FormControl('', this.validatorsForDatepicker),
-		datepickerDisabled: new FormControl(''),
-		datepickerWithMinMax: new FormControl('', this.validatorsForDatepicker),
-	});
-
-	myMinDate = new Date(2023, 2, 10);
-
-	myMaxDate = new Date(2023, 2, 25);
-
-	constructor(private formBuilder: FormBuilder) {}
-}
 
 export default {
 	title: 'Components/Form/Datepicker',
 	component: DatepickerComponent,
-	decorators: [
-		moduleMetadata({
-			imports: [
-				BrowserAnimationsModule,
-				MatFormFieldModule,
-				MatDatepickerModule,
-				MatInputModule,
-				ReactiveFormsModule,
-				FormsModule,
-			],
-			declarations: [DatepickerWithFormControlExampleComponent],
-		}),
-	],
-	argTypes: {},
 	parameters: {
 		docs: {
 			page: readme,
 		},
 	},
+	decorators: [
+		moduleMetadata({
+			imports: [],
+			declarations: [],
+		}),
+	],
+	argTypes: {},
 } as Meta;
 
-export const Datepicker: Story = () => ({
+const Template: Story<DatepickerComponent> = (args: DatepickerComponent) => ({
+	props: args,
 	template: `
-	<example-datepicker-with-form-control></example-datepicker-with-form-control>
+	<fudis-datepicker 
+		[label]="label" 
+		[id]="id" 
+		[requiredText]="requiredText"  
+		[helpText]="helpText" 
+		[errorMsg]="errorMsg" 
+		[control]="control" 
+		[disabled]="disabled" 
+		[minDate]="minDate" 
+		[maxDate]="maxDate">
+	</fudis-datepicker>
 	`,
 });
+
+export const Datepicker = Template.bind({});
+Datepicker.args = {
+	id: 'example-id-for-datepicker-required-validation',
+	label: 'Select a date',
+	required: true,
+	requiredText: 'Required',
+	helpText: 'Choose your favourite date.',
+	errorMsg: { required: 'Date is required.' },
+	control: new FormControl('', Validators.required),
+};
+
+export const Disabled = Template.bind({});
+Disabled.args = {
+	id: 'example-id-for-datepicker-disabled',
+	label: 'Select a date',
+	control: new FormControl(''),
+	disabled: true,
+};
+
+export const WithMinMaxValidator = Template.bind({});
+WithMinMaxValidator.args = {
+	id: 'example-id-for-datepicker-min-max-validator',
+	label: 'Select a date',
+	helpText: 'Choose a date between the allowed range.',
+	errorMsg: {
+		matDatepickerMin: 'Date is not inside the allowed range.',
+		matDatepickerMax: 'Date is not inside the allowed range.',
+	},
+	control: new FormControl(''),
+	minDate: new Date(2023, 2, 13),
+	maxDate: new Date(2023, 2, 26),
+};
