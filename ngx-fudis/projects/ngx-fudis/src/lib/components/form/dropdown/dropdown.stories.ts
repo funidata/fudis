b1f310afe61e1469f22ee56d82bbbfafe28aa1b7
riinakuukso <riina.kuukso@funidata.fi>
2023-02-27 11:09:07 +0200

    Add required boolean to dropdown stories in order it to be displayed properly

--- ngx-fudis/projects/ngx-fudis/src/lib/components/form/dropdown/dropdown.stories.ts
+++ ngx-fudis/projects/ngx-fudis/src/lib/components/form/dropdown/dropdown.stories.ts
@@ -49,6 +49,7 @@ export const SingleSelect = Template.bind({});
 SingleSelect.args = {
 	errorMsg: { required: "It is necessary to choose a pet. It's good for your health!" },
 	requiredText: 'Required',
+	required: true,
 	label: 'Select a pet',
 	placeholder: 'Choose a pet',
 	multipleOption: false,
@@ -74,6 +75,7 @@ MultiSelect.args = {
 	},
 	multipleOption: true,
 	requiredText: 'Required',
+	required: true,
 	label: 'Select from two to three pets',
 	placeholder: 'Choose a pet',
 	control: new FormControl('', [Validators.required, Validators.minLength(2), Validators.maxLength(3)]),
