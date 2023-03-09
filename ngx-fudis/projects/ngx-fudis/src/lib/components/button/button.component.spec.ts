ec6f04df30e5097827014988f3e1f9986ab09c27
riinakuukso <riina.kuukso@funidata.fi>
2023-03-09 11:28:22 +0200

    DS-94: Add test cases

--- ngx-fudis/projects/ngx-fudis/src/lib/components/button/button.component.spec.ts
+++ ngx-fudis/projects/ngx-fudis/src/lib/components/button/button.component.spec.ts
@@ -1,4 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MockComponent } from 'ng-mocks';
+import { IconComponent } from 'ngx-fudis';
 import { ButtonComponent } from './button.component';
 
 describe('ButtonComponent', () => {
@@ -7,7 +9,7 @@ describe('ButtonComponent', () => {
 
 	beforeEach(async () => {
 		await TestBed.configureTestingModule({
-			declarations: [ButtonComponent],
+			declarations: [ButtonComponent, MockComponent(IconComponent)],
 		}).compileComponents();
 	});
 
@@ -21,22 +23,26 @@ describe('ButtonComponent', () => {
 		return fixture.nativeElement.querySelector('button') as HTMLElement;
 	}
 
-	function assertButtonHasClasses(...classes: string[]): void {
-		const buttonClasses = getButton()?.className ?? '';
-		expect(buttonClasses.split(' ').sort()).toEqual([...classes].sort());
+	function assertButtonHasClasses(classes: string): void {
+		const buttonClasses = getButton().className.split(' ').sort();
+		expect(buttonClasses).toEqual(classes.split(' ').sort());
 	}
 
-	describe('button CSS classes', () => {
+	describe('CSS classes', () => {
 		it('should contain classes for medium sized primary button by default', () => {
-			assertButtonHasClasses('fudis-button', 'fudis-button__primary', 'fudis-button__medium');
+			assertButtonHasClasses('fudis-button fudis-button__primary fudis-button__medium');
 		});
 
 		it('should map the given inputs to the corresponding CSS classes', () => {
 			component.size = 'small';
 			component.variant = 'secondary';
 			fixture.detectChanges();
+			assertButtonHasClasses('fudis-button fudis-button__small fudis-button__secondary');
 
-			assertButtonHasClasses('fudis-button', 'fudis-button__small', 'fudis-button__secondary');
+			component.size = 'medium';
+			component.variant = 'tertiary';
+			fixture.detectChanges();
+			assertButtonHasClasses('fudis-button fudis-button__medium fudis-button__tertiary');
 		});
 	});
 
@@ -62,6 +68,27 @@ describe('ButtonComponent', () => {
 
 			getButton()?.click();
 			expect(clicked).withContext('Unexpected click event received').toEqual(false);
+		});
+	});
+
+	describe('button with icon and aria-label', () => {
+		it('should have icon component present if icon and aria-label has been given as an Input', () => {
+			component.icon = 'three-dots';
+			component.ariaLabel = 'Open additional menu';
+			component.type = 'button';
+			fixture.detectChanges();
+			expect(IconComponent).toBeTruthy();
+			expect(getButton().getAttribute('aria-label')).toBeTruthy();
+			expect(getButton().getAttribute('aria-label')).toEqual('Open additional menu');
+			expect(getButton().getAttribute('type')).toEqual('button');
+		});
+	});
+
+	describe('button label', () => {
+		it('should show uppercase context', () => {
+			component.label = 'Click me!';
+			fixture.detectChanges();
+			expect(getButton().innerText).toEqual('CLICK ME!');
 		});
 	});
 });
