53462dbff71183234af9428496749bb5fe91678b
riinakuukso <riina.kuukso@funidata.fi>
2023-03-22 16:59:28 +0200

    DS-85: Add mocking and necessary imports to avoid build failure

--- ngx-fudis/projects/ngx-fudis/src/lib/components/form/autocomplete/autocomplete.component.spec.ts
+++ ngx-fudis/projects/ngx-fudis/src/lib/components/form/autocomplete/autocomplete.component.spec.ts
@@ -1,6 +1,13 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MockComponent } from 'ng-mocks';
+import { GuidanceComponent } from '../guidance/guidance.component';
+import { LabelComponent } from '../label/label.component';
 import { AutocompleteComponent } from './autocomplete.component';
+
+const autocompleteControl: FormControl = new FormControl('');
 
 describe('AutocompleteComponent', () => {
 	let component: AutocompleteComponent;
@@ -8,11 +15,13 @@ describe('AutocompleteComponent', () => {
 
 	beforeEach(async () => {
 		await TestBed.configureTestingModule({
-			declarations: [AutocompleteComponent],
+			declarations: [AutocompleteComponent, MockComponent(LabelComponent), MockComponent(GuidanceComponent)],
+			imports: [MatAutocompleteModule, ReactiveFormsModule, BrowserAnimationsModule],
 		}).compileComponents();
 
 		fixture = TestBed.createComponent(AutocompleteComponent);
 		component = fixture.componentInstance;
+		component.control = autocompleteControl;
 		fixture.detectChanges();
 	});
 
