dfed7a1a271c2f5b87cc33041bc9580722161d57
videoeero <videoeero@users.noreply.github.com>
2023-04-05 09:19:20 +0000

    857327487/update-ng-14-to-15

---
+++ update-ng-14-to-15/components-form-error-summary-error-summary-stories.34c52041.iframe.bundle.js
@@ -0,0 +1,7 @@
+"use strict";(self.webpackChunkngx_fudis=self.webpackChunkngx_fudis||[]).push([[791],{"./projects/ngx-fudis/src/lib/components/form/error-summary/error-summary.stories.ts":(__unused_webpack_module,__webpack_exports__,__webpack_require__)=>{__webpack_require__.r(__webpack_exports__),__webpack_require__.d(__webpack_exports__,{ErrorSummary:()=>ErrorSummary,default:()=>__WEBPACK_DEFAULT_EXPORT__});const __WEBPACK_DEFAULT_EXPORT__={title:"Components/Form/ErrorSummary",component:__webpack_require__("./projects/ngx-fudis/src/lib/components/form/error-summary/error-summary.component.ts").i,parameters:{controls:{exclude:["control"]}}},html=String.raw,ErrorSummary=()=>({template:html`
+        <form id="testform">
+            <fudis-error-summary>
+                <h1>TBD</h1>
+            </fudis-error-summary>
+        </form>
+    `});ErrorSummary.parameters={...ErrorSummary.parameters,docs:{...ErrorSummary.parameters?.docs,source:{originalSource:'() => ({\n  template: html`\n        <form id="testform">\n            <fudis-error-summary>\n                <h1>TBD</h1>\n            </fudis-error-summary>\n        </form>\n    `\n})',...ErrorSummary.parameters?.docs?.source}}}}}]);
