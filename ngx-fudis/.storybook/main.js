44ca8ca9befa777d9479bbf65f70bb5ed878181c
videoeero <eero.suvanto@gmail.com>
2023-04-24 18:25:59 +0300

    DS-132: Update deps

--- ngx-fudis/.storybook/main.js
+++ ngx-fudis/.storybook/main.js
@@ -1,5 +1,4 @@
 import remarkGfm from 'remark-gfm';
-
 module.exports = {
 	stories: [
 		'../projects/ngx-fudis/src/lib/**/*.stories.ts',
@@ -12,49 +11,39 @@ module.exports = {
 		'@storybook/addon-interactions',
 		'@storybook/addon-a11y',
 		'storybook-version',
-		{
-			name: '@storybook/addon-docs',
-			options: {
-				transcludeMarkdown: true,
-				mdxPluginOptions: {
-					mdxCompileOptions: {
-						remarkPlugins: [remarkGfm],
-					},
-				},
-			},
-		},
 	],
 	framework: {
 		name: '@storybook/angular',
-		options: { enableIvy: true },
+		options: {
+			enableIvy: true,
+		},
 	},
 	features: {
 		modernInlineRender: true,
 		previewMdx2: true,
-	},
-	core: {
-		builder: '@storybook/builder-webpack5',
 	},
 	staticDirs: [
 		'./../projects/ngx-fudis/src/lib/assets/fonts/fira/woff2',
 		'./../projects/ngx-fudis/src/lib/assets/icons',
 	],
 	docs: {
-		autodocs: true, // see below for alternatives
+		autodocs: true,
+		// see below for alternatives
 		defaultName: 'Documentation', // set to change the name of generated docs entries
 	},
+
 	previewHead: (head) => `
     ${head}
     <style>
-			.storybook-flex {
-				display: flex;
-				align-items: center;
-			}
-			.storybook-flex-column{
-				display: flex;
-				align-items: center;
-				flex-direction: column;
-			}
+            .storybook-flex {
+                display: flex;
+                align-items: center;
+            }
+            .storybook-flex-column{
+                display: flex;
+                align-items: center;
+                flex-direction: column;
+            }
     </style>
   `,
 	env: (config) => ({
