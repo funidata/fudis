186503b2944d9cc743cc3c803a2a8220fa578d01
videoeero <eero.suvanto@gmail.com>
2023-06-20 13:16:41 +0300

    Remove unneeded type

--- ngx-fudis/projects/ngx-fudis/src/lib/types/typography.ts
+++ ngx-fudis/projects/ngx-fudis/src/lib/types/typography.ts
@@ -1,6 +1,5 @@
 export type FudisHeadingTag = 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6';
+
 export type FudisHeadingSize = 'xxs' | 'xs' | 'sm' | 'md' | 'lg' | 'xl' | 'xxl';
 
 export type FudisBodyText = 'lg-regular' | 'md-regular' | 'sm-regular' | 'lg-light' | 'md-light';
-
-export type BodyTextMarginBottom = 'md' | 'lg' | 'none';
