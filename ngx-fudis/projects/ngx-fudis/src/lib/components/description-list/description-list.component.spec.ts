2eee9feef115d4e829040d683e90672f12c3ef9b
Marjut Kinnari <marjut.kinnari@funidata.fi>
2023-06-28 15:58:56 +0300

    DS-152 Rewrote tests

--- ngx-fudis/projects/ngx-fudis/src/lib/components/description-list/description-list.component.spec.ts
+++ ngx-fudis/projects/ngx-fudis/src/lib/components/description-list/description-list.component.spec.ts
@@ -10,7 +10,7 @@ import { DescriptionListItemComponent } from './description-list-item/descriptio
 import { DescriptionListItemTermComponent } from './description-list-item/description-list-item-term/description-list-item-term.component';
 import { DescriptionListItemDetailsComponent } from './description-list-item/description-list-item-details/description-list-item-details.component';
 
-fdescribe('DescriptionListComponent', () => {
+describe('DescriptionListComponent', () => {
 	let component: DescriptionListComponent;
 	let fixture: ComponentFixture<DescriptionListComponent>;
 
@@ -42,38 +42,41 @@ fdescribe('DescriptionListComponent', () => {
 	});
 
 	function getDescriptionList(): HTMLElement {
-		const dlItemDisplayValue = fixture.debugElement.query(By.css('fudis-dl-item .fudis-description-list__item'));
-		console.log(getComputedStyle(dlItemDisplayValue.nativeElement).display);
-		return fixture.nativeElement.querySelector('dl') as HTMLElement;
+		const dlItemDisplayValue = fixture.nativeElement.querySelector('dl') as HTMLElement;
+		return dlItemDisplayValue;
 	}
 
-	// function getDescriptionListDt(): HTMLElement {
-	// 	const i = fixture.nativeElement.querySelector('fudis-dt') as HTMLElement;
-	// 	console.log('Katso mikä tämä on', i);
-	// 	return fixture.nativeElement.querySelector('fudis-dt') as HTMLElement;
-	// }
+	function getDescriptionListDt(classes: string): HTMLElement {
+		const itemTermValue = fixture.debugElement.query(By.css(`fudis-dt .${classes}`));
+		return itemTermValue.nativeElement;
+	}
 
-	// function getDescriptionListDd(): HTMLElement {
-	// 	return fixture.nativeElement.querySelector('fudis-dd') as HTMLElement;
-	// }
+	function getDescriptionListDd(classes: string): HTMLElement {
+		const itemDetail = fixture.debugElement.query(By.css(`fudis-dd .${classes}`));
+		return itemDetail.nativeElement;
+	}
 
 	function assertDescriptionListHasClasses(classes: string[]): void {
 		const dlClasses = getDescriptionList()?.className ?? '';
-
 		expect(dlClasses.split(' ').sort()).toEqual(classes.sort());
 	}
 
-	// function assertDtHasClasses(...classes: string[]): void {
-	// 	const dtClasses = getDescriptionListDt()?.className ?? '';
-	// 	expect(dtClasses.split(' ').sort()).toEqual([...classes].sort());
-	// }
+	function assertDtHasClasses(classes: string): void {
+		const itemTermElement = getDescriptionListDt(classes);
+		const dtClasses = itemTermElement.className ?? '';
 
-	// function assertDdHasClasses(...classes: string[]): void {
-	// 	const ddClasses = getDescriptionListDd()?.className ?? '';
-	// 	expect(ddClasses.split(' ').sort()).toEqual([...classes].sort());
-	// }
+		expect(dtClasses).toContain(classes);
+	}
 
-	fdescribe('Parent CSS class', () => {
+	function assertDdHasClasses(classes: string, display: string) {
+		const itemDetailElement = getDescriptionListDd(classes);
+		const ddClasses = itemDetailElement.className ?? '';
+		const ddDisplay = getComputedStyle(itemDetailElement).display;
+		expect(ddDisplay).toEqual(display);
+		expect(ddClasses).toContain(classes);
+	}
+
+	describe('Parent CSS class', () => {
 		it('should have fudis-description-list and fudis-grid classes if regular list', () => {
 			const classList = [
 				'fudis-description-list',
@@ -84,14 +87,11 @@ fdescribe('DescriptionListComponent', () => {
 				'fudis-grid__margin__top__none',
 				'fudis-grid__xxl',
 			];
-
 			assertDescriptionListHasClasses(classList);
 		});
-
 		it('should have fudis-description-list-compact and fudis-grid classes if compact list', () => {
 			component.variant = 'compact';
 			component.ngOnChanges();
-
 			fixture.detectChanges();
 			const classList = [
 				'fudis-description-list-compact',
@@ -108,13 +108,10 @@ fdescribe('DescriptionListComponent', () => {
 		it('should not have fudis-grid classes if grid directive is disabled if regular list', () => {
 			component.disableGrid = true;
 			component.ngOnChanges();
-
 			fixture.detectChanges();
 			const classList = ['fudis-description-list', 'fudis-description-list__disabled-grid'];
-
 			assertDescriptionListHasClasses(classList);
 		});
-
 		it('should not have fudis-grid classes if grid directive is disabled if compact list', () => {
 			component.variant = 'compact';
 			component.disableGrid = true;
@@ -125,25 +122,26 @@ fdescribe('DescriptionListComponent', () => {
 		});
 	});
 
-	// describe('dt and dd child elements', () => {
-	// 	it('should be present', () => {
-	// 		const parent = fixture.debugElement.query(By.css('dl'));
-	// 		const childDt = parent.nativeElement.querySelector('dt');
-	// 		const childDd = parent.nativeElement.querySelector('dd');
-	// 		expect(childDt).toBeTruthy();
-	// 		expect(childDd).toBeTruthy();
-	// 	});
+	describe('description list item, term and detail components', () => {
+		it('should be present', () => {
+			const parent = fixture.debugElement.query(By.css('fudis-dl-item'));
+			const childDt = parent.nativeElement.querySelector('fudis-dt');
+			const childDd = parent.nativeElement.querySelector('fudis-dd');
+			expect(childDt).toBeTruthy();
+			expect(childDd).toBeTruthy();
+		});
 
-	// 	it('should have respective CSS classes in regular list', () => {
-	// 		assertDtHasClasses('fudis-description-list__item__term');
-	// 		// assertDdHasClasses('fudis-description-list__item__details');
-	// 	});
+		it('should have respective CSS classes and display style in regular list', () => {
+			assertDtHasClasses('fudis-description-list__item__term');
+			assertDdHasClasses('fudis-description-list__item__details', 'flex');
+		});
 
-	// 	it('should have respective CSS classes in compact list', () => {
-	// 		component.variant = 'compact';
-	// 		fixture.detectChanges();
-	// 		assertDtHasClasses('fudis-description-list-compact__item__term');
-	// 		// assertDdHasClasses('fudis-description-list-compact__item__details');
-	// 	});
-	// });
+		it('should have respective CSS classes and display style in compact list', () => {
+			component.variant = 'compact';
+			component.ngOnChanges();
+			fixture.detectChanges();
+			assertDtHasClasses('fudis-description-list-compact__item__term');
+			assertDdHasClasses('fudis-description-list-compact__item__details', 'inline');
+		});
+	});
 });
