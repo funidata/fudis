7dc67a59a143200b4e09096fd2f4aeed22f266f4
videoeero <eero.suvanto@gmail.com>
2023-07-05 09:12:05 +0300

    DS-160: fix initial test

--- ngx-fudis/projects/ngx-fudis/src/lib/components/footer/footer.component.spec.ts
+++ ngx-fudis/projects/ngx-fudis/src/lib/components/footer/footer.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { FooterComponent } from './footer.component';
+import { GridComponent } from '../grid/grid/grid.component';
+import { FudisGridService } from '../../directives/grid/grid-service/grid.service';
 
 describe('FooterComponent', () => {
 	let component: FooterComponent;
@@ -8,7 +10,8 @@ describe('FooterComponent', () => {
 
 	beforeEach(() => {
 		TestBed.configureTestingModule({
-			declarations: [FooterComponent],
+			declarations: [FooterComponent, GridComponent],
+			providers: [FudisGridService],
 		});
 		fixture = TestBed.createComponent(FooterComponent);
 		component = fixture.componentInstance;
