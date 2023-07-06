7966c3d84d30cb3ef254e4b2932cc59c78f4d849
videoeero <eero.suvanto@gmail.com>
2023-07-06 21:22:47 +0300

    DS-149: cleaning

--- ngx-fudis/projects/ngx-fudis/src/lib/directives/form/input-base/input-base.directive.ts
+++ ngx-fudis/projects/ngx-fudis/src/lib/directives/form/input-base/input-base.directive.ts
@@ -17,58 +17,58 @@ export class InputBaseDirective extends TooltipApiDirective {
 		});
 	}
 
-	/* *
+	/**
 	 * Label for input.
 	 */
 	@Input({ required: true }) label: string;
 
-	/* *
+	/**
 	 * For screen reader users for providing additional information by extending text content in normal label. Used in e. g. input with language options for providing info about currently selected language.
 	 */
 	@Input() ariaLabel: string;
 
-	/* *
+	/**
 	 * Unique id for input.
 	 */
 	@Input() id: string;
 
-	/* *
+	/**
 	 * Option for disabling the input.
 	 */
 	@Input() disabled: boolean = false;
 
-	/* *
+	/**
 	 * Text to indicate compulsory.
 	 */
 	@Input() requiredText: string | undefined;
 
-	/* *
+	/**
 	 * Help text, aligned underneath the input.
 	 */
 	@Input() helpText: string | undefined;
 
-	/* *
+	/**
 	 * Error messages shown when form control validators are invalid
 	 */
 
 	@Input() errorMsg: FudisFormErrors | undefined;
 
-	/* *
+	/**
 	 * Set input's visual style and attributes as invalid. Does not override if control.invalid is true.
 	 */
 	@Input() invalidState: boolean = false;
 
-	/* *
+	/**
 	 * To ignore initial check if input has both requiredText and control Validators.required
 	 */
 	@Input() ignoreRequiredCheck: boolean = false;
 
-	/* *
+	/**
 	 * To listen for input's blur event.
 	 */
 	@Output() handleBlur: EventEmitter<Event> = new EventEmitter<Event>();
 
-	/* *
+	/**
 	 * Internal id to generate unique id
 	 */
 	protected _id: string;
