811ec1f62f2ff2cef9d0c09e082ac470e7f99f0a
riinakuukso <riina.kuukso@funidata.fi>
2023-07-11 15:53:06 +0300

    DS-102: Clean and add unit test

--- ngx-fudis/projects/ngx-fudis/src/lib/components/form/text-area/text-area.component.spec.ts
+++ ngx-fudis/projects/ngx-fudis/src/lib/components/form/text-area/text-area.component.spec.ts
@@ -1,7 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { ReactiveFormsModule, FormControl } from '@angular/forms';
+import { ReactiveFormsModule, FormControl, Validators } from '@angular/forms';
 import { MockComponent } from 'ng-mocks';
-import { ChangeDetectionStrategy } from '@angular/core';
 import { TextAreaComponent } from './text-area.component';
 import { LabelComponent } from '../label/label.component';
 import { GuidanceComponent } from '../guidance/guidance.component';
@@ -16,24 +15,37 @@ describe('TextAreaComponent', () => {
 		await TestBed.configureTestingModule({
 			declarations: [TextAreaComponent, MockComponent(LabelComponent), MockComponent(GuidanceComponent)],
 			imports: [ReactiveFormsModule],
-		})
-			.overrideComponent(TextAreaComponent, {
-				set: { changeDetection: ChangeDetectionStrategy.Default },
-			})
-			.compileComponents();
+		}).compileComponents();
 	});
 
 	beforeEach(() => {
 		fixture = TestBed.createComponent(TextAreaComponent);
 		component = fixture.componentInstance;
-		component.label = 'Label for testing purposes';
 		component.control = textAreaControl;
-		component.id = 'id-for-testing';
-
 		fixture.detectChanges();
 	});
 
 	it('should create', () => {
 		expect(component).toBeTruthy();
+	});
+
+	describe('MinLength', () => {
+		it('should set control as invalid according to given minLength validator value', () => {
+			component.control = new FormControl('', [Validators.minLength(10)]);
+			component.control.patchValue('too short');
+
+			expect(component.control.value).toEqual('too short');
+			expect(component.control.invalid).toBeTruthy();
+		});
+	});
+
+	describe('Required', () => {
+		it('should set control as invalid if required text area is touched and empty', () => {
+			component.control = new FormControl('', Validators.required);
+			component.control.patchValue('');
+
+			expect(component.control.value).toEqual('');
+			expect(component.control.invalid).toBeTruthy();
+		});
 	});
 });
