3daa33f831730481c83b71f6829c9570f2a9f428
riinakuukso <riina.kuukso@funidata.fi>
2023-07-20 11:53:28 +0300

    DS-93: Add stories, unit tests and readme file

--- ngx-fudis/projects/ngx-fudis/src/lib/components/badge/badge.stories.ts
+++ ngx-fudis/projects/ngx-fudis/src/lib/components/badge/badge.stories.ts
@@ -1,14 +1,15 @@
-import { StoryFn, Meta, moduleMetadata } from '@storybook/angular';
+import { StoryFn, Meta } from '@storybook/angular';
 import { BadgeComponent } from './badge.component';
+import readme from './readme.mdx';
 
 export default {
 	title: 'Components/Badge',
 	component: BadgeComponent,
-	decorators: [
-		moduleMetadata({
-			imports: [],
-		}),
-	],
+	parameters: {
+		docs: {
+			page: readme,
+		},
+	},
 	argTypes: {},
 } as Meta;
 
@@ -16,8 +17,22 @@ const Template: StoryFn<BadgeComponent> = (args: BadgeComponent) => ({
 	props: args,
 });
 
+const html = String.raw;
+
 export const Badge = Template.bind({});
 Badge.args = {
 	variant: 'primary',
 	content: 'Badge',
 };
+
+export const AllVariants: StoryFn = () => ({
+	template: html`
+		<fudis-grid rowGap="sm">
+			<fudis-badge [variant]="'accent'" [content]="'accent'"></fudis-badge>
+			<fudis-badge [variant]="'danger'" [content]="'danger'"></fudis-badge>
+			<fudis-badge [variant]="'primary'" [content]="'primary'"></fudis-badge>
+			<fudis-badge [variant]="'secondary'" [content]="'secondary'"></fudis-badge>
+			<fudis-badge [variant]="'success'" [content]="'success'"></fudis-badge>
+		</fudis-grid>
+	`,
+});
