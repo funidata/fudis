da0ea28e11f53cc4880f32cf9911b1322d87de39
videoeero <eero.suvanto@gmail.com>
2023-08-18 16:26:43 +0300

    POC validators to exported package

--- ngx-fudis/projects/ngx-fudis/src/public-api.ts
+++ ngx-fudis/projects/ngx-fudis/src/public-api.ts
@@ -67,3 +67,5 @@ export { TextAreaComponent } from './lib/components/form/text-area/text-area.com
 export { TextInputComponent } from './lib/components/form/text-input/text-input.component';
 // export { TextSpacingComponent } from './lib/components/typography/text-spacing/text-spacing.component';
 export { TooltipDirective } from './lib/directives/tooltip/tooltip.directive';
+
+export { FormGroupValidators } from './lib/utilities/form/validators';
