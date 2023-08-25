709c83366548c5dc285dcf2d7749e6f4a27656ab
videoeero <eero.suvanto@gmail.com>
2023-08-25 14:41:04 +0300

    DS-147: alert service basic functions

--- ngx-fudis/projects/ngx-fudis/src/lib/components/alert/alert-service/alert.service.ts
+++ ngx-fudis/projects/ngx-fudis/src/lib/components/alert/alert-service/alert.service.ts
@@ -1,8 +1,40 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Signal, signal } from '@angular/core';
+import { FudisAlert } from '../../../types/miscellaneous';
+import { FudisIdService } from '../../../utilities/id-service.service';
 
 @Injectable({
 	providedIn: 'root',
 })
 export class FudisAlertService {
-	constructor() {}
+	constructor(private _idService: FudisIdService) {}
+
+	private _alerts = signal<FudisAlert[]>([]);
+
+	public addAlert(newAlert: FudisAlert): void {
+		const alertToAdd = newAlert;
+
+		const currentAlerts: FudisAlert[] = this._alerts();
+
+		currentAlerts.push(alertToAdd);
+
+		this._alerts.set(currentAlerts);
+
+		console.log(alertToAdd);
+	}
+
+	public dismissAlert(id: string): void {
+		const currentAlerts: FudisAlert[] = this._alerts();
+
+		const filteredAlerts = currentAlerts.filter((alert) => alert.id !== id);
+
+		this._alerts.set(filteredAlerts);
+	}
+
+	public dismissAll(): void {
+		this._alerts.set([]);
+	}
+
+	public getAlerts(): Signal<FudisAlert[]> {
+		return this._alerts.asReadonly();
+	}
 }
