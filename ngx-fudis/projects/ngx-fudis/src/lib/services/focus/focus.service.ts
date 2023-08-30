8c37012a574f5da1e88699ed3e68f43947651f69
videoeero <eero.suvanto@gmail.com>
2023-08-30 13:23:39 +0300

    DS-147: focus service ver1

--- ngx-fudis/projects/ngx-fudis/src/lib/services/focus/focus.service.ts
+++ ngx-fudis/projects/ngx-fudis/src/lib/services/focus/focus.service.ts
@@ -1,33 +1,36 @@
-import { Injectable } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Inject, Injectable } from '@angular/core';
 
 @Injectable({
 	providedIn: 'root',
 })
 export class FudisFocusService {
-	private _focusTarget: HTMLElement | null = null;
+	constructor(@Inject(DOCUMENT) private _document: Document) {}
 
-	private _focusCloseButton: HTMLElement | null = null;
+	private _focusTarget: HTMLElement | null = null;
 
 	public getFocusTarget(): HTMLElement | null {
 		return this._focusTarget;
 	}
 
-	public getAlertCloseButtonTarget(): HTMLButtonElement | null {
-		if (!this._focusCloseButton) {
-			return null;
-		}
-		return this._focusCloseButton as HTMLButtonElement;
-	}
-
 	public setFocusTarget(target: EventTarget | null): void {
 		if (target) {
 			const element = target as HTMLElement;
-			if (element.classList.contains('fudis-alert__close')) {
-				this._focusCloseButton = element;
-			} else {
+			if (!element.classList.contains('fudis-alert__close')) {
 				this._focusTarget = element;
 			}
-			console.log(this._focusTarget);
 		}
+	}
+
+	public focusToElementById(id: string, tryCounter: number = 100): void {
+		setTimeout(() => {
+			const element = this._document.getElementById(id);
+
+			if (element) {
+				element.focus();
+			} else if (tryCounter > 0) {
+				this.focusToElementById(id, tryCounter - 1);
+			}
+		}, 50);
 	}
 }
