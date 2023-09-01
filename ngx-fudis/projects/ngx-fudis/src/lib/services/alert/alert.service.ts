d307f99c136fdebe4b511ad3fc93f4fa4a758a44
videoeero <eero.suvanto@gmail.com>
2023-09-01 12:47:39 +0300

    DS-147: alert service docs

--- ngx-fudis/projects/ngx-fudis/src/lib/services/alert/alert.service.ts
+++ ngx-fudis/projects/ngx-fudis/src/lib/services/alert/alert.service.ts
@@ -10,6 +10,9 @@ export class FudisAlertService {
 
 	private _alerts = signal<FudisAlertElement[]>([]);
 
+	/**
+	 * To add new alert, which will be rendered then by Alert Group component. Note 'id' doesn't necessarily need to be unique. It could also e. g. 'error-in-form-add-new-user', so all these error could be dismissed with dismissAlert() using this 'id'.
+	 */
 	public addAlert(newAlert: FudisAlert): void {
 		const alertToAdd = newAlert;
 
@@ -22,6 +25,9 @@ export class FudisAlertService {
 		this._alerts.set(currentAlerts);
 	}
 
+	/**
+	 * To dismiss alert by 'id' provided in 'addAlert()'. It will dismiss all alerts matching the 'id'.
+	 */
 	public dismissAlert(id: string): void {
 		const currentAlerts: FudisAlertElement[] = this._alerts();
 
@@ -30,6 +36,9 @@ export class FudisAlertService {
 		this._alerts.set(filteredAlerts);
 	}
 
+	/**
+	 * Dismisses only one alert from alert's close button click in the UI
+	 */
 	public dismissAlertFromButton(id: string): void {
 		const currentAlerts: FudisAlertElement[] = this._alerts();
 
@@ -38,18 +47,27 @@ export class FudisAlertService {
 		this._alerts.set(filteredAlerts);
 	}
 
+	/**
+	 * Dismiss all alerts
+	 */
 	public dismissAll(): void {
 		this._alerts.set([]);
 	}
 
+	/**
+	 * Get Signal containing alerts array
+	 */
 	public getAlertsSignal(): Signal<FudisAlertElement[]> {
 		return this._alerts.asReadonly();
 	}
 
-	public updateAlertLinkFocusState(id: string): void {
+	/**
+	 * Set 'initialFocus' attribute of alert to false, so that if same alert with link is rendered again, the initial focus will not jump there anymore.
+	 */
+	public updateAlertLinkFocusState(htmlId: string): void {
 		const currentAlerts: FudisAlertElement[] = this._alerts();
 
-		const index = currentAlerts.findIndex((alert) => alert.htmlId === id);
+		const index = currentAlerts.findIndex((alert) => alert.htmlId === htmlId);
 
 		if (index !== -1) {
 			currentAlerts[index] = { ...currentAlerts[index], initialFocus: false };
