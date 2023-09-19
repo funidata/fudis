7977335ceaf53ac3810114d0a5187c266d918c44
riinakuukso <riina.kuukso@funidata.fi>
2023-09-19 12:24:34 +0300

    DS-200: Update unit tests and documentation

--- ngx-fudis/projects/ngx-fudis/src/lib/components/typography/body-text/body-text.component.spec.ts
+++ ngx-fudis/projects/ngx-fudis/src/lib/components/typography/body-text/body-text.component.spec.ts
@@ -1,8 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ChangeDetectionStrategy } from '@angular/core';
 import { BodyTextComponent } from './body-text.component';
-import { FudisBodyText } from '../../../types/typography';
-import { FudisSpacing } from '../../../types/miscellaneous';
+import { getElement, sortClasses } from '../../../../../utilities/tests/utilities';
 
 describe('BodyTextComponent', () => {
 	let component: BodyTextComponent;
@@ -24,46 +23,73 @@ describe('BodyTextComponent', () => {
 		fixture.detectChanges();
 	});
 
-	function assertBodyTextHasClasses(classes: string): void {
-		const childSpan = fixture.nativeElement.childNodes;
-		const componentClasses = childSpan[0].className.split(' ').sort();
-
-		expect(componentClasses).toEqual(classes.split(' ').sort());
-	}
-
-	function bodyTextSizes(size: FudisBodyText): void {
-		component.size = size;
-		fixture.detectChanges();
-		assertBodyTextHasClasses(`fudis-body-text fudis-body-text__${size} fudis-mb-none fudis-body-text__default`);
-	}
-
-	function marginBottomSizes(marginBottom: FudisSpacing): void {
-		component.marginBottom = marginBottom;
-
-		fixture.detectChanges();
-		assertBodyTextHasClasses(
-			`fudis-body-text fudis-body-text__default fudis-body-text__md-regular fudis-mb-${marginBottom}`
-		);
-	}
-
 	describe('CSS classes', () => {
-		it('should change the class according to the given body text size', () => {
-			bodyTextSizes('lg-regular');
-			bodyTextSizes('md-regular');
-			bodyTextSizes('sm-regular');
-			bodyTextSizes('lg-light');
-			bodyTextSizes('md-light');
+		it('should change CSS classes according to the given body-text size', () => {
+			const element = getElement(fixture, '.fudis-body-text');
+
+			expect(sortClasses(element.className)).toEqual(
+				sortClasses('fudis-body-text fudis-body-text__default fudis-body-text__left fudis-body-text__md-regular')
+			);
+
+			component.size = 'lg-light';
+			fixture.detectChanges();
+
+			expect(sortClasses(element.className)).toEqual(
+				sortClasses('fudis-body-text fudis-body-text__default fudis-body-text__left fudis-body-text__lg-light')
+			);
+
+			component.size = 'lg-regular';
+			fixture.detectChanges();
+
+			expect(sortClasses(element.className)).toEqual(
+				sortClasses('fudis-body-text fudis-body-text__default fudis-body-text__left fudis-body-text__lg-regular')
+			);
+
+			component.size = 'md-light';
+			fixture.detectChanges();
+
+			expect(sortClasses(element.className)).toEqual(
+				sortClasses('fudis-body-text fudis-body-text__default fudis-body-text__left fudis-body-text__md-light')
+			);
+
+			component.size = 'sm-regular';
+			fixture.detectChanges();
+
+			expect(sortClasses(element.className)).toEqual(
+				sortClasses('fudis-body-text fudis-body-text__default fudis-body-text__left fudis-body-text__sm-regular')
+			);
 		});
 
-		it('should change the class according to the given margin bottom value', () => {
-			marginBottomSizes('xxl');
-			marginBottomSizes('xl');
-			marginBottomSizes('md');
-			marginBottomSizes('lg');
-			marginBottomSizes('sm');
-			marginBottomSizes('xs');
-			marginBottomSizes('xxs');
-			marginBottomSizes('none');
+		it('should change CSS classes according to given body-text color', () => {
+			const element = getElement(fixture, '.fudis-body-text');
+			component.color = 'white';
+			fixture.detectChanges();
+
+			expect(sortClasses(element.className)).toEqual(
+				sortClasses('fudis-body-text fudis-body-text__white fudis-body-text__left fudis-body-text__md-regular')
+			);
+		});
+
+		it('should change CSS classes according to given body-text align', () => {
+			const element = getElement(fixture, '.fudis-body-text');
+
+			expect(sortClasses(element.className)).toEqual(
+				sortClasses('fudis-body-text fudis-body-text__default fudis-body-text__left fudis-body-text__md-regular')
+			);
+
+			component.align = 'center';
+			fixture.detectChanges();
+
+			expect(sortClasses(element.className)).toEqual(
+				sortClasses('fudis-body-text fudis-body-text__default fudis-body-text__center fudis-body-text__md-regular')
+			);
+
+			component.align = 'right';
+			fixture.detectChanges();
+
+			expect(sortClasses(element.className)).toEqual(
+				sortClasses('fudis-body-text fudis-body-text__default fudis-body-text__right fudis-body-text__md-regular')
+			);
 		});
 	});
 });
