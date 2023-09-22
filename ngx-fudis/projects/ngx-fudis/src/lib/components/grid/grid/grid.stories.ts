f9e9fd2b9b6dc473f90dcf4b3eccca09aede02d2
riinakuukso <riina.kuukso@funidata.fi>
2023-09-22 12:03:46 +0300

    DS-96: Add readme for Grid

--- ngx-fudis/projects/ngx-fudis/src/lib/components/grid/grid/grid.stories.ts
+++ ngx-fudis/projects/ngx-fudis/src/lib/components/grid/grid/grid.stories.ts
@@ -1,6 +1,6 @@
 import { StoryFn, Meta, componentWrapperDecorator } from '@storybook/angular';
-
 import { GridComponent } from './grid.component';
+import readme from './readme.mdx';
 
 const html = String.raw;
 
@@ -52,6 +52,9 @@ export default {
 	],
 
 	parameters: {
+		docs: {
+			page: readme,
+		},
 		controls: {
 			exclude: commonExclude,
 		},
@@ -191,7 +194,7 @@ const ResponsiveColumnsTemplate: StoryFn<GridComponent> = (args: GridComponent) 
 				until hitting 'xxl' breakpoint.</fudis-body-text
 			>
 			<fudis-body-text class="text-margin"
-				>Using FudisGridService's 'setGridDefaultColumns()' you can define default values applied to all your
+				>Using FudisGridService's 'setGridDefaultValues()' you can define default values applied to all your
 				Grids.</fudis-body-text
 			>
 			<fudis-body-text class="text-margin"
