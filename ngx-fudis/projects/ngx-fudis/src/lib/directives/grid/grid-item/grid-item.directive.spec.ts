6c5912fe80ea78f00d2758ea5d91e6a39827edee
riinakuukso <riina.kuukso@funidata.fi>
2023-09-25 14:05:51 +0300

    DS-96: Basic unit tests for grid-item directive

--- ngx-fudis/projects/ngx-fudis/src/lib/directives/grid/grid-item/grid-item.directive.spec.ts
+++ ngx-fudis/projects/ngx-fudis/src/lib/directives/grid/grid-item/grid-item.directive.spec.ts
@@ -1,18 +1,135 @@
-// import { ElementRef } from '@angular/core';
-// import { FudisGridService } from '../grid-service/grid.service';
-// import { GridItemDirective } from './grid-item.directive';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Component } from '@angular/core';
+import { MockComponent } from 'ng-mocks';
+import { By } from '@angular/platform-browser';
+import { GridItemDirective } from './grid-item.directive';
+import { FudisGridService } from '../../../services/grid/grid.service';
+import { FudisBreakpointService } from '../../../services/breakpoint/breakpoint.service';
+import { GridComponent } from '../../../components/grid/grid/grid.component';
+import { HeadingComponent } from '../../../components/typography/heading/heading.component';
+import { ButtonComponent } from '../../../components/button/button.component';
+import { FudisGridItemAlignment } from '../../../types/grid';
+import { FudisBreakpointStyleResponsive } from '../../../types/breakpoints';
+import { BodyTextComponent } from '../../../components/typography/body-text/body-text.component';
 
-// const fakeFudisGridService = jasmine.createSpyObj<FudisGridService>('FudisGridService', {
-// 	getBreakpointState: null,
-// });
+@Component({
+	selector: 'fudis-mock-grid-item-directive',
+	template: `<fudis-grid [columns]="4">
+		<fudis-heading [level]="3">I am test heading</fudis-heading>
+		<fudis-body-text fudisGridItem [columns]="columns">
+			Paragraph text for testing grid item functionalities. This is so much fun!
+		</fudis-body-text>
+		<fudis-button fudisGridItem [alignY]="alignY" [alignX]="alignX" [label]="'Test button'" />
+	</fudis-grid>`,
+})
+class HostComponent {
+	columns: string | FudisBreakpointStyleResponsive = '2';
 
-// const fakeGridElement = jasmine.createSpyObj<ElementRef>('GridElement', {
-// 	nativeElement: null,
-// });
+	alignY: FudisGridItemAlignment = 'stretch';
 
-// describe('GridItemDirective', () => {
-// 	it('should create an instance', () => {
-// 		const directive = new GridItemDirective(fakeGridElement, fakeFudisGridService);
-// 		expect(directive).toBeTruthy();
-// 	});
-// });
+	alignX: FudisGridItemAlignment = 'stretch';
+}
+
+describe('GridItemDirective', () => {
+	let component: HostComponent;
+	let fixture: ComponentFixture<HostComponent>;
+
+	beforeEach(async () => {
+		await TestBed.configureTestingModule({
+			declarations: [
+				HostComponent,
+				GridItemDirective,
+				GridComponent,
+				MockComponent(HeadingComponent),
+				BodyTextComponent,
+				MockComponent(ButtonComponent),
+			],
+			providers: [FudisGridService, FudisBreakpointService],
+		}).compileComponents();
+	});
+
+	beforeEach(() => {
+		fixture = TestBed.createComponent(HostComponent);
+		component = fixture.componentInstance;
+	});
+
+	function getGridItemDirective() {
+		return fixture.debugElement.queryAll(By.directive(GridItemDirective));
+	}
+
+	function getAlignXAttribute(index: number) {
+		return getGridItemDirective()[index].nativeElement.style.justifySelf;
+	}
+
+	function getAlignYAttribute(index: number) {
+		return getGridItemDirective()[index].nativeElement.style.alignSelf;
+	}
+
+	function getColumnsAttribute(index: number) {
+		return getGridItemDirective()[index].nativeElement.style.gridColumn;
+	}
+
+	function getDefaultCSSClass(index: number) {
+		return getGridItemDirective()[index].nativeElement.classList;
+	}
+
+	describe('Component creation', () => {
+		it('should create mock component', () => {
+			expect(component).toBeTruthy();
+		});
+	});
+
+	describe('GridItem Directive', () => {
+		it('should find elements with fudisGridItem directive', () => {
+			const elements = getGridItemDirective();
+
+			expect(elements.length).toEqual(2);
+		});
+
+		it('should have respective grid-item CSS class present', () => {
+			fixture.detectChanges();
+
+			expect(getDefaultCSSClass(0)).toContain('fudis-grid-item');
+			expect(getDefaultCSSClass(1)).toContain('fudis-grid-item');
+		});
+
+		it('should convert alignX and alignY attributes to style properties', () => {
+			fixture.detectChanges();
+
+			expect(getAlignXAttribute(1)).toBe('stretch');
+			expect(getAlignYAttribute(1)).toBe('stretch');
+
+			component.alignY = 'end';
+			component.alignX = 'end';
+			fixture.detectChanges();
+
+			expect(getAlignXAttribute(1)).toBe('end');
+			expect(getAlignYAttribute(1)).toBe('end');
+
+			component.alignY = 'start';
+			component.alignX = 'start';
+			fixture.detectChanges();
+
+			expect(getAlignXAttribute(1)).toBe('start');
+			expect(getAlignYAttribute(1)).toBe('start');
+
+			component.alignY = 'center';
+			component.alignX = 'center';
+			fixture.detectChanges();
+
+			expect(getAlignXAttribute(1)).toBe('center');
+			expect(getAlignYAttribute(1)).toBe('center');
+		});
+
+		it('should convert columns attribute to grid-column properties', () => {
+			fixture.detectChanges();
+
+			expect(getColumnsAttribute(0)).toBe('2 / auto');
+
+			component.columns = '6';
+			fixture.detectChanges();
+
+			expect(getColumnsAttribute(0)).toBe('6 / auto');
+		});
+	});
+});
