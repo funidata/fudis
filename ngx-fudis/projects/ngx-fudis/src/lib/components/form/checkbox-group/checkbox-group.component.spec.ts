21336a9d4ee23503dfea2a70dbea3b10ee96534a
Anna Salo <anna.salo@nitor.com>
2023-09-28 16:36:23 +0300

    DS-140: Add component declarations and ReactiveFormsModule import for checkbox-group test

--- ngx-fudis/projects/ngx-fudis/src/lib/components/form/checkbox-group/checkbox-group.component.spec.ts
+++ ngx-fudis/projects/ngx-fudis/src/lib/components/form/checkbox-group/checkbox-group.component.spec.ts
@@ -1,6 +1,10 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { MockComponent } from 'ng-mocks';
+import { ReactiveFormsModule } from '@angular/forms';
 import { CheckboxGroupComponent } from './checkbox-group.component';
+import { FieldSetComponent } from '../fieldset/fieldset.component';
+import { CheckboxComponent } from './checkbox/checkbox.component';
 
 describe('CheckboxGroupComponent', () => {
 	let component: CheckboxGroupComponent;
@@ -8,7 +12,8 @@ describe('CheckboxGroupComponent', () => {
 
 	beforeEach(() => {
 		TestBed.configureTestingModule({
-			declarations: [CheckboxGroupComponent],
+			declarations: [CheckboxComponent, CheckboxGroupComponent, MockComponent(FieldSetComponent)],
+			imports: [ReactiveFormsModule],
 		});
 		fixture = TestBed.createComponent(CheckboxGroupComponent);
 		component = fixture.componentInstance;
