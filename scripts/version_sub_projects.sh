7308e75837424e74e1745b238f44f47747263912
Joonas Häkkinen <joonas.hakkinen@funidata.fi>
2023-10-18 16:07:43 +0300

    Configure npm version command to work on all sub-projects

---
+++ scripts/version_sub_projects.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Sync sub-projects to given version number.
+#
+# Pass current version as first argument.
+# This script is meant to be used along with `npm version` in project root.
+
+# List projects to update here by their folder name.
+projects=(
+  "ngx-fudis/projects"
+  "ngx-fudis/projects/ngx-fudis"
+)
+
+new_version=$1
+repo_root=$(pwd)
+
+function update_sub_project_version {
+  project_name=$1
+  dir="${repo_root}/${project_name}"
+
+  cd $dir
+  echo -n "${project_name}: "
+
+  # Don't commit, just bump to given version.
+  npm version --git-tag-version false $new_version
+  # Running `npm version` in root will not stage extra files, so add them here.
+  git add "${dir}/package.json" "${dir}/package-lock.json"
+}
+
+for project in ${projects[@]}; do
+  update_sub_project_version $project
+done
