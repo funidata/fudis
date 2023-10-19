ce7296b276d9743be2dfd11825064ac4d56c505a
Joonas Häkkinen <joonas.hakkinen@funidata.fi>
2023-10-19 15:20:10 +0300

    Show current package version in Storybook in dev env without changing version label in builds

--- ngx-fudis/.storybook/getVersion.js
+++ ngx-fudis/.storybook/getVersion.js
@@ -1,25 +1,27 @@
 import { parseSemVer, isValidSemVer } from 'semver-parser';
 import pkg from '../package.json';
 
+const packageVersion = pkg.version;
+
 // The path, i.e., the branch name or tag under which we are deploying.
 const tagOrBranchName = process.env.VERSION || '';
 
-const defaultVersion = {
-	major: 0,
-	minor: 0,
-	patch: 0,
-	postfix: '',
-};
-
 const getVersion = () => {
-	if (!isValidSemVer(pkg.version)) {
+	// Use just current version from package.json in dev env.
+	if (process.env.NODE_ENV === 'development') {
+		return packageVersion;
+	}
+
+	// If branch name is passed, prefix it with current version.
+	if (!isValidSemVer(tagOrBranchName)) {
 		return {
-			...defaultVersion,
+			...parseSemVer(packageVersion),
 			postfix: tagOrBranchName,
 		};
 	}
 
-	return parseSemVer(pkg.version);
+	// Otherwise use given version from env.
+	return parseSemVer(tagOrBranchName);
 };
 
 export default getVersion;
