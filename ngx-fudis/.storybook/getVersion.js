4e503c949c71ba28f3b782ecdae1e74f4618b9b8
riinakuukso <riina.kuukso@funidata.fi>
2023-10-20 14:37:15 +0300

    Fix Storybook version label for PR branches

--- ngx-fudis/.storybook/getVersion.js
+++ ngx-fudis/.storybook/getVersion.js
@@ -7,16 +7,12 @@ const packageVersion = pkg.version;
 const tagOrBranchName = process.env.VERSION || '';
 
 const getVersion = () => {
-	// Use just current version from package.json in dev env.
-	if (process.env.NODE_ENV === 'development') {
-		return packageVersion;
-	}
 
 	// If branch name is passed, prefix it with current version.
 	if (!isValidSemVer(tagOrBranchName)) {
 		return {
 			...parseSemVer(packageVersion),
-			postfix: tagOrBranchName,
+			build: tagOrBranchName,
 		};
 	}
 
