8c67c495be83ff8fcd0bc14a0fde2ed8ca9fef39
videoeero <eero.suvanto@gmail.com>
2023-10-23 18:03:54 +0300

    fix: add initial formGroup to test with

--- ngx-fudis/projects/ngx-fudis/src/lib/components/form/checkbox-group/checkbox-group.component.spec.ts
+++ ngx-fudis/projects/ngx-fudis/src/lib/components/form/checkbox-group/checkbox-group.component.spec.ts
@@ -1,10 +1,24 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { MockComponent } from 'ng-mocks';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { BehaviorSubject } from 'rxjs';
 import { CheckboxGroupComponent } from './checkbox-group.component';
 import { FieldSetComponent } from '../fieldset/fieldset.component';
 import { CheckboxComponent } from './checkbox/checkbox.component';
+import { FudisCheckboxGroupFormGroup } from '../../../types/forms';
+import { FudisGroupValidator } from '../../../utilities/form/validators';
+
+const testFormGroup = new FormGroup<FudisCheckboxGroupFormGroup>(
+	{
+		apple: new FormControl<boolean | null | undefined>(null),
+		fairTradeBanana: new FormControl<boolean | null | undefined>(null),
+		pear: new FormControl<boolean | null | undefined>(null),
+		pineapple: new FormControl<boolean | null | undefined>(null),
+		orange: new FormControl<boolean | null | undefined>(null),
+	},
+	[FudisGroupValidator.atLeastOneRequired(new BehaviorSubject('No fruit picked! :('))]
+);
 
 describe('CheckboxGroupComponent', () => {
 	let component: CheckboxGroupComponent;
@@ -17,6 +31,7 @@ describe('CheckboxGroupComponent', () => {
 		});
 		fixture = TestBed.createComponent(CheckboxGroupComponent);
 		component = fixture.componentInstance;
+		component.formGroup = testFormGroup;
 		fixture.detectChanges();
 	});
 
