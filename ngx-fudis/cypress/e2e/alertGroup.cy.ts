7d9ae8ecf3f96693e54d5401722afd4bdf412e6b
Eero Suvanto <eero.suvanto@gmail.com>
2023-11-10 11:55:33 +0200

    chore: cypress and karma configs

--- ngx-fudis/cypress/e2e/alertGroup.cy.ts
+++ ngx-fudis/cypress/e2e/alertGroup.cy.ts
@@ -1,3 +1,4 @@
+/* eslint-disable cypress/no-unnecessary-waiting */
 describe('Visuals', () => {
 	it('should compare screenshot of the entire page', () => {
 		cy.visit('/iframe.html?args=&id=components-alert-group--example&viewMode=story');
