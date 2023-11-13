f5e32014e59223ea5791de158849707748c0917f
riinakuukso <riina.kuukso@funidata.fi>
2023-11-13 15:05:25 +0200

    DS-223: Update dialog base images

--- ngx-fudis/cypress/e2e/dialog.cy.ts
+++ ngx-fudis/cypress/e2e/dialog.cy.ts
@@ -1,14 +1,25 @@
 import { fudisScreenshots } from './utilities/utility';
 
 describe('Dialog screenshot', () => {
-	it('should match with previous screenshot', () => {
+	it('should match with previous screenshot om DESKTOP', () => {
 		cy.visit('/iframe.html?args=&id=components-dialog--dialog&viewMode=story');
 		cy.get('#fudis-button-1').click();
-		fudisScreenshots('both', '1', 500);
+		fudisScreenshots('desktop', '1', 500);
 		cy.get('#fudis-dialog-1-close').click();
 		cy.get('#fudis-button-2').click();
-		fudisScreenshots('both', '2', 500);
+		fudisScreenshots('desktop', '2', 500);
 		cy.get('fudis-dialog-content').scrollTo('bottom');
-		fudisScreenshots('both', '3', 500);
+		fudisScreenshots('desktop', '3', 500);
+	});
+
+	it('should match with previous screenshot on MOBILE', () => {
+		cy.visit('/iframe.html?args=&id=components-dialog--dialog&viewMode=story');
+		cy.get('#fudis-button-1').click();
+		fudisScreenshots('mobile', '1', 500);
+		cy.get('#fudis-dialog-1-close').click();
+		cy.get('#fudis-button-2').click();
+		fudisScreenshots('mobile', '2', 500);
+		cy.get('fudis-dialog-content').scrollTo('bottom');
+		fudisScreenshots('mobile', '3', 500);
 	});
 });
