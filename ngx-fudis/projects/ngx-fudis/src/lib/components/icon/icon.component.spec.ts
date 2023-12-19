a002c870e56f39318012ee0ae4355d8da344de67
Eero Suvanto <eero.suvanto@gmail.com>
2023-12-19 13:37:57 +0200

    chore: adjust icon types and tests

--- ngx-fudis/projects/ngx-fudis/src/lib/components/icon/icon.component.spec.ts
+++ ngx-fudis/projects/ngx-fudis/src/lib/components/icon/icon.component.spec.ts
@@ -2,7 +2,105 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ChangeDetectionStrategy } from '@angular/core';
 import { By } from '@angular/platform-browser';
 import { IconComponent } from './icon.component';
-import { FudisIconColor, FudisIconRotate } from '../../types/icons';
+import { FudisIconColor, FudisIconRotate, fudisIconColorArray, fudisIconArray } from '../../types/icons';
+
+/**
+ * To be used as benchmark so that the list in types folder keeps in sync with ones in the tests
+ */
+const arrayForComparing: string[] = [
+	'achievement',
+	'achievement-small',
+	'alert',
+	'alert-fill',
+	'alert-small',
+	'archive',
+	'arrow-big',
+	'arrow-dashed',
+	'arrow-solid',
+	'back',
+	'brochure',
+	'bullets',
+	'calendar',
+	'change-log',
+	'check',
+	'check-indeterminate-small',
+	'check-small',
+	'checkmark-circle',
+	'chevron',
+	'chevron-ring',
+	'chevron-ring-fill',
+	'clock',
+	'close',
+	'close-big',
+	'code',
+	'decrease-indent',
+	'delete',
+	'dot',
+	'double-chevron-left',
+	'double-chevron-right',
+	'edit',
+	'editor',
+	'exclamation-mark-circle',
+	'exclamation-mark-circle-fill',
+	'exclamation-mark-small',
+	'eye',
+	'eye-blind',
+	'fail',
+	'hourglass',
+	'increase-indent',
+	'info',
+	'info-circle',
+	'info-circle-fill',
+	'info-small',
+	'junction',
+	'link',
+	'list-add',
+	'list-minus',
+	'lock',
+	'lock-open',
+	'magic-wand',
+	'mail',
+	'menu',
+	'message',
+	'minus',
+	'minus-ring-fill',
+	'new-tab',
+	'notebook',
+	'notification',
+	'numbering',
+	'pdf',
+	'people',
+	'person',
+	'person-small',
+	'picker',
+	'pin-small',
+	'place',
+	'place-ring-fill',
+	'plus',
+	'print',
+	'question-mark',
+	'question-mark-small',
+	'required',
+	'ring-close',
+	'ring-close-fill',
+	'ring-plus',
+	'ring-plus-fill',
+	'rosette',
+	'rule',
+	'search',
+	'seats',
+	'settings',
+	'shopping-cart',
+	'sorter',
+	'star',
+	'switch',
+	'three-dots',
+	'three-dots-small',
+	'waiting-approval',
+	'waiting-decline',
+	'zoom-in',
+	'zoom-out',
+];
 
 describe('IconComponent', () => {
 	let component: IconComponent;
@@ -52,19 +150,19 @@ describe('IconComponent', () => {
 		expect(component).toBeTruthy();
 	});
 
+	it('type name array should match with test array', () => {
+		expect(fudisIconArray).toEqual(arrayForComparing);
+	});
+
 	describe('CSS classes', () => {
 		it('should have default classes when icon is set', () => {
 			assertIconHasClasses(['fudis-icon', 'fudis-icon-color__default', 'fudis-icon-rotate__none', 'fudis-icon__lg']);
 		});
 
 		it('should change color class according to given color Input value', () => {
-			iconColorCheck('attention');
-			iconColorCheck('danger');
-			iconColorCheck('default');
-			iconColorCheck('light');
-			iconColorCheck('primary');
-			iconColorCheck('success');
-			iconColorCheck('white');
+			fudisIconColorArray.forEach((color) => {
+				iconColorCheck(color);
+			});
 		});
 
 		it('should change rotate class according to given rotate Input value', () => {
@@ -77,14 +175,16 @@ describe('IconComponent', () => {
 
 	describe('Icon', () => {
 		it('should be displayed according to given icon Input value', () => {
-			component.icon = 'alert';
-			component.ngOnChanges();
-			fixture.detectChanges();
-			const svgElem = fixture.debugElement.query(By.css('svg'));
-			const useElem = svgElem.nativeElement.querySelector('use');
+			fudisIconArray.forEach((iconName) => {
+				component.icon = iconName;
+				component.ngOnChanges();
+				fixture.detectChanges();
+				const svgElem = fixture.debugElement.query(By.css('svg'));
+				const useElem = svgElem.nativeElement.querySelector('use');
 
-			expect(useElem.getAttribute('href')).toBeTruthy();
-			expect(useElem.getAttribute('href')).toEqual('alert.svg#alert');
+				expect(useElem.getAttribute('href')).toBeTruthy();
+				expect(useElem.getAttribute('href')).toEqual(`${iconName}.svg#${iconName}`);
+			});
 		});
 	});
 });
