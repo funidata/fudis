d55b10c4f7173581e90c3a0a335616e9fdf808a6
riinakuukso <riina.kuukso@funidata.fi>
2024-01-03 12:56:32 +0200

    DS-189: Update Error Summary Service unit tests

--- ngx-fudis/projects/ngx-fudis/src/lib/services/form/error-summary/error-summary.service.spec.ts
+++ ngx-fudis/projects/ngx-fudis/src/lib/services/form/error-summary/error-summary.service.spec.ts
@@ -1,30 +1,47 @@
 import { TestBed } from '@angular/core/testing';
+import { of } from 'rxjs';
 import { FudisErrorSummaryService } from './error-summary.service';
 import { FudisInternalErrorSummaryService } from './internal-error-summary.service';
 
 describe('ErrorSummaryService', () => {
 	let service: FudisErrorSummaryService;
-	let internalService: FudisInternalErrorSummaryService;
+	let internalServiceSpy: jasmine.SpyObj<FudisInternalErrorSummaryService>;
 
 	beforeEach(() => {
-		TestBed.configureTestingModule({});
+		const spy = jasmine.createSpyObj('FudisInternalErrorSummaryService', ['reloadErrors']);
+
+		TestBed.configureTestingModule({
+			providers: [FudisErrorSummaryService, { provide: FudisInternalErrorSummaryService, useValue: spy }],
+		});
 		service = TestBed.inject(FudisErrorSummaryService);
-		internalService = TestBed.inject(FudisInternalErrorSummaryService);
+		internalServiceSpy = TestBed.inject(
+			FudisInternalErrorSummaryService
+		) as jasmine.SpyObj<FudisInternalErrorSummaryService>;
 
 		spyOn(service, 'reloadErrors');
-		spyOn(internalService, 'reloadErrors');
 	});
 
 	it('should be created', () => {
 		expect(service).toBeTruthy();
 	});
 
-	// TODO: Do this correctly
-	it('should call reloadErrors from InternalErrorSummaryService', () => {
+	it('should call reloadErrors', () => {
 		service.reloadErrors();
-		internalService.reloadErrors();
 
 		expect(service.reloadErrors).toHaveBeenCalledWith();
-		expect(internalService.reloadErrors).toHaveBeenCalledWith();
+	});
+
+	it('should return mocked value from a spy', () => {
+		const mockValue = jasmine.createSpyObj('FudisInternalErrorSummaryService', { reloadErrors: of() });
+		internalServiceSpy.reloadErrors.and.returnValue(mockValue);
+
+		// eslint-disable-next-line @typescript-eslint/dot-notation
+		expect(service['_errorSummaryService'].reloadErrors())
+			.withContext('service returned mock value')
+			.toEqual(mockValue);
+
+		expect(internalServiceSpy.reloadErrors.calls.count()).withContext('spy method was called once').toEqual(1);
+
+		expect(internalServiceSpy.reloadErrors.calls.mostRecent().returnValue).toEqual(mockValue);
 	});
 });
