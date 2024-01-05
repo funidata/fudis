af61ec0c9b52786e29923eba3b8c5a3854c72ef5
Eero Suvanto <eero.suvanto@gmail.com>
2024-01-05 11:04:58 +0200

    aria fixes

--- ngx-fudis/projects/ngx-fudis/src/lib/directives/form/dropdown-base/dropdown-base.directive.ts
+++ ngx-fudis/projects/ngx-fudis/src/lib/directives/form/dropdown-base/dropdown-base.directive.ts
@@ -2,43 +2,43 @@ import { Directive, ElementRef, HostBinding, Input, ViewChild } from '@angular/c
 import { FudisInputSize } from '../../../types/forms';
 
 @Directive({
-	selector: '[fudisDropdownBase]',
+  selector: '[fudisDropdownBase]',
 })
 export class DropdownBaseDirective {
-	@ViewChild('dropdownElement') dropdownElement: ElementRef<HTMLElement>;
+  @ViewChild('dropdownElement') dropdownElement: ElementRef<HTMLElement>;
 
-	/**
-	 * Binding public variable for querying variant type
-	 */
-	@HostBinding('class') classes = 'fudis-dropdown-menu-host';
+  /**
+   * Binding public variable for querying variant type
+   */
+  @HostBinding('class') classes = 'fudis-dropdown-menu-host';
 
-	/**
-	 * Assign dropdown as single-select or multiselect (with checkboxes)
-	 */
-	@Input() multiselect: boolean = false;
+  /**
+   * Assign dropdown as single-select or multiselect (with checkboxes)
+   */
+  @Input() multiselect: boolean = false;
 
-	/**
-	 * Set dropdown size (should follow the given input element size)
-	 */
-	@Input() size: FudisInputSize | 'xs' = 'lg';
+  /**
+   * Set dropdown size (should follow the given input element size)
+   */
+  @Input() size: FudisInputSize | 'xs' = 'lg';
 
-	/**
-	 * Set dropdown open
-	 */
-	@Input() open: boolean = false;
+  /**
+   * Set dropdown open
+   */
+  @Input() open: boolean = false;
 
-	/**
-	 * Id of parent component's label. e.g. in Select or Multiselect form components
-	 */
-	@Input() labelId: string;
+  /**
+   * Id of parent component
+   */
+  @Input() parentId: string;
 
-	/**
-	 * Id for Dropdown Menu. Generated with FudisIdService
-	 */
-	public id: string;
+  /**
+   * Id for Dropdown Menu. Generated with FudisIdService
+   */
+  public id: string;
 
-	/**
-	 * Determine dropdown max-width
-	 */
-	protected _maxWidth: string = 'initial';
+  /**
+   * Determine dropdown max-width
+   */
+  protected _maxWidth: string = 'initial';
 }
