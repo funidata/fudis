d2d066137c3a18234b85ab2fcb4c79be7afb3bbf
riinakuukso <riina.kuukso@funidata.fi>
2024-01-11 17:10:28 +0200

    DS-226: Add min and max validator get methods

--- ngx-fudis/projects/ngx-fudis/src/lib/utilities/form/getValidators.ts
+++ ngx-fudis/projects/ngx-fudis/src/lib/utilities/form/getValidators.ts
@@ -57,3 +57,41 @@ export const getMinFromValidator = (control: AbstractControl): number | undefine
   const errors = validatorFn(new FormControl(-Infinity)) ?? {};
   return errors?.['min']?.min;
 };
+
+export const getMinDateFromValidator = (control: AbstractControl): Date | undefined => {
+  const validatorFn = control.validator;
+
+  if (validatorFn === null) {
+    return undefined;
+  }
+
+  const errors = validatorFn(new FormControl(new Date('1337-01-01')));
+  const value = errors?.['datepickerMin']?.value;
+
+  if (value) {
+    const timezone = value.getTimezoneOffset() * 60000;
+
+    const returnValue = new Date(value.getTime() - timezone);
+    return returnValue;
+  }
+  return undefined;
+};
+
+export const getMaxDateFromValidator = (control: AbstractControl): Date | undefined => {
+  const validatorFn = control.validator;
+
+  if (validatorFn === null) {
+    return undefined;
+  }
+
+  const errors = validatorFn(new FormControl(new Date('4242-01-01')));
+  const value = errors?.['datepickerMax']?.value;
+
+  if (value) {
+    const timezone = value.getTimezoneOffset() * 60000;
+
+    const returnValue = new Date(value.getTime() - timezone);
+    return returnValue;
+  }
+  return undefined;
+};
