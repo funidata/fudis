ef13a38a0b2bde93bbe9e7d8ebc401f13bb28b9f
Joonas Häkkinen <joonas.hakkinen@funidata.fi>
2024-01-30 13:58:07 +0200

    Install image diff plugin

--- snapshot/cypress.config.ts
+++ snapshot/cypress.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from "cypress";
+import getCompareSnapshotsPlugin from "cypress-image-diff-js/plugin";
 
 export default defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      return getCompareSnapshotsPlugin(on, config);
     },
   },
 });
