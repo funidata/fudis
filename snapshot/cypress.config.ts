a865d67f805da3795d701b6aa88f828480b3860d
Joonas Häkkinen <joonas.hakkinen@funidata.fi>
2024-01-30 15:30:49 +0200

    Simple test setup

--- snapshot/cypress.config.ts
+++ snapshot/cypress.config.ts
@@ -3,6 +3,7 @@ import getCompareSnapshotsPlugin from "cypress-image-diff-js/plugin";
 
 export default defineConfig({
   e2e: {
+    baseUrl: "http://localhost:6006",
     setupNodeEvents(on, config) {
       return getCompareSnapshotsPlugin(on, config);
     },
--- snapshot/cypress/e2e/btest.cy.ts
+++ snapshot/cypress/e2e/btest.cy.ts
@@ -1,5 +1,12 @@
 describe("template spec", () => {
   it("passes", () => {
-    cy.visit("https://example.cypress.io");
+    // cy.on("uncaught:exception", (err, runnable) => {
+    //   return false;
+    // });
+    // cy.visit("/?path=/docs/documentation-introduction-introduction--documentation");
+    cy.visit("/iframe.html?args=&id=components-form-dropdown--single-select&viewMode=story");
+    // cy.contains("Introduction");
+    // cy.get("#storybook-docs").should("be.visible");
+    cy.compareSnapshot("test");
   });
 });
