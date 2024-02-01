a40a1bb73d59360ef80679c59728de7c68683256
riinakuukso <riina.kuukso@funidata.fi>
2024-02-01 12:31:30 +0200

    DS.228: Fix naming, remove duplicate code

--- ngx-fudis/projects/ngx-fudis/src/lib/components/form/select/common/autocomplete/autocomplete.component.spec.ts
+++ ngx-fudis/projects/ngx-fudis/src/lib/components/form/select/common/autocomplete/autocomplete.component.spec.ts
@@ -1,5 +1,4 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { SelectAutocompleteComponent } from './autocomplete.component';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { ButtonComponent } from '../../../../button/button.component';
@@ -20,32 +19,30 @@ import { By } from '@angular/platform-browser';
 import { ValidatorErrorMessageComponent } from '../../../error-message/validator-error-message/validator-error-message.component';
 
 @Component({
-  selector: 'fudis-mock-with-parent',
-  template: `<div [id]="id">
-    <fudis-select-autocomplete
-      #autoSelect
-      [placeholder]="placeholder"
-      [control]="control"
-      [id]="id"
-      [dropdownOpen]="dropdownOpen"
-      [required]="true"
-    />
-  </div>`,
+  selector: 'fudis-mock-select-autocomplete',
+  template: ` <fudis-select-autocomplete
+    #autoSelect
+    [placeholder]="placeholder"
+    [control]="control"
+    [id]="id"
+    [dropdownOpen]="dropdownOpen"
+    [required]="true"
+  />`,
 })
-class MockWithParentComponent {
+class MockSelecAutocompleteComponent {
   @ViewChild('autoSelect') autoSelect: SelectAutocompleteComponent;
 
-  id: string = 'parent-select-id';
+  id: string = 'select-id';
   placeholder: string = 'Test placeholder';
   dropdownOpen: boolean = false;
-  control: FormControl = new FormControl<FudisSelectOption[] | null>(null);
+  control: FormControl = new FormControl<FudisSelectOption | FudisSelectOption[] | null>(null);
 }
 
 describe('AutocompleteComponent', () => {
   let component: SelectAutocompleteComponent;
   let fixture: ComponentFixture<SelectAutocompleteComponent>;
-  let parentComponent: MockWithParentComponent;
-  let parentFixture: ComponentFixture<MockWithParentComponent>;
+  let mockComponent: MockSelecAutocompleteComponent;
+  let mockFixture: ComponentFixture<MockSelecAutocompleteComponent>;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -60,7 +57,7 @@ describe('AutocompleteComponent', () => {
         GuidanceComponent,
         IconComponent,
         ValidatorErrorMessageComponent,
-        MockWithParentComponent,
+        MockSelecAutocompleteComponent,
       ],
       providers: [FudisFocusService, FudisIdService, FudisTranslationService],
       imports: [ReactiveFormsModule],
@@ -68,9 +65,9 @@ describe('AutocompleteComponent', () => {
     fixture = TestBed.createComponent(SelectAutocompleteComponent);
     component = fixture.componentInstance;
 
-    parentFixture = TestBed.createComponent(MockWithParentComponent);
-    parentComponent = parentFixture.componentInstance;
-    parentFixture.detectChanges();
+    mockFixture = TestBed.createComponent(MockSelecAutocompleteComponent);
+    mockComponent = mockFixture.componentInstance;
+    mockFixture.detectChanges();
   });
 
   function getInputElement() {
@@ -78,9 +75,14 @@ describe('AutocompleteComponent', () => {
     return inputElement;
   }
 
-  function getComponentWrapper() {
-    const inputElement = getElement(parentFixture, 'fudis-select-autocomplete');
+  function getMockInputElement() {
+    const inputElement = getElement(mockFixture, 'input');
     return inputElement;
+  }
+
+  function getComponentWrapper() {
+    const wrapperElement = getElement(mockFixture, 'fudis-select-autocomplete');
+    return wrapperElement;
   }
 
   describe('Select Autocomplete default values', () => {
@@ -89,23 +91,24 @@ describe('AutocompleteComponent', () => {
     });
 
     it('should have required attribute', () => {
-      expect(getComponentWrapper().getAttribute('ng-reflect-required')).toEqual('true');
+      expect(getMockInputElement().getAttribute('required')).toEqual('true');
     });
 
     it('should have given id', () => {
-      expect(getComponentWrapper().getAttribute('ng-reflect-id')).toEqual('parent-select-id');
+      expect(getMockInputElement().getAttribute('id')).toEqual('select-id');
     });
 
     it('should have dropdown open', () => {
-      parentComponent.dropdownOpen = true;
-      parentFixture.detectChanges();
-      expect(getComponentWrapper().getAttribute('ng-reflect-dropdown-open')).toEqual('true');
+      mockComponent.dropdownOpen = true;
+      mockFixture.detectChanges();
+
+      expect(getMockInputElement().getAttribute('aria-expanded')).toEqual('true');
     });
   });
 
   describe('Select Autocomplete functionality', () => {
     beforeEach(() => {
-      component.id = 'parent-id';
+      component.id = 'select-autocomplete-id';
       component.placeholder = 'Test placeholder';
       component.dropdownOpen = false;
       component.control = new FormControl<FudisSelectOption | FudisSelectOption[] | null>(null);
@@ -148,18 +151,6 @@ describe('AutocompleteComponent', () => {
       getInputElement().dispatchEvent(new KeyboardEvent('keyup', { key: 'ArrowDown' }));
 
       expect(component.triggerDropdownOpen.emit).toHaveBeenCalled();
-    });
-
-    it('should trigger filter text update event', () => {
-      jest.spyOn(component.triggerFilterTextUpdate, 'emit');
-
-      const input = fixture.debugElement.query(By.css('.fudis-select-autocomplete__input'));
-      const el = input.nativeElement;
-
-      el.value = '';
-      el.dispatchEvent(new KeyboardEvent('keyup'));
-
-      expect(component.triggerFilterTextUpdate.emit).toHaveBeenCalled();
     });
 
     it('should trigger filter text update event', () => {
