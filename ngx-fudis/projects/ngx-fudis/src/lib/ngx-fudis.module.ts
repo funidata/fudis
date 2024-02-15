4cd242d1b2a25f1e755788a55c7c000bd3e8150a
riinakuukso <riina.kuukso@funidata.fi>
2024-02-15 12:11:16 +0200

    DS-240: Remove duplicate provider

--- ngx-fudis/projects/ngx-fudis/src/lib/ngx-fudis.module.ts
+++ ngx-fudis/projects/ngx-fudis/src/lib/ngx-fudis.module.ts
@@ -283,7 +283,6 @@ import { SelectOptionBaseDirective } from './components/form/select/common/selec
     FudisGridService,
     FudisIdService,
     FudisLanguageBadgeGroupService,
-    FudisErrorSummaryService,
     FudisTranslationService,
   ],
 })
