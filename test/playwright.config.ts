df593d3d0f6df55770be0b8ec7ae1df6219f6659
Joonas Häkkinen <joonas.hakkinen@funidata.fi>
2024-02-15 13:27:07 +0200

    Disable VR parallel running in container

--- test/playwright.config.ts
+++ test/playwright.config.ts
@@ -1,24 +1,15 @@
 import { defineConfig, devices } from "@playwright/test";
 
-/**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// require('dotenv').config();
-
-/**
- * See https://playwright.dev/docs/test-configuration.
- */
 export default defineConfig({
   testDir: "./visual-regression",
-  /* Run tests in files in parallel */
+  /* Run tests __in files__ in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  workers: process.env.CI || process.env.CONTAINER ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [["html", { host: "0.0.0.0" }]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
