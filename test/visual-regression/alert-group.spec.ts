f50606d9fe8c2833ce401391f5dd04547f8e3e5e
Joonas Häkkinen <joonas.hakkinen@funidata.fi>
2024-02-15 10:10:47 +0200

    Remove VR test for deprecated alert-group component

--- test/visual-regression/alert-group.spec.ts
+++
@@ -1,7 +0,0 @@
-import test, { expect } from "@playwright/test";
-
-test("alert-group default", async ({ page }) => {
-  await page.goto("/iframe.html?args=&id=components-alert-group--example&viewMode=story");
-  await expect(page.locator(".fudis-alert-group")).toBeVisible();
-  await expect(page).toHaveScreenshot();
-});
