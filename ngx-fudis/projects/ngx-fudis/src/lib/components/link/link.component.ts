e788b8520c24e55c004ded1fe249b0975aab7220
riinakuukso <riina.kuukso@funidata.fi>
2024-02-21 13:18:55 +0200

    DS-252: Refactor Link Component and its input naming

--- ngx-fudis/projects/ngx-fudis/src/lib/components/link/link.component.ts
+++ ngx-fudis/projects/ngx-fudis/src/lib/components/link/link.component.ts
@@ -34,20 +34,25 @@ export class LinkComponent implements AfterViewInit {
   @ViewChild('linkRef') private _linkRef: ElementRef;
 
   /**
-   * Link URL using native href
+   * External link URL
    */
-  @Input() href: string;
+  @Input() externalLink: string;
 
   /**
    * Link URL using Angular RouterLink
    */
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  @Input() routerLinkUrl: string | any[] | null;
+  @Input() link: string | any[];
+
+  /**
+   * Regular HTML href
+   */
+  @Input() href: string;
 
   /**
    * Fragment ID for Angular Router
    */
-  @Input() fragmentId: string | undefined;
+  @Input() fragmentId: string;
 
   /**
    * Title for the link, if not defined title will be the same as link URL
@@ -58,12 +63,6 @@ export class LinkComponent implements AfterViewInit {
    * Link size. By default link will inherit its parent's font-size. If link is not inside e.g. <fudis-heading> or <fudis-body-text> its size can be defined either 'md' (14px) or 'lg' (16px).
    */
   @Input() size: 'inherit' | 'md' | 'lg' = 'inherit';
-
-  /**
-   * Create an external link to point a target page on another domain.
-   * External link contains new-tab icon and assistive aria-label.
-   */
-  @Input() external: boolean = false;
 
   /**
    * Link color
--- ngx-fudis/projects/ngx-fudis/src/lib/components/link/link.stories.ts
+++ ngx-fudis/projects/ngx-fudis/src/lib/components/link/link.stories.ts
@@ -15,12 +15,6 @@ export default {
     },
   },
   argTypes: {
-    external: {
-      options: [true, false],
-      control: {
-        type: 'radio',
-      },
-    },
     color: {
       options: ['primary-dark', 'gray-dark', 'white'],
       control: {
@@ -38,26 +32,23 @@ export const Example = Template.bind({});
 Example.args = {
   href: 'https://www.example.com',
   color: 'primary-dark',
-  external: false,
   initialFocus: false,
   size: 'inherit',
 };
 
 export const ExampleWithExternalLink = Template.bind({});
 ExampleWithExternalLink.args = {
-  href: 'https://www.example.com',
+  externalLink: 'https://www.example.com',
   color: 'primary-dark',
-  external: true,
   initialFocus: false,
   size: 'inherit',
 };
 
 export const ExampleWithLinkWithTitle = Template.bind({});
 ExampleWithLinkWithTitle.args = {
-  href: 'https://www.example.com',
+  externalLink: 'https://www.example.com',
   linkTitle: 'Visible title for the link',
   color: 'primary-dark',
-  external: false,
   initialFocus: false,
   size: 'inherit',
 };
