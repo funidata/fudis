8e70673d639c6c2fff0d34cb8dfc2df92cb33a2f
riinakuukso <riina.kuukso@funidata.fi>
2024-03-14 08:08:53 +0200

    DS-274: Fix sandbox, add onClick to Storybook options

--- ngx-fudis/projects/ngx-fudis/src/lib/components/form/form/form.stories.ts
+++ ngx-fudis/projects/ngx-fudis/src/lib/components/form/form/form.stories.ts
@@ -535,7 +535,7 @@ export default {
       },
     },
     errorSummaryLinkType: {
-      options: ['href', 'router'],
+      options: ['href', 'router', 'onClick'],
       control: {
         type: 'select',
       },
@@ -569,7 +569,7 @@ Example.args = {
   badgeText: 'Example',
   errorSummaryHelpText:
     'There are errors in this form. Please address these before trying to submit again.',
-  errorSummaryLinkType: 'href',
+  errorSummaryLinkType: 'onClick',
   errorSummaryVisible: false,
 };
 
