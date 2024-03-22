4430cee0fa1429d74c779239505f92ef6bd0a885
Eero Suvanto <eero.suvanto@gmail.com>
2024-03-22 14:59:53 +0200

    tests: breadcrumbs and button vr tests

---
+++ test/visual-regression/button.spec.ts
@@ -0,0 +1,16 @@
+import test, { expect } from "@playwright/test";
+
+test("button default", async ({ page }) => {
+  await page.goto("/iframe.html?args=&id=components-button--all-variants&viewMode=story");
+  await expect(page.getByTestId("fudis-button-1")).toBeVisible();
+  await expect(page).toHaveScreenshot("all-variants.png");
+
+  page.getByTestId("fudis-button-1").focus();
+  await expect(page).toHaveScreenshot("focus-primary.png");
+  page.getByTestId("fudis-button-2").focus();
+  await expect(page).toHaveScreenshot("focus-secondary.png");
+  page.getByTestId("fudis-button-3").focus();
+  await expect(page).toHaveScreenshot("focus-tertiary.png");
+  page.getByTestId("fudis-button-4").focus();
+  await expect(page).toHaveScreenshot("focus-disabled.png");
+});
