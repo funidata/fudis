af1b78e84fd0bf0c03851e17fd1f7367bbc540cd
Eero Suvanto <eero.suvanto@gmail.com>
2024-04-08 12:07:41 +0300

    icon: remove accidental > char and update vr tests

--- test/visual-regression/icon.spec.ts
+++ test/visual-regression/icon.spec.ts
@@ -2,5 +2,5 @@ import test, { expect } from "@playwright/test";
 
 test("all icons", async ({ page }) => {
   await page.goto("/iframe.html?args=&id=components-icon--all-icons&viewMode=story");
-  await expect(page).toHaveScreenshot();
+  await expect(page).toHaveScreenshot({ fullPage: true });
 });
