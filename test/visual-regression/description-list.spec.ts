c2ba7f817dbc2b52d7ef4205f5924189f9521f97
riinakuukso <riina.kuukso@funidata.fi>
2024-04-10 12:41:25 +0300

    DS-259: Add Playwright tests for DL with sub components

--- test/visual-regression/description-list.spec.ts
+++ test/visual-regression/description-list.spec.ts
@@ -1,5 +1,6 @@
 import test, { expect } from "@playwright/test";
 
+// Description List
 test("description list default regular", async ({ page }) => {
   await page.goto("/iframe.html?args=variant:regular&id=components-description-list--description-list&viewMode=story");
   await expect(page).toHaveScreenshot();
@@ -17,5 +18,26 @@ test("description list default regular and grid disabled", async ({ page }) => {
 
 test("description list default compact and grid disabled", async ({ page }) => {
   await page.goto("/iframe.html?args=variant:regular;disableGrid:!true&id=components-description-list--description-list&viewMode=story");
+  await expect(page).toHaveScreenshot();
+});
+
+// Description List With Sub Components
+test("description list regular with sub components", async ({ page }) => {
+  await page.goto("/iframe.html?args=variant:regular&id=components-description-list--description-list-with-sub-components&viewMode=story");
+  await expect(page).toHaveScreenshot();
+});
+
+test("description list regular with sub components and grid disabled", async ({ page }) => {
+  await page.goto("/iframe.html?args=variant:regular;disableGrid:!true&id=components-description-list--description-list-with-sub-components&viewMode=story");
+  await expect(page).toHaveScreenshot();
+});
+
+test("description list compact with sub components", async ({ page }) => {
+  await page.goto("/iframe.html?args=variant:compact&id=components-description-list--description-list-with-sub-components&viewMode=story");
+  await expect(page).toHaveScreenshot();
+});
+
+test("description list compact with sub components and grid disabled", async ({ page }) => {
+  await page.goto("/iframe.html?args=variant:compact;disableGrid:!true&id=components-description-list--description-list-with-sub-components&viewMode=story");
   await expect(page).toHaveScreenshot();
 });
