5cefd68d9497ed81558f851d3f350199ea63642f
riinakuukso <riina.kuukso@funidata.fi>
2024-04-10 12:08:59 +0300

    DS-259: Add Playwright tests for disabled grid

--- test/visual-regression/description-list.spec.ts
+++ test/visual-regression/description-list.spec.ts
@@ -9,3 +9,13 @@ test("description list default compact", async ({ page }) => {
   await page.goto("/iframe.html?args=variant:compact&id=components-description-list--description-list&viewMode=story");
   await expect(page).toHaveScreenshot();
 });
+
+test("description list default regular and grid disabled", async ({ page }) => {
+  await page.goto("/iframe.html?args=variant:compact;disableGrid:!true&id=components-description-list--description-list&viewMode=story");
+  await expect(page).toHaveScreenshot();
+});
+
+test("description list default compact and grid disabled", async ({ page }) => {
+  await page.goto("/iframe.html?args=variant:regular;disableGrid:!true&id=components-description-list--description-list&viewMode=story");
+  await expect(page).toHaveScreenshot();
+});
