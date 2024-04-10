22625ed8b011667b60b6e77132ffb44778ddad2f
riinakuukso <riina.kuukso@funidata.fi>
2024-04-10 12:51:30 +0300

    DS-259: Add Playwright tests for DL with multple dd elements

--- test/visual-regression/description-list.spec.ts
+++ test/visual-regression/description-list.spec.ts
@@ -41,3 +41,24 @@ test("description list compact with sub components and grid disabled", async ({ 
   await page.goto("/iframe.html?args=variant:compact;disableGrid:!true&id=components-description-list--description-list-with-sub-components&viewMode=story");
   await expect(page).toHaveScreenshot();
 });
+
+// Description List With Multiple Dd Elements
+test("description list regular with multiple dd elements", async ({ page }) => {
+  await page.goto("/iframe.html?args=variant:regular&id=components-description-list--description-list-with-multiple-dd-elements&viewMode=story");
+  await expect(page).toHaveScreenshot();
+});
+
+test("description list regular with multiple dd elements and grid disabled", async ({ page }) => {
+  await page.goto("/iframe.html?args=variant:regular;disableGrid:!true&id=components-description-list--description-list-with-multiple-dd-elements&viewMode=story");
+  await expect(page).toHaveScreenshot();
+});
+
+test("description list compact with multiple dd elements", async ({ page }) => {
+  await page.goto("/iframe.html?args=variant:compact&id=components-description-list--description-list-with-multiple-dd-elements&viewMode=story");
+  await expect(page).toHaveScreenshot();
+});
+
+test("description list compact with multiple dd elements and grid disabled", async ({ page }) => {
+  await page.goto("/iframe.html?args=variant:compact;disableGrid:!true&id=components-description-list--description-list-with-multiple-dd-elements&viewMode=story");
+  await expect(page).toHaveScreenshot();
+});
