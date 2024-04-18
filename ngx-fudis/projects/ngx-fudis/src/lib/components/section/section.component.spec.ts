cf635a9009f001497b7e40ae2bb4579ee9fc6dbe
RiinaKuu <riina.kuukso@funidata.fi>
2024-04-18 13:14:04 +0300

    DS-243: Add Section unit tests

--- ngx-fudis/projects/ngx-fudis/src/lib/components/section/section.component.spec.ts
+++ ngx-fudis/projects/ngx-fudis/src/lib/components/section/section.component.spec.ts
@@ -1,27 +1,25 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Component } from '@angular/core';
+import { By } from '@angular/platform-browser';
+import { MatTooltipModule } from '@angular/material/tooltip';
 import { SectionComponent } from './section.component';
 import { HeadingComponent } from '../typography/heading/heading.component';
+import { ButtonComponent } from '../button/button.component';
+import { BodyTextComponent } from '../typography/body-text/body-text.component';
+import { NotificationComponent } from '../notification/notification.component';
+import { IconComponent } from '../icon/icon.component';
 import { FudisGridService } from '../../services/grid/grid.service';
 import { FudisIdService } from '../../services/id/id.service';
 import { GridDirective } from '../../directives/grid/grid/grid.directive';
 import { FudisInternalErrorSummaryService } from '../../services/form/error-summary/internal-error-summary.service';
 import { FudisBreakpointService } from '../../services/breakpoint/breakpoint.service';
-import { Component } from '@angular/core';
-import { ButtonComponent } from '../button/button.component';
-import { BodyTextComponent } from '../typography/body-text/body-text.component';
 import { ActionsDirective } from '../../directives/content-projection/actions/actions.directive';
 import { NotificationsDirective } from '../../directives/content-projection/notifications/notifications.directive';
 import { ContentDirective } from '../../directives/content-projection/content/content.directive';
+import { TooltipDirective } from '../../directives/tooltip/tooltip.directive';
 import { FudisTooltipPosition } from '../../types/miscellaneous';
 import { FudisHeadingLevel, FudisHeadingSize } from '../../types/typography';
-
-// import { phl } from '@angular-extensions/pretty-html-log';
-import { NotificationComponent } from '../notification/notification.component';
-import { IconComponent } from '../icon/icon.component';
-import { TooltipDirective } from '../../directives/tooltip/tooltip.directive';
-import { MatTooltipModule } from '@angular/material/tooltip';
 import { getElement, sortClasses } from '../../utilities/tests/utilities';
-import { By } from '@angular/platform-browser';
 
 @Component({
   selector: 'mock-fudis-section',
@@ -29,6 +27,7 @@ import { By } from '@angular/platform-browser';
     [title]="title"
     [titleSize]="titleSize"
     [titleLevel]="titleLevel"
+    [classes]="classes"
     [tooltip]="tooltip"
     [tooltipToggle]="tooltipToggle"
     [tooltipPosition]="tooltipPosition"
@@ -48,15 +47,13 @@ class MockFudisSectionComponent {
   title: string = 'This is section title';
   titleSize: FudisHeadingSize = 'lg';
   titleLevel: FudisHeadingLevel = 2;
+  classes: string[];
   tooltip: string = 'This is tooltip in section';
   tooltipToggle: boolean = false;
   tooltipPosition: FudisTooltipPosition = 'below';
 }
 
 describe('SectionComponent', () => {
-  // let component: SectionComponent;
-  // let fixture: ComponentFixture<SectionComponent>;
-
   let mockComponent: MockFudisSectionComponent;
   let mockFixture: ComponentFixture<MockFudisSectionComponent>;
 
@@ -84,31 +81,34 @@ describe('SectionComponent', () => {
       ],
       imports: [MatTooltipModule],
     });
-    // fixture = TestBed.createComponent(SectionComponent);
-    // component = fixture.componentInstance;
-    // component.ngOnInit();
-    // fixture.detectChanges();
 
     mockFixture = TestBed.createComponent(MockFudisSectionComponent);
     mockComponent = mockFixture.componentInstance;
     mockFixture.detectChanges();
   });
 
-  function getSectionHeadingWrapper() {
-    const headingWrapper = getElement(mockFixture, '#fudis-section-1-heading');
-
-    return headingWrapper;
+  function getSectionElement() {
+    const sectionEl = getElement(mockFixture, 'section') as HTMLElement;
+    return sectionEl;
   }
 
-  // function sectionTitleCheck(size: FudisHeadingSize, level: FudisHeadingLevel): void {
-  //   mockComponent.titleSize = size;
-  //   mockComponent.titleLevel = level;
-  //   mockFixture.detectChanges();
+  function sectionTitleSizeCheck(size: FudisHeadingSize): void {
+    mockComponent.titleSize = size;
+    mockFixture.detectChanges();
 
-  //   const sectionHeadingEl = getElement(mockFixture, '.fudis-heading') as HTMLElement;
+    const sectionHeadingEl = getElement(mockFixture, '.fudis-heading') as HTMLElement;
 
-  //   expect(sectionHeadingEl.className).toContain(`fudis-heading__size__${size}`);
-  // }
+    expect(sectionHeadingEl.className).toContain(`fudis-heading__size__${size}`);
+  }
+
+  function sectionTitleLevelCheck(level: FudisHeadingLevel): void {
+    mockComponent.titleLevel = level;
+    mockFixture.detectChanges();
+
+    const headingElement = mockFixture.nativeElement.querySelector('fudis-heading');
+
+    expect(headingElement.querySelector(`h${level}`)).toBeTruthy();
+  }
 
   it('should create', () => {
     expect(mockComponent).toBeTruthy();
@@ -116,25 +116,34 @@ describe('SectionComponent', () => {
 
   describe('CSS classes', () => {
     it('should have Grid related classes', () => {
-      const sectionEl = getElement(mockFixture, 'section');
-
-      expect(sortClasses(sectionEl.className)).toEqual(
+      expect(sortClasses(getSectionElement().className)).toEqual(
         sortClasses(
           'fudis-section fudis-grid fudis-grid__align__start fudis-grid__initial fudis-grid__margin__top__none fudis-grid__margin__bottom__none fudis-grid__row-gap__none',
+        ),
+      );
+    });
+
+    it('should add custom CSS classes if given', () => {
+      mockComponent.classes = ['my-custom-class other-custom-class'];
+      mockFixture.detectChanges();
+
+      expect(sortClasses(getSectionElement().className)).toEqual(
+        sortClasses(
+          'fudis-section my-custom-class other-custom-class fudis-grid fudis-grid__align__start fudis-grid__initial fudis-grid__margin__top__none fudis-grid__margin__bottom__none fudis-grid__row-gap__none',
         ),
       );
     });
   });
 
   describe('HTML elements and properties', () => {
-    it('should have id', () => {
-      const sectionEl = getElement(mockFixture, 'section');
-
-      expect(sectionEl.getAttribute('aria-describedby')).toEqual('fudis-section-1-heading');
-      expect(sectionEl.getAttribute('id')).toEqual('fudis-section-1');
+    it('should have id related props', () => {
+      expect(getSectionElement().getAttribute('aria-describedby')).toEqual(
+        'fudis-section-1-heading',
+      );
+      expect(getSectionElement().getAttribute('id')).toEqual('fudis-section-1');
     });
 
-    it('should have heading and title props as given', () => {
+    it('should have heading present and title as given', () => {
       const headingWrapper = getElement(mockFixture, '#fudis-section-1-heading');
       const headingComponent = mockFixture.debugElement.query(By.directive(HeadingComponent));
       const headingElement = headingWrapper.querySelector('.fudis-heading');
@@ -145,10 +154,52 @@ describe('SectionComponent', () => {
     });
 
     it('should return correct title size', () => {
-      const headingElemenetClasses =
-        getSectionHeadingWrapper()?.querySelector('.fudis-heading')?.className;
-      console.log(headingElemenetClasses);
-      // phl(mockFixture);
+      sectionTitleSizeCheck('xxs');
+      sectionTitleSizeCheck('xs');
+      sectionTitleSizeCheck('sm');
+      sectionTitleSizeCheck('md');
+      sectionTitleSizeCheck('lg');
+      sectionTitleSizeCheck('xl');
+      sectionTitleSizeCheck('xxl');
+    });
+
+    it('should return correct title level', () => {
+      sectionTitleLevelCheck(1);
+      sectionTitleLevelCheck(2);
+      sectionTitleLevelCheck(3);
+      sectionTitleLevelCheck(4);
+      sectionTitleLevelCheck(5);
+      sectionTitleLevelCheck(6);
+    });
+  });
+
+  describe('Content projection', () => {
+    it('should render notification inside header if given', () => {
+      const notifications = mockFixture.nativeElement.querySelector(
+        '.fudis-section__notifications',
+      );
+      const notificationComponent = mockFixture.debugElement.query(
+        By.directive(NotificationComponent),
+      );
+
+      expect(notifications).toBeTruthy();
+      expect(notificationComponent).toBeTruthy();
+    });
+
+    it('should render action button(s) inside header if given', () => {
+      const actions = mockFixture.nativeElement.querySelector('.fudis-section__header__actions');
+      const actionComponent = mockFixture.debugElement.query(By.directive(ButtonComponent));
+
+      expect(actions).toBeTruthy();
+      expect(actionComponent).toBeTruthy();
+    });
+
+    it('should render content', () => {
+      const content = mockFixture.nativeElement.querySelector('.fudis-section__content');
+      const contentComponent = mockFixture.debugElement.query(By.directive(BodyTextComponent));
+
+      expect(content).toBeTruthy();
+      expect(contentComponent).toBeTruthy();
     });
   });
 });
