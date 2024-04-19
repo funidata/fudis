a460b5c127f3bab449c7d5ee3ead6cd1ffca5263
Eero Suvanto <eero.suvanto@gmail.com>
2024-04-19 12:11:19 +0300

    DS-243: playwright test update

--- test/visual-regression/section.spec.ts
+++ test/visual-regression/section.spec.ts
@@ -2,14 +2,17 @@ import test, { expect } from "@playwright/test";
 
 test("section default", async ({ page }) => {
   await page.goto("/iframe.html?args=&id=components-section--example&viewMode=story");
-  await expect(page).toHaveScreenshot();
+  await expect(page).toHaveScreenshot({ fullPage: true });
   await page.getByTestId("fudis-button-1").hover();
-  await expect(page).toHaveScreenshot("1-header-tooltip-hovered.png");
-  await page.getByTestId("fudis-expandable-1").click();
-  await expect(page).toHaveScreenshot("1-content-expandable-clicked.png");
+  await expect(page).toHaveScreenshot({ fullPage: true });
 });
 
 test("section titleSize md", async ({ page }) => {
   await page.goto("/iframe.html?args=titleSize:md&id=components-section--example&viewMode=story");
-  await expect(page).toHaveScreenshot();
+  await expect(page).toHaveScreenshot({ fullPage: true });
+});
+
+test("nested section", async ({ page }) => {
+  await page.goto("/iframe.html?args=&id=components-section--nested-example&viewMode=story");
+  await expect(page).toHaveScreenshot({ fullPage: true });
 });
