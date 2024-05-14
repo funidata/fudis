92aaed4ed6407913e92f66734a0853d8a21985e0
RiinaKuu <riina.kuukso@funidata.fi>
2024-05-14 13:05:49 +0300

    DS-188: Clean and add Datepicker unit tests

--- ngx-fudis/projects/ngx-fudis/src/lib/components/form/date/datepicker/datepicker.component.spec.ts
+++ ngx-fudis/projects/ngx-fudis/src/lib/components/form/date/datepicker/datepicker.component.spec.ts
@@ -10,11 +10,14 @@ import { GuidanceComponent } from '../../guidance/guidance.component';
 import { LabelComponent } from '../../label/label.component';
 import { DatepickerComponent } from './datepicker.component';
 import { ValidatorErrorMessageComponent } from '../../error-message/validator-error-message/validator-error-message.component';
-import { getElement } from 'projects/ngx-fudis/src/lib/utilities/tests/utilities';
+import { getElement, sortClasses } from '../../../../utilities/tests/utilities';
+import { fudisInputSizeArray } from '../../../../types/forms';
 
 describe('DatepickerComponent', () => {
   let component: DatepickerComponent;
   let fixture: ComponentFixture<DatepickerComponent>;
+  let wrapperElement: HTMLDivElement;
+  let datepickerInput: HTMLInputElement;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -38,58 +41,46 @@ describe('DatepickerComponent', () => {
     fixture = TestBed.createComponent(DatepickerComponent);
     component = fixture.componentInstance;
     component.control = new FormControl(null);
-    component.id = 'fudis-dp-unique-id-3';
     component.label = 'Select a date';
     fixture.detectChanges();
   });
 
-  function getDatepickerTopWrapperDiv(): HTMLElement {
-    return fixture.nativeElement.querySelector('.fudis-datepicker') as HTMLElement;
-  }
-
-  function assertDatepickerTopWrapperDivHasClasses(...classes: string[]): void {
-    const datepickerTopDivClasses = getDatepickerTopWrapperDiv()?.className ?? '';
-
-    expect(datepickerTopDivClasses.split(' ').sort()).toEqual([...classes].sort());
-  }
-
-  describe('Top wrapper div', () => {
-    it('should always have default CSS classes: fudis-datepicker and fudis-datepicker__md', () => {
-      assertDatepickerTopWrapperDivHasClasses('fudis-datepicker', 'fudis-input-size__md');
+  describe('Wrapper element', () => {
+    beforeEach(() => {
+      wrapperElement = getElement(fixture, '.fudis-datepicker') as HTMLDivElement;
     });
 
-    // TODO: Testing size Input could be improved and compacted to one test
-    it('should have respective CSS class indicating the datepicker size of small', () => {
-      component.size = 'sm';
-      fixture.detectChanges();
-
-      assertDatepickerTopWrapperDivHasClasses('fudis-datepicker', 'fudis-input-size__sm');
+    it('should always have default CSS classes', () => {
+      expect(sortClasses(wrapperElement.className)).toEqual(
+        sortClasses('fudis-datepicker fudis-input-size__md'),
+      );
     });
 
-    it('should have respective CSS class indicating the datepicker size of large', () => {
-      component.size = 'lg';
-      fixture.detectChanges();
+    it('should change CSS classes according to the given datepicker input size', () => {
+      fudisInputSizeArray.forEach((size) => {
+        component.size = size;
+        fixture.detectChanges();
 
-      assertDatepickerTopWrapperDivHasClasses('fudis-datepicker', 'fudis-input-size__lg');
+        expect(sortClasses(wrapperElement.className)).toEqual(
+          sortClasses(`fudis-datepicker fudis-input-size__${size}`),
+        );
+      });
     });
 
-    // TODO: Change ng-reflect to HTML id and for attributes if possible
-    it('should have fudis-label component present with given id', () => {
-      const childLabelComponent = fixture.debugElement.query(
-        By.css('fudis-label label.fudis-label'),
+    it('should have label element present', () => {
+      const childLabelElement = fixture.debugElement.query(By.css('label.fudis-label'));
+
+      expect(childLabelElement).toBeTruthy();
+      expect(childLabelElement.attributes['id']).toEqual('fudis-datepicker-1-label');
+      expect(childLabelElement.attributes['for']).toEqual('fudis-datepicker-1');
+    });
+
+    it('should have guidance element present', () => {
+      const childGuidanceElement = fixture.debugElement.query(
+        By.css('fudis-guidance .fudis-guidance #fudis-datepicker-1_guidance'),
       );
 
-      expect(childLabelComponent).toBeTruthy();
-      expect(childLabelComponent.attributes['id']).toEqual('label_fudis-dp-unique-id-3');
-      expect(childLabelComponent.attributes['for']).toEqual('fudis-dp-unique-id-3');
-    });
-
-    it('should have fudis-guidance component present with given id and helpText', () => {
-      const childGuidanceComponent = fixture.debugElement.query(
-        By.css('fudis-guidance .fudis-guidance #fudis-dp-unique-id-3_guidance'),
-      );
-
-      expect(childGuidanceComponent).toBeTruthy();
+      expect(childGuidanceElement).toBeTruthy();
 
       component.helpText = 'Select your favourite date';
       fixture.detectChanges();
@@ -103,8 +94,14 @@ describe('DatepickerComponent', () => {
   });
 
   describe('Input', () => {
+    beforeEach(() => {
+      datepickerInput = getElement(fixture, 'input') as HTMLInputElement;
+
+      jest.spyOn(component.handleBlur, 'emit');
+      jest.spyOn(component.handleKeyUp, 'emit');
+    });
+
     it('should have invalid attribute if datepicker is required, input is touched and no date has been chosen', () => {
-      const datepickerInput = fixture.nativeElement.querySelector('input');
       const requiredControl = new FormControl(null, FudisValidators.required('Date is required'));
       component.control = requiredControl;
       fixture.detectChanges();
@@ -118,28 +115,34 @@ describe('DatepickerComponent', () => {
       expect(!!datepickerInput.getAttribute('aria-invalid')).toEqual(true);
     });
 
-    it('should have calendar icon from fudis-icon component', () => {
-      const datepickerIcon = fixture.debugElement.query(By.css('fudis-icon'));
+    it('should have calendar icon', () => {
+      const datepickerIconSvgElement = fixture.debugElement.query(By.css('fudis-icon svg'));
 
-      expect(datepickerIcon.attributes['ng-reflect-icon']).toEqual('calendar');
+      expect(datepickerIconSvgElement.nativeElement.getAttribute('id')).toEqual('calendar');
     });
 
-    it('should have proper disabled attribute if input has been disabled', () => {
-      const datepickerInput = fixture.nativeElement.querySelector('input');
+    it('should have disabled and readonly attributes if input has been disabled', () => {
       component.disabled = true;
       fixture.detectChanges();
 
       expect(!!datepickerInput.getAttribute('aria-disabled')).toEqual(true);
+      expect(datepickerInput.hasAttribute('readonly')).toBeTruthy();
+    });
+
+    it('should emit events', () => {
+      datepickerInput.dispatchEvent(new Event('blur'));
+      fixture.detectChanges();
+
+      expect(component.handleBlur.emit).toHaveBeenCalled();
+
+      datepickerInput.dispatchEvent(new KeyboardEvent('keyup', { 'keyCode': 65}));
+      fixture.detectChanges();
+
+      expect(component.handleKeyUp.emit).toHaveBeenCalled();
     });
   });
 
   describe('Control updates', () => {
-    beforeEach(() => {
-      fixture = TestBed.createComponent(DatepickerComponent);
-      component = fixture.componentInstance;
-      component.label = 'Select a date';
-    });
-
     it('should have no value selected', () => {
       component.control = new FormControl(null);
       fixture.detectChanges();
@@ -147,6 +150,49 @@ describe('DatepickerComponent', () => {
       const inputEl = getElement(fixture, 'input') as HTMLInputElement;
 
       expect(inputEl.value).toEqual('');
+      expect(component.control.valid).toBe(true);
+    });
+
+    it('should update control validity according to given min validator and value', () => {
+      component.control = new FormControl(
+        null,
+        FudisValidators.datepickerMin({
+          value: new Date('2024-01-12'),
+          message: 'Date is not inside allowed range',
+        }),
+      );
+      fixture.detectChanges();
+
+      component.control.patchValue(new Date('2024-01-10'));
+      fixture.detectChanges();
+
+      expect(component.control.valid).toBe(false);
+
+      component.control.patchValue(new Date('2024-01-13'));
+      fixture.detectChanges();
+
+      expect(component.control.valid).toBe(true);
+    });
+
+    it('should update control validity according to given max validator and value', () => {
+      component.control = new FormControl(
+        null,
+        FudisValidators.datepickerMax({
+          value: new Date('2025-01-01'),
+          message: 'Date is not inside allowed range',
+        }),
+      );
+      fixture.detectChanges();
+
+      component.control.patchValue(new Date('2025-01-10'));
+      fixture.detectChanges();
+
+      expect(component.control.valid).toBe(false);
+
+      component.control.patchValue(new Date('2024-12-31'));
+      fixture.detectChanges();
+
+      expect(component.control.valid).toBe(true);
     });
   });
 });
