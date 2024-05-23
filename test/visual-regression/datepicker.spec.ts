379d0aec0e4e217c756ffb8e965f600e6058be37
RiinaKuu <riina.kuukso@funidata.fi>
2024-05-23 12:36:52 +0300

    DS-188: Update base images, add language change tests (WIP)

--- test/visual-regression/datepicker.spec.ts
+++ test/visual-regression/datepicker.spec.ts
@@ -2,14 +2,14 @@ import test, { expect } from "@playwright/test";
 
 let date = "20.1.2024";
 
-test("datepicker", async ({ page }) => {
+test("datepicker default", async ({ page }) => {
   await page.goto(
     "/iframe.html?args=&id=components-form-date-datepicker--datepicker&viewMode=story",
   );
   await expect(page).toHaveScreenshot();
 });
 
-test("datepicker focus and fill input", async ({ page }) => {
+test("datepicker default focus and fill input", async ({ page }) => {
   await page.goto(
     "/iframe.html?args=&id=components-form-date-datepicker--datepicker&viewMode=story",
   );
@@ -18,13 +18,36 @@ test("datepicker focus and fill input", async ({ page }) => {
   await expect(page).toHaveScreenshot();
 });
 
-test("datepicker open calendar pop-up", async ({ page }) => {
+test("datepicker default open calendar pop-up", async ({ page }) => {
   await page.goto(
     "/iframe.html?args=&id=components-form-date-datepicker--datepicker&viewMode=story",
   );
   await page.getByTestId("fudis-datepicker-1").fill(date);
-  await page.getByTestId("fudis-datepicker-calendar-icon-toggle").click();
+  await page.getByTestId("fudis-datepicker-1-calendar-icon-toggle").click();
   await expect(page).toHaveScreenshot({ fullPage: true });
+});
+
+test("datepicker default change calendar language", async ({ page }) => {
+  await page.goto(
+    "/iframe.html?args=&id=components-form-date-datepicker--datepicker&viewMode=story",
+  );
+  await page.getByTestId("fudis-button-1").click();
+  await page.getByTestId("fudis-datepicker-1-calendar-icon-toggle").click();
+  await expect(page).toHaveScreenshot({ path:"to-finnish.png", fullPage: true });
+  await page.waitForText("Touko 2024").toBeVisible();
+  await page.getByTestId("fudis-datepicker-1-calendar-icon-toggle").click();
+
+  await page.waitForSelector(".fudis-button").toBeVisible();
+  await page.getByTestId("fudis-button-1").click();
+  await page.getByTestId("fudis-datepicker-1-calendar-icon-toggle").click();
+  await expect(page).toHaveScreenshot({ path: "to-swedish.png", fullPage: true });
+  await page.waitForText("Maj 2024").toBeVisible();
+  await page.getByTestId("fudis-datepicker-1-calendar-icon-toggle").click();
+
+  await page.waitForSelector(".fudis-button").toBeVisible();
+  await page.getByTestId("fudis-button-1").click();
+  await page.getByTestId("fudis-datepicker-1-calendar-icon-toggle").click();
+  await expect(page).toHaveScreenshot({ path: "to-english.png", fullPage: true });
 });
 
 test("datepicker disabled", async ({ page }) => {
@@ -36,6 +59,6 @@ test("datepicker min and max", async ({ page }) => {
   await page.goto(
     "/iframe.html?args=&id=components-form-date-datepicker--with-min-max-validator&viewMode=story",
   );
-  await page.getByTestId("fudis-datepicker-calendar-icon-toggle").click();
+  await page.getByTestId("fudis-datepicker-1-calendar-icon-toggle").click();
   await expect(page).toHaveScreenshot({ fullPage: true });
 });
