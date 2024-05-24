c3d581141aff16e8ad6ab2ff535509b5c7fd956f
marjutkinnari <marjut.kinnari@funidata.fi>
2024-05-24 10:14:31 +0300

    Exercise layout

--- ngx-fudis/projects/dev/src/app/exercise/exercise.component.ts
+++ ngx-fudis/projects/dev/src/app/exercise/exercise.component.ts
@@ -3,5 +3,6 @@ import { Component } from "@angular/core";
 @Component({
   selector: 'app-exercise',
   templateUrl: 'exercise.component.html',
+  styleUrls: ['exercise.component.scss'],
 })
 export class ExerciseComponent {}
