cde27d18824ced8ff5087c7f98b8e11b8726936d
Eero Suvanto <eero.suvanto@gmail.com>
2024-05-30 16:15:55 +0300

    DS-299: lang badge cleaning

--- ngx-fudis/projects/ngx-fudis/src/lib/components/language-badge-group/language-badge/language-badge.component.ts
+++ ngx-fudis/projects/ngx-fudis/src/lib/components/language-badge-group/language-badge/language-badge.component.ts
@@ -7,14 +7,9 @@ import {
   OnChanges,
   OnInit,
   Output,
-  Signal,
   effect,
 } from '@angular/core';
-import {
-  FudisComponentChanges,
-  FudisLanguageAbbr,
-  FudisTranslationConfig,
-} from '../../../types/miscellaneous';
+import { FudisComponentChanges, FudisLanguageAbbr } from '../../../types/miscellaneous';
 import { TooltipApiDirective } from '../../../directives/tooltip/tooltip-api.directive';
 import { FudisTranslationService } from '../../../services/translation/translation.service';
 import { FudisIdService } from '../../../services/id/id.service';
@@ -35,9 +30,10 @@ export class LanguageBadgeComponent extends TooltipApiDirective implements OnCha
     super();
 
     effect(() => {
-      this._translations = this._translationService.getTranslations();
-      this._selectedLabel = this._translations().LANGUAGE_BADGE.ARIA_LABEL.SELECTED;
-      this._missingTranslation = this._translations().LANGUAGE_BADGE.ARIA_LABEL.MISSING_TRANSLATION;
+      const translations = this._translationService.getTranslations()();
+
+      this._selectedLabel = translations.LANGUAGE_BADGE.ARIA_LABEL.SELECTED;
+      this._missingTranslation = translations.LANGUAGE_BADGE.ARIA_LABEL.MISSING_TRANSLATION;
 
       this._setLabel();
     });
@@ -97,11 +93,6 @@ export class LanguageBadgeComponent extends TooltipApiDirective implements OnCha
    * Generated HTML id
    */
   protected _id: string;
-
-  /**
-   * Fudis translations
-   */
-  protected _translations: Signal<FudisTranslationConfig>;
 
   ngOnInit(): void {
     this._id = this._idService.getNewChildId('language-badge-group', this.parentId);
