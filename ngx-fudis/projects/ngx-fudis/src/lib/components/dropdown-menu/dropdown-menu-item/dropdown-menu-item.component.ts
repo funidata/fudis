c07ffbfbac994b16adbb47ccffff1ca074598e2d
RiinaKuu <riina.kuukso@funidata.fi>
2024-06-11 18:24:38 +0300

    DS-260: Remove unnecessary code since DropdownMenuItem is no longer used in Multiselect, add translation for disabled item

--- ngx-fudis/projects/ngx-fudis/src/lib/components/dropdown-menu/dropdown-menu-item/dropdown-menu-item.component.ts
+++ ngx-fudis/projects/ngx-fudis/src/lib/components/dropdown-menu/dropdown-menu-item/dropdown-menu-item.component.ts
@@ -7,32 +7,47 @@ import {
   Input,
   Optional,
   Inject,
+  ChangeDetectionStrategy,
+  effect,
 } from '@angular/core';
 import { DOCUMENT } from '@angular/common';
 import { DropdownMenuComponent } from '../dropdown-menu.component';
 import { FudisIdService } from '../../../services/id/id.service';
 import { DropdownItemBaseDirective } from '../../../directives/form/dropdown-item-base/dropdown-item-base.directive';
 import { ButtonComponent } from '../../button/button.component';
+import { FudisTranslationService } from '../../../services/translation/translation.service';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'fudis-dropdown-menu-item',
   templateUrl: './dropdown-menu-item.component.html',
   styleUrls: ['./dropdown-menu-item.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class DropdownMenuItemComponent extends DropdownItemBaseDirective implements OnInit {
   constructor(
     private _idService: FudisIdService,
+    private _translationService: FudisTranslationService,
     @Host() protected _parentComponent: DropdownMenuComponent,
     @Inject(DOCUMENT) _document: Document,
     @Host() @Optional() protected _parentButton: ButtonComponent,
   ) {
     super(_document);
+
+    effect(() => {
+      const translations = _translationService.getTranslations()();
+
+      this.translationItemDisabledText.next(translations.DROPDOWNMENU.ITEM.DISABLED);
+    });
   }
 
-  @ViewChild('dropdownItem') dropdownItem: ElementRef;
+  /**
+   * Template reference for dropdown item button element
+   */
+  @ViewChild('dropdownItem') dropdownItem: ElementRef<HTMLButtonElement>;
 
   /**
-   * Label for dropdown item
+   * Visible label for dropdown item
    */
   @Input({ required: true }) label: string;
 
@@ -41,14 +56,25 @@ export class DropdownMenuItemComponent extends DropdownItemBaseDirective impleme
    */
   @Input() disabled: boolean = false;
 
+  /**
+   * Internal translated text for disabled dropdown menu item
+   */
+  public translationItemDisabledText = new BehaviorSubject<string>('');
+
   ngOnInit(): void {
     this._id = this._idService.getNewChildId('dropdown-menu', this._parentComponent.id);
   }
 
+  /**
+   * Handle key down event
+   */
   protected _handleKeyDown(event: KeyboardEvent) {
-    this._baseHandleKeyDown(event, this.dropdownItem, 'fudis-dropdown-menu-item__focusable');
+    this._baseHandleKeyDown(event, this.dropdownItem, '.fudis-dropdown-menu-item__focusable');
   }
 
+  /**
+   * Handle blur event
+   */
   protected _handleButtonBlur(event: FocusEvent): void {
     const closeDropdown = this._focusedOutFromComponent(
       event,
@@ -61,11 +87,13 @@ export class DropdownMenuItemComponent extends DropdownItemBaseDirective impleme
     }
   }
 
+  /**
+   * Handle and emit event when closing the parent Dropdown Menu. Dropdown Menu should always close when dropdown item is clicked.
+   */
   protected _closeDropdown(event: Event): void {
-    // TODO: Enable when Dropdown Menu is re-enabled
-    // if (this._parentButton) {
-    //   this._parentButton.closeMenu();
-    // }
+    if (this._parentButton) {
+      this._parentButton.closeMenu();
+    }
 
     this.handleClick.emit(event);
   }
