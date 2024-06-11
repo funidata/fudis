a653b4f8d844f162ad963a04ed4440c364b16d03
RiinaKuu <riina.kuukso@funidata.fi>
2024-06-11 18:19:55 +0300

    DS-260: Add disabled translation key for DropdownMenuItem

--- ngx-fudis/projects/ngx-fudis/src/lib/services/translation/translationKeys.ts
+++ ngx-fudis/projects/ngx-fudis/src/lib/services/translation/translationKeys.ts
@@ -30,6 +30,11 @@ export const en: FudisTranslationConfig = {
     },
   },
   DIALOG: { CLOSE: 'Close' },
+  DROPDOWNMENU: {
+    ITEM: {
+      DISABLED: 'Disabled',
+    },
+  },
   INPUT_WITH_LANGUAGE_OPTIONS: {
     LANGUAGE: 'Language',
     MISSING: 'Missing',
@@ -102,6 +107,11 @@ export const fi: FudisTranslationConfig = {
     },
   },
   DIALOG: { CLOSE: 'Sulje' },
+  DROPDOWNMENU: {
+    ITEM: {
+      DISABLED: 'Ei valittavissa',
+    },
+  },
   INPUT_WITH_LANGUAGE_OPTIONS: {
     LANGUAGE: 'Kieli',
     MISSING: 'Puuttuu',
@@ -174,6 +184,11 @@ export const sv: FudisTranslationConfig = {
     },
   },
   DIALOG: { CLOSE: 'Stäng' },
+  DROPDOWNMENU: {
+    ITEM: {
+      DISABLED: 'Inaktiverad',
+    },
+  },
   INPUT_WITH_LANGUAGE_OPTIONS: {
     LANGUAGE: 'Språk',
     MISSING: 'Saknas',
--- ngx-fudis/projects/ngx-fudis/src/lib/types/miscellaneous.ts
+++ ngx-fudis/projects/ngx-fudis/src/lib/types/miscellaneous.ts
@@ -83,6 +83,11 @@ export interface FudisTranslationConfig {
     // Label for close button
     CLOSE: string;
   };
+  DROPDOWNMENU: {
+    ITEM: {
+      DISABLED: string;
+    };
+  };
   INPUT_WITH_LANGUAGE_OPTIONS: {
     // Label for language selection dropdown
     LANGUAGE: string;
