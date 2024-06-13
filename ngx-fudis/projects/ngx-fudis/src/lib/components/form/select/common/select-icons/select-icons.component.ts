ab3057ce64948f8b53b7082b8937a8eba08f42c1
Eero Suvanto <eero.suvanto@gmail.com>
2024-06-13 23:23:46 +0300

    DS-315: select-icons tests

--- ngx-fudis/projects/ngx-fudis/src/lib/components/form/select/common/select-icons/select-icons.component.ts
+++ ngx-fudis/projects/ngx-fudis/src/lib/components/form/select/common/select-icons/select-icons.component.ts
@@ -1,14 +1,23 @@
-import { Component, EventEmitter, Input, OnChanges, Output, effect } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  EventEmitter,
+  Input,
+  Output,
+  effect,
+  OnChanges,
+} from '@angular/core';
 import { FudisTranslationService } from '../../../../../services/translation/translation.service';
 import { FudisSelectVariant } from '../../../../../types/forms';
 import { FormControl } from '@angular/forms';
-import { FudisComponentChanges } from '../../../../../types/miscellaneous';
 import { BehaviorSubject } from 'rxjs';
+import { FudisComponentChanges } from '../../../../../types/miscellaneous';
 
 @Component({
   selector: 'fudis-select-icons',
   templateUrl: './select-icons.component.html',
   styleUrls: ['./select-icons.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class SelectIconsComponent implements OnChanges {
   constructor(protected _translationService: FudisTranslationService) {
@@ -19,61 +28,94 @@ export class SelectIconsComponent implements OnChanges {
     });
   }
 
+  /**
+   * If Select's dropdown is open
+   */
   @Input() dropdownOpen: boolean;
 
+  /**
+   * Parent Select's variant
+   */
   @Input() parentVariant: FudisSelectVariant;
 
+  /**
+   * Show / hide clear button
+   */
   @Input() clearButton: boolean;
 
-  @Input() chevron: boolean;
-
+  /**
+   * If parent'c control is disabled
+   * TODO: to be removed when Fudis is refactored only to use FormControl's disabled state
+   */
   @Input() disabled: boolean;
 
+  /**
+   * Autocomplete's filter text
+   */
   @Input() filterText: boolean;
 
-  @Input() parentControlValue: boolean;
-
+  /**
+   * Parent Select's form control
+   */
   @Input() parentControl: FormControl;
 
+  /**
+   * Output event for Clear Button click
+   */
   @Output() handleClearButtonClick: EventEmitter<Event> = new EventEmitter<Event>();
 
+  /**
+   * Output event for Clear Button focus
+   */
   @Output() handleClearButtonFocus: EventEmitter<FocusEvent> = new EventEmitter<FocusEvent>();
 
+  /**
+   * Output event for Clear Button blur
+   */
   @Output() handleClearButtonBlur: EventEmitter<FocusEvent> = new EventEmitter<FocusEvent>();
 
+  /**
+   * Output event for Clear Button destroy
+   */
   @Output() handleClearButtonDestroy: EventEmitter<void> = new EventEmitter<void>();
 
   ngOnChanges(changes: FudisComponentChanges<SelectIconsComponent>): void {
     if (changes.parentControl?.currentValue !== changes.parentControl?.previousValue) {
-      this._controlValue = this.parentControl.value;
+      this._controlValue.next(!!changes.parentControl?.currentValue);
 
       this.parentControl.valueChanges.subscribe((value) => {
-        this._controlValue = !!value;
+        this._controlValue.next(!!value);
       });
     }
   }
+
+  /**
+   * Observable for status if parent control has value
+   */
+  protected _controlValue = new BehaviorSubject<boolean>(false);
 
   /**
    * Translated aria-label for autocomplete close icon button which clears the input
    */
   protected _translationClearFilterText = new BehaviorSubject<string>('');
 
-  protected _chevronVisible: boolean;
-
-  protected _searchVisible: boolean;
-
-  protected _clearVisible: boolean;
-
-  protected _controlValue: boolean;
-
+  /**
+   * Click handler for Clear Button
+   */
   protected _clearButtonClick(event: Event): void {
     this.handleClearButtonClick.emit(event);
   }
 
+  /**
+   * Focus handler for Clear Button
+   */
   protected _handleClearButtonFocus(event: FocusEvent): void {
     this.handleClearButtonFocus.emit(event);
   }
 
+  /**
+   * Blur handler for Clear Button
+   */
   protected _handleClearButtonBlur(event: FocusEvent): void {
     this.handleClearButtonBlur.emit(event);
   }
