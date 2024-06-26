470e83f9676d7dc7dbccfedd191b6d13f2f9a714
RiinaKuu <riina.kuukso@funidata.fi>
2024-06-26 10:47:12 +0300

    DS-260: Lint fix

--- ngx-fudis/projects/ngx-fudis/src/lib/components/dropdown-menu/dropdown-menu-item/dropdown-menu-item.component.ts
+++ ngx-fudis/projects/ngx-fudis/src/lib/components/dropdown-menu/dropdown-menu-item/dropdown-menu-item.component.ts
@@ -105,8 +105,7 @@ export class DropdownMenuItemComponent extends DropdownItemBaseDirective {
       this.handleClick.emit(event);
 
       this._parentDropdownMenu.closeDropdownMenu();
-    }
-    else {
+    } else {
       event.stopPropagation();
     }
   }
