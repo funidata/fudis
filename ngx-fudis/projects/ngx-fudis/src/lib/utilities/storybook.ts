d8c93f282caec53e5b08c15c16bb651eb2f7f561
RiinaKuu <riina.kuukso@funidata.fi>
2024-07-15 16:22:51 +0300

    DS-278: Improve DL docs, clean Storybook excludes

--- ngx-fudis/projects/ngx-fudis/src/lib/utilities/storybook.ts
+++ ngx-fudis/projects/ngx-fudis/src/lib/utilities/storybook.ts
@@ -110,11 +110,9 @@ export const daterangeExclude: RegExp = excludeRegex([
  * Description List
  */
 const descriptionListCommonExclude: string[] = [
-  'addChildId',
   'align',
   'alignItemsX',
   'alignItemsY',
-  'childDlItems',
   'getDisabledGridStatus',
   'getVariant',
   'classes',
@@ -124,7 +122,6 @@ const descriptionListCommonExclude: string[] = [
   'serviceDefaults',
   'marginBottom',
   'marginTop',
-  'removeChildId',
   'rowGap',
   'tag',
   'width',
@@ -133,10 +130,7 @@ const descriptionListCommonExclude: string[] = [
 export const descriptionListArgsTypesExclude: RegExp = excludeRegex([
   'id',
   'getVariant',
-  'addChildId',
-  'childDlItems',
   'getDisabledGridStatus',
-  'removeChildId',
   'serviceDefaults',
 ]);
 
