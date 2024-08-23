7b3cd4d2fd6629649d53aa109721ca662c7f9003
marjutkinnari <marjut.kinnari@funidata.fi>
2024-08-23 11:02:47 +0300

    DS-270: Vr test update

--- test/visual-regression/radio-button-group.spec.ts
+++ test/visual-regression/radio-button-group.spec.ts
@@ -9,19 +9,19 @@ test("radio button group with required", async ({ page }) => {
   await page.getByTestId("fudis-radio-button-group-1-item-1").focus();
   await expect(page).toHaveScreenshot("validation-2-focus-1.png");
 
-  await page.getByTestId("fudis-radio-button-group-1-item-2").focus();
+  await page.keyboard.press("Tab");
   await page.waitForSelector(".fudis-error-message");
   await expect(page.getByText("You must choose a fruit")).toBeVisible();
-  await expect(page).toHaveScreenshot("validation-3-errors.png.png");
+  await expect(page).toHaveScreenshot("validation-3-errors.png");
 
-  await page.getByTestId("fudis-radio-button-group-1-item-2").click();
-  await expect(page).toHaveScreenshot("validation-4-click-1.png");
+  await page.getByTestId("fudis-radio-button-group-1-item-2").focus();
+  await expect(page).toHaveScreenshot("validation-4-focus.png");
+
+  await page.keyboard.press("Space");
+  await expect(page).toHaveScreenshot("validation-5-click-1.png");
 
   await page.keyboard.press("ArrowDown");
-  await expect(page).toHaveScreenshot("validation-5-click-2.png");
-
-  await page.keyboard.press("Tab");
-  await expect(page).toHaveScreenshot("validation-6-blur.png");
+  await expect(page).toHaveScreenshot("validation-6-click-2.png");
 });
 
 test("radio button group disabled", async ({ page }) => {
