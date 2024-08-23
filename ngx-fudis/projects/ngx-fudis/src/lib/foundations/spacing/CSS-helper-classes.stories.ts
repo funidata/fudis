ec7a9664e85249aa7f759d8c9af5bd1e925226d7
marjutkinnari <marjut.kinnari@funidata.fi>
2024-08-23 09:02:24 +0300

    DS-271: CSS Helper documentation

--- ngx-fudis/projects/ngx-fudis/src/lib/foundations/spacing/CSS-helper-classes.stories.ts
+++ ngx-fudis/projects/ngx-fudis/src/lib/foundations/spacing/CSS-helper-classes.stories.ts
@@ -1,10 +1,10 @@
 import { StoryFn, Meta } from '@storybook/angular';
 import { ButtonComponent } from '../../components/button/button.component';
 import { spacingExclude } from '../../utilities/storybook';
-import docs from './spacing.docs.mdx';
+import docs from './CSS-helper-classes.docs.mdx';
 
 export default {
-  title: 'Utilities/Spacing',
+  title: 'Foundations/Spacing/CSS Helper Classes',
   component: ButtonComponent,
   parameters: {
     docs: {
@@ -21,16 +21,18 @@ const html = String.raw;
 const ExampleTemplate: StoryFn = (args) => ({
   template: html`
     <fudis-body-text [variant]="'lg-regular'"
-      >This Fudis Button uses <strong>Fudis Spacing Classes</strong> with responsive margins and paddings for different screen sizes. Check what happens to the button container when you switch between different class values while resizing the browser window.</fudis-body-text
+      >This Fudis Button uses <strong>Fudis CSS Helper Classes</strong> with responsive margins and
+      paddings for different screen sizes. Check what happens to the button container when you
+      switch between different class values while resizing the browser window.</fudis-body-text
     >
-    <fudis-body-text [variant]="'lg-regular'" class="fudis-mt-lg"><strong>Currently applied classes: </strong><br><code>{{classes}}</code></fudis-body-text>
-    <div class="fudis-mt-lg"
+    <fudis-body-text [variant]="'lg-regular'" class="fudis-mt-lg"
+      ><strong>Currently applied classes: </strong><br /><code>{{classes}}</code></fudis-body-text
+    >
+    <div
+      class="fudis-mt-lg"
       style="border: 2px solid var(--fudis-color-primary-light); display: inline-block;"
     >
-      <fudis-button
-        [class]="classes"
-        [label]="'Test button'"
-      ></fudis-button>
+      <fudis-button [class]="classes" [label]="'Test button'"></fudis-button>
     </div>
   `,
   props: args,
@@ -44,9 +46,9 @@ Example.args = {
 Example.argTypes = {
   classes: {
     options: [
-      'fudis-xs-pt-md fudis-sm-mr-xxl fudis-md-mb-xxl fudis-xs-pl-lg', 
-      'fudis-md-mx-xxl', 
-      'fudis-xs-my-xxl fudis-xxl-my-none', 
+      'fudis-xs-pt-md fudis-sm-mr-xxl fudis-md-mb-xxl fudis-xs-pl-lg',
+      'fudis-md-mx-xxl',
+      'fudis-xs-my-xxl fudis-xxl-my-none',
       'fudis-px-sm fudis-xl-py-lg',
     ],
     control: { type: 'select' },
