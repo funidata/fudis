cd3caea84fa83cd9c5eeb65cda470cdae4d96b07
Eero Suvanto <eero.suvanto@gmail.com>
2024-09-06 09:25:46 +0300

    DS-242: refactoring input-base to input-api

---
+++ ngx-fudis/projects/ngx-fudis/src/lib/directives/form/input-api/input-api.directive.spec.ts
@@ -0,0 +1,8 @@
+import { InputApiDirective } from './input-api.directive';
+
+describe('InputApiDirective', () => {
+  it('should create an instance', () => {
+    const directive = new InputApiDirective();
+    expect(directive).toBeTruthy();
+  });
+});
