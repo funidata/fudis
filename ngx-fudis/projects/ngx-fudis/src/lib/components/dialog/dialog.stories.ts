3947c190c2aa2bbf0e42293c2c7ce32668630b68
Anna Salo <anna.salo@nitor.com>
2024-09-11 13:59:35 +0200

    DS-359: Update Dialog doucmentation and story example for nested Dialogs

--- ngx-fudis/projects/ngx-fudis/src/lib/components/dialog/dialog.stories.ts
+++ ngx-fudis/projects/ngx-fudis/src/lib/components/dialog/dialog.stories.ts
@@ -1,4 +1,4 @@
-import { Component, Inject, Input, TemplateRef } from '@angular/core';
+import { Component, Inject, Input, Optional, TemplateRef } from '@angular/core';
 import { StoryFn, Meta, moduleMetadata } from '@storybook/angular';
 import { FormControl, FormGroup, ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { ComponentType } from '@angular/cdk/portal';
@@ -13,6 +13,14 @@ import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 type TestForm = {
   powerAnimal: FormControl<string | null>;
 };
+
+type TestNestedDialogForm = {
+  favoriteVeggie: FormControl<string | null>;
+};
+
+type Veggie = 'fruit' | 'berry' | 'vegetable';
+
+type Veggies = { [veg in Veggie]?: string | null | undefined };
 
 @Component({
   selector: 'fudis-dialog-laucher',
@@ -230,70 +238,149 @@ class DialogWithGridComponent {
 }
 
 @Component({
+  selector: 'fudis-nested-dialog',
+  template: `
+    <fudis-dialog [size]="size">
+      <fudis-dialog-content *ngIf="id">
+        <fudis-form [title]="title" [level]="2" [titleVariant]="'xl'">
+          <ng-template fudisContent [type]="'form'">
+            <ng-container *ngTemplateOutlet="favoriteVeggies" />
+            <fudis-text-input
+              class="fudis-mb-md"
+              [id]="'example-input-dialog'"
+              [label]="'What is your favorite ' + id"
+              [control]="exampleDialogFormGroup.controls['favoriteVeggie']"
+            />
+          </ng-template>
+          <ng-template fudisActions [type]="'form'">
+            <fudis-button
+              fudisFormSubmit
+              (handleClick)="closeDialog()"
+              [label]="'Save and close dialog'"
+              [variant]="'secondary'"
+            />
+            <fudis-button
+              *ngIf="nextDialogToOpen"
+              fudisFormSubmit
+              (handleClick)="openDialogTemplate(nextDialogToOpen)"
+              [label]="'Save and open next Dialog'"
+            />
+          </ng-template>
+        </fudis-form>
+      </fudis-dialog-content>
+      <ng-container *ngIf="!id">
+        <fudis-heading fudisDialogTitle [level]="2" [variant]="'xl'">{{ title }}</fudis-heading>
+        <fudis-dialog-content>
+          <ng-container *ngTemplateOutlet="favoriteVeggies" />
+        </fudis-dialog-content>
+        <fudis-dialog-actions>
+          <fudis-button fudisDialogClose [label]="'Close this dialog'" [variant]="'secondary'" />
+          <fudis-button (handleClick)="closeAll()" [label]="'Close all Dialogs'" />
+        </fudis-dialog-actions>
+      </ng-container>
+    </fudis-dialog>
+
+    <ng-template #favoriteVeggies>
+      <fudis-body-text class="fudis-mb-sm" *ngIf="(_favoriteVeggies | keyvalue)?.length === 0">
+        You don't have any favorite veggies!
+      </fudis-body-text>
+      <fudis-body-text class="fudis-mb-sm" *ngFor="let veggie of _favoriteVeggies | keyvalue">
+        Your favorite {{ veggie.key }} is <b>{{ veggie.value }}</b
+        >.
+      </fudis-body-text>
+    </ng-template>
+  `,
+})
+class NestedDialogComponent {
+  constructor(
+    private _dialogService: FudisDialogService,
+    @Optional()
+    @Inject(MAT_DIALOG_DATA)
+    private data?: { favoriteVeggies?: Veggies },
+  ) {
+    this._favoriteVeggies = this.data?.favoriteVeggies || {};
+  }
+
+  @Input() size: FudisDialogSize = 'md';
+  @Input() id: Veggie;
+  @Input() title: string;
+  @Input() nextDialogToOpen:
+    | ComponentType<NestedDialogComponent>
+    | TemplateRef<NestedDialogComponent>;
+
+  protected _favoriteVeggies: Veggies = {};
+  exampleDialogFormGroup = new FormGroup<TestNestedDialogForm>({
+    favoriteVeggie: new FormControl(null),
+  });
+
+  updateFavoriteVeggies(): Veggies {
+    const value = this.exampleDialogFormGroup?.controls?.favoriteVeggie?.value;
+
+    if (value) {
+      this._favoriteVeggies[this.id] = value;
+    }
+
+    return this._favoriteVeggies;
+  }
+
+  openDialogTemplate(
+    dialogToOpen: ComponentType<NestedDialogComponent> | TemplateRef<NestedDialogComponent>,
+  ) {
+    this._dialogService.open(dialogToOpen, {
+      data: {
+        favoriteVeggies: this.updateFavoriteVeggies(),
+      },
+    });
+  }
+
+  closeDialog() {
+    this._dialogService.close(this.updateFavoriteVeggies());
+  }
+
+  closeAll(): void {
+    this._dialogService.closeAll();
+  }
+}
+@Component({
   selector: 'fudis-nested-dialogs',
   template: `
+    <fudis-body-text class="fudis-mb-md">
+      NOTE: It is recommended to have only one Dialog open at a time.
+    </fudis-body-text>
     <fudis-button
       (handleClick)="openDialogTemplate(firstDialog)"
       [label]="'Open dialog with nested dialogs'"
-    ></fudis-button>
+    />
 
     <ng-template #firstDialog>
-      <fudis-dialog [size]="size">
-        <fudis-heading fudisDialogTitle [level]="1" [variant]="'xl'"
-          >First opened Dialog</fudis-heading
-        >
-        <fudis-dialog-content>
-          <fudis-grid [rowGap]="'sm'"
-            ><fudis-body-text
-              >It is not recommended to have multiple dialogs open simultaneously, but sometimes it
-              is the least worse solution.</fudis-body-text
-            >
-            <fudis-button
-              (handleClick)="openDialogTemplate(secondDialog)"
-              [label]="'Open nested Dialog'"
-            ></fudis-button
-          ></fudis-grid>
-        </fudis-dialog-content>
-        <fudis-dialog-actions>
-          <fudis-button fudisDialogClose [label]="'Ok'"></fudis-button>
-        </fudis-dialog-actions>
-      </fudis-dialog>
+      <fudis-nested-dialog
+        [id]="'fruit'"
+        [title]="'First opened Dialog'"
+        [size]="size"
+        [nextDialogToOpen]="secondDialog"
+      />
     </ng-template>
-    <ng-template #secondDialog>
-      <fudis-dialog [size]="'md'">
-        <fudis-heading fudisDialogTitle [level]="1" [variant]="'xl'"
-          >Second opened Dialog</fudis-heading
-        >
-        <fudis-dialog-content>
-          <fudis-grid [rowGap]="'sm'"
-            ><fudis-body-text>This is a nested Dialog component.</fudis-body-text>
-            <fudis-button
-              (handleClick)="openDialogTemplate(thirdDialog)"
-              [label]="'Open final third Dialog'"
-            ></fudis-button
-          ></fudis-grid>
-        </fudis-dialog-content>
-        <fudis-dialog-actions>
-          <fudis-button fudisDialogClose [label]="'Ok'"></fudis-button>
-        </fudis-dialog-actions>
-      </fudis-dialog>
-    </ng-template>
-    <ng-template #thirdDialog>
-      <fudis-dialog [size]="'sm'">
-        <fudis-heading fudisDialogTitle [level]="1" [variant]="'xl'"
-          >Third opened Dialog</fudis-heading
-        >
-        <fudis-dialog-content>
-          <fudis-grid [rowGap]="'sm'"
-            ><fudis-body-text>This is now third and last dialog opened. </fudis-body-text>
-            <fudis-button (handleClick)="closeAll()" [label]="'Close All Dialogs'"></fudis-button
-          ></fudis-grid>
-        </fudis-dialog-content>
 
-        <fudis-dialog-actions>
-          <fudis-button fudisDialogClose [label]="'Ok'"></fudis-button>
-        </fudis-dialog-actions>
-      </fudis-dialog>
+    <ng-template #secondDialog>
+      <fudis-nested-dialog
+        [id]="'berry'"
+        [title]="'Second opened Dialog'"
+        [size]="size"
+        [nextDialogToOpen]="thirdDialog"
+      />
+    </ng-template>
+
+    <ng-template #thirdDialog>
+      <fudis-nested-dialog
+        [id]="'vegetable'"
+        [title]="'Third opened Dialog'"
+        [size]="size"
+        [nextDialogToOpen]="fourthDialog"
+      />
+    </ng-template>
+
+    <ng-template #fourthDialog>
+      <fudis-nested-dialog [title]="'Fourth opened Dialog'" [size]="size" />
     </ng-template>
   `,
 })
@@ -302,13 +389,10 @@ class NestedDialogsComponent {
 
   @Input() size: FudisDialogSize = 'md';
 
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  openDialogTemplate<T = any>(dialogToOpen: ComponentType<T> | TemplateRef<T>) {
+  openDialogTemplate(
+    dialogToOpen: ComponentType<NestedDialogComponent> | TemplateRef<NestedDialogComponent>,
+  ) {
     this._dialogService.open(dialogToOpen);
-  }
-
-  closeAll(): void {
-    this._dialogService.closeAll();
   }
 }
 
@@ -324,6 +408,7 @@ export default {
         DialogWithFormComponent,
         NestedDialogsComponent,
         DialogLaucherComponent,
+        NestedDialogComponent,
       ],
     }),
   ],
@@ -372,5 +457,5 @@ ExampleWithGrid.args = {
 
 export const ExampleWithNestedDialogs = TemplateNested.bind({});
 ExampleWithNestedDialogs.args = {
-  size: 'lg',
+  size: 'sm',
 };
