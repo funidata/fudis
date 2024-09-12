03e2231d87a2ad446e66a1021a90b9b63723f0ff
marjutkinnari <marjut.kinnari@funidata.fi>
2024-09-12 11:09:35 +0300

    DS-369: Removed selectModule

--- ngx-fudis/projects/ngx-fudis/src/lib/ngx-fudis.module.ts
+++ ngx-fudis/projects/ngx-fudis/src/lib/ngx-fudis.module.ts
@@ -7,7 +7,6 @@ import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
-import { MatSelectModule } from '@angular/material/select';
 import { AutocompleteComponent } from './components/form/autocomplete/autocomplete.component';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatNativeDateModule } from '@angular/material/core';
@@ -212,7 +211,6 @@ import {
     MatInputModule,
     MatNativeDateModule,
     MatButtonModule,
-    MatSelectModule,
     MatTooltipModule,
     ReactiveFormsModule,
     RouterModule,
