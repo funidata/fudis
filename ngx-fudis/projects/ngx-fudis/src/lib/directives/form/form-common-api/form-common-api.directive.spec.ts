37a5c66a0c0dfa8a03f8329ef5f9f1535e87ee50
Eero Suvanto <eero.suvanto@gmail.com>
2024-09-13 15:53:55 +0300

    DS-242: form-common-api unit tests

--- ngx-fudis/projects/ngx-fudis/src/lib/directives/form/form-common-api/form-common-api.directive.spec.ts
+++ ngx-fudis/projects/ngx-fudis/src/lib/directives/form/form-common-api/form-common-api.directive.spec.ts
@@ -3,6 +3,27 @@ import { FormCommonApiDirective } from './form-common-api.directive';
 import { FudisIdService } from '../../../services/id/id.service';
 import { ChangeDetectorRef } from '@angular/core';
 
+const defaultValues: Partial<FormCommonApiDirective> = {
+  ariaLabel: undefined,
+  disabled: false,
+  disableGuidance: false,
+  errorSummaryReloadOnInit: true,
+  helpText: undefined,
+  id: undefined,
+  initialFocus: false,
+  invalidState: false,
+  label: undefined,
+  tooltip: undefined,
+  tooltipPosition: 'below',
+  tooltipToggle: false,
+};
+
+const nonTestedValues = ['handleBlur', 'handleKeyUp', 'handleViewInit', 'handleFocus'];
+
+// class MockElementRef implements ElementRef {
+//   nativeElement = {};
+// }
+
 describe('FormCommonApiDirective', () => {
   let idService: FudisIdService;
   let cdr: ChangeDetectorRef;
@@ -24,78 +45,30 @@ describe('FormCommonApiDirective', () => {
       expect(directive).toBeTruthy();
     });
   });
+
+  it('should have default input values', () => {
+    TestBed.runInInjectionContext(() => {
+      const directive = new FormCommonApiDirective(idService, cdr);
+      Object.keys(directive).forEach((inputKey) => {
+        const typedKey = inputKey as keyof FormCommonApiDirective;
+
+        if (Array.from(typedKey)[0] !== '_' && !nonTestedValues.includes(inputKey)) {
+          expect(directive[typedKey]).toEqual(defaultValues[typedKey]);
+        }
+      });
+    });
+  });
+
+  it('should emit handleFocus when onFocus is called', () => {
+    TestBed.runInInjectionContext(() => {
+      const directive = new FormCommonApiDirective(idService, cdr);
+      jest.spyOn(directive.handleFocus, 'emit');
+
+      const focusEvent = new FocusEvent('focus');
+
+      directive.onFocus(focusEvent);
+
+      expect(directive.handleFocus.emit).toHaveBeenCalledWith(focusEvent);
+    });
+  });
 });
-
-// import { TestBed } from '@angular/core/testing';
-// import { FudisDOMUtilitiesService } from '../../../services/dom/dom-utilities.service';
-// import { InputApiDirective } from './input-api.directive';
-// import { DestroyRef, ElementRef } from '@angular/core';
-
-// const defaultValues: Partial<InputApiDirective> = {
-//   ariaLabel: undefined,
-//   disabled: false,
-//   errorSummaryReloadOnInit: true,
-//   disableGuidance: false,
-//   helpText: undefined,
-//   id: undefined,
-//   initialFocus: false,
-//   invalidState: false,
-//   label: undefined,
-//   tooltip: undefined,
-//   tooltipPosition: 'below',
-//   tooltipToggle: false,
-// };
-
-// const nonTestedValues = ['handleBlur', 'handleKeyUp', 'handleViewInit', 'handleFocus'];
-
-// class MockElementRef implements ElementRef {
-//   nativeElement = {};
-// }
-
-// describe('InputApiDirective', () => {
-//   beforeEach(() => {
-//     TestBed.configureTestingModule({
-//       declarations: [],
-//       providers: [
-//         FudisDOMUtilitiesService,
-//         { provide: ElementRef, useClass: MockElementRef },
-//         DestroyRef,
-//       ],
-//     });
-
-//     TestBed.inject(DestroyRef);
-//     TestBed.inject(FudisDOMUtilitiesService);
-//   });
-
-//   it('should create an instance', () => {
-//     TestBed.runInInjectionContext(() => {
-//       const directive = new InputApiDirective();
-//       expect(directive).toBeTruthy();
-//     });
-//   });
-
-//   it('should have default input values', () => {
-//     TestBed.runInInjectionContext(() => {
-//       const directive = new InputApiDirective();
-//       Object.keys(directive).forEach((inputKey) => {
-//         const typedKey = inputKey as keyof InputApiDirective;
-
-//         if (Array.from(typedKey)[0] !== '_' && !nonTestedValues.includes(inputKey)) {
-//           expect(directive[typedKey]).toEqual(defaultValues[typedKey]);
-//         }
-//       });
-//     });
-//   });
-// });
-
-//     it('should call focusToInput', () => {
-//       TestBed.runInInjectionContext(() => {
-//         const directive: InputBaseDirective = new InputBaseDirective(idService, changeDetectorRef);
-
-//         jest.spyOn(directive, 'focusToInput').mockImplementation(() => {});
-//         directive.focusToInput();
-
-//         expect(directive.focusToInput).toHaveBeenCalled();
-//       });
-//     });
-//   });
