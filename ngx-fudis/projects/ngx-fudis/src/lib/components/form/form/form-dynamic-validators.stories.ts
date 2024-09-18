bc2e0c915cf6807b5accfc1f4510d5a279bb3587
Anna Salo <anna.salo@nitor.com>
2024-09-18 15:09:24 +0300

    DS-251: Run prettier

--- ngx-fudis/projects/ngx-fudis/src/lib/components/form/form/form-dynamic-validators.stories.ts
+++ ngx-fudis/projects/ngx-fudis/src/lib/components/form/form/form-dynamic-validators.stories.ts
@@ -156,7 +156,7 @@ import { FudisGroupValidators } from '../../../utilities/form/groupValidators';
                     (handleChange)="
                       toggleRequiredFromOthers([
                         formExample.controls.winter,
-                        formExample.controls.working,
+                        formExample.controls.working
                       ])
                     "
                   />
@@ -171,7 +171,7 @@ import { FudisGroupValidators } from '../../../utilities/form/groupValidators';
                     (handleChange)="
                       toggleRequiredFromOthers([
                         formExample.controls.summer,
-                        formExample.controls.working,
+                        formExample.controls.working
                       ])
                     "
                   />
@@ -186,7 +186,7 @@ import { FudisGroupValidators } from '../../../utilities/form/groupValidators';
                     (handleChange)="
                       toggleRequiredFromOthers([
                         formExample.controls.summer,
-                        formExample.controls.winter,
+                        formExample.controls.winter
                       ])
                     "
                   />
