b977d51dee25fcbf2733506006d8d7799a332487
Eero Suvanto <eero.suvanto@gmail.com>
2024-09-23 13:15:17 +0300

    DS-322: cleaning

--- ngx-fudis/eslint.config.mjs
+++ ngx-fudis/eslint.config.mjs
@@ -4,8 +4,6 @@ import path from "node:path";
 import { fileURLToPath } from "node:url";
 import js from "@eslint/js";
 import { FlatCompat } from "@eslint/eslintrc";
-import { rules } from "@angular-eslint/eslint-plugin-template";
-import { IndentStyle } from "typescript";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -42,10 +40,6 @@ export default [
         project: "tsconfig.json",
       },
     },
-  },
-  {
-    files: ["**/*.scss"],
-    rules: {},
   },
   ...compat.extends("prettier").map((config) => ({
     ...config,
