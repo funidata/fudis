f16dca18acfb941d850e4167a5a71cc3891692d6
Eero Suvanto <eero.suvanto@gmail.com>
2024-09-26 10:55:58 +0300

    DS-314: update link directive unit tests

--- ngx-fudis/projects/ngx-fudis/src/lib/directives/link/link.directive.ts
+++ ngx-fudis/projects/ngx-fudis/src/lib/directives/link/link.directive.ts
@@ -6,22 +6,31 @@ import {
   OnChanges,
   effect,
   Input,
+  inject,
+  ViewContainerRef,
+  Inject,
+  ComponentRef,
+  ChangeDetectorRef,
 } from '@angular/core';
 import { FudisComponentChanges } from '../../types/miscellaneous';
 import { FudisFocusService } from '../../services/focus/focus.service';
 import { FudisIdService } from '../../services/id/id.service';
 import { BehaviorSubject } from 'rxjs';
 import { FudisTranslationService } from '../../services/translation/translation.service';
+import { IconComponent } from '../../components/icon/icon.component';
+import { DOCUMENT } from '@angular/common';
 
 @Directive({
   selector: '[fudisLink]',
 })
 export class LinkDirective implements OnInit, OnChanges, AfterViewInit {
   constructor(
+    @Inject(DOCUMENT) private _document: Document,
     private _bindedElement: ElementRef<HTMLAnchorElement>,
     private _focusService: FudisFocusService,
     private _idService: FudisIdService,
     private _translationService: FudisTranslationService,
+    private _cdr: ChangeDetectorRef,
   ) {
     effect(() => {
       this._externalLinkAriaLabel.next(
@@ -57,6 +66,12 @@ export class LinkDirective implements OnInit, OnChanges, AfterViewInit {
 
   private _sizeCssClass: string = `fudis-link__size__inherit`;
 
+  private _viewContainerRef = inject(ViewContainerRef);
+
+  private _iconComponentRef: ComponentRef<IconComponent>;
+
+  private _parsedTitle: string[] = [];
+
   /**
    * Aria-label for the external link
    */
@@ -65,21 +80,20 @@ export class LinkDirective implements OnInit, OnChanges, AfterViewInit {
   );
 
   ngAfterViewInit(): void {
-    this._bindedElement.nativeElement.classList.add('fudis-link');
     if (this.initialFocus && !this._focusService.isIgnored(this.id)) {
-      this._focusToAnchorElement();
+      (this._bindedElement.nativeElement as HTMLAnchorElement).focus();
     }
   }
 
   ngOnInit(): void {
-    //this._setCssClasses();
-
     if (this.id) {
       this._idService.addNewId('link', this.id);
     } else {
       this.id = this._idService.getNewId('link');
     }
 
+    this._setSizeClass();
+    this._bindedElement.nativeElement.classList.add('fudis-link');
     this._bindedElement.nativeElement.setAttribute('id', this.id);
   }
 
@@ -88,12 +102,52 @@ export class LinkDirective implements OnInit, OnChanges, AfterViewInit {
       this._setSizeClass();
     }
 
-    if (changes.external?.currentValue !== changes.external?.previousValue) {
-      this._setExternalAttributes();
-    }
+    const titleChanged = changes.title?.currentValue !== changes.title?.previousValue;
 
-    if (changes.title?.currentValue !== changes.title?.previousValue) {
-      this._setTitle();
+    const externalChanged = changes.external?.currentValue !== changes.title?.previousValue;
+
+    if (titleChanged || externalChanged) {
+      this._setHtmlAttributes();
+
+      if (titleChanged) {
+        this._parseTitle();
+
+        if (!changes.title?.isFirstChange() && this.external) {
+          this._updateTitleHtml();
+        }
+      }
+
+      if (externalChanged) {
+        this._setExternalHtml();
+      }
+
+      this._cdr.detectChanges();
+    }
+  }
+
+  private _updateTitleHtml(): void {
+    // Remove texts from parent
+    this._bindedElement.nativeElement.childNodes.forEach(
+      (node) => node.nodeType === Node.TEXT_NODE && node.remove(),
+    );
+
+    const iconWrapper = this._bindedElement.nativeElement.querySelector(
+      '.fudis-link__external__icon-wrapper',
+    ) as HTMLSpanElement;
+
+    if (iconWrapper) {
+      // Remove texts from icon wrapper
+      iconWrapper.childNodes.forEach((node) => node.nodeType === Node.TEXT_NODE && node.remove());
+
+      if (this._parsedTitle.length === 1) {
+        const text = this._document.createTextNode(this._parsedTitle[0]);
+        iconWrapper.prepend(text);
+      } else {
+        const textStart = this._document.createTextNode(`${this._parsedTitle[0]} `);
+        const textEnd = this._document.createTextNode(this._parsedTitle[1]);
+        this._bindedElement.nativeElement.prepend(textStart);
+        iconWrapper.prepend(textEnd);
+      }
     }
   }
 
@@ -109,82 +163,74 @@ export class LinkDirective implements OnInit, OnChanges, AfterViewInit {
     this._bindedElement.nativeElement.classList.add(this._sizeCssClass);
   }
 
-  private _setExternalAttributes(): void {
+  /**
+   * Add to remove relevant HTML attributes from binded element.
+   */
+  private _setHtmlAttributes(): void {
     if (this.external) {
       this._bindedElement.nativeElement.setAttribute('target', '_blank');
       this._bindedElement.nativeElement.setAttribute('rel', 'noopener noreferrer');
       this._bindedElement.nativeElement.setAttribute(
         'aria-label',
-        `${this.title}, ${this._externalLinkAriaLabel}`,
+        `${this.title}, ${this._externalLinkAriaLabel.value}`,
       );
     } else {
       this._bindedElement.nativeElement.removeAttribute('target');
       this._bindedElement.nativeElement.removeAttribute('rel');
       this._bindedElement.nativeElement.setAttribute('aria-label', this.title);
+
+      const titleNode = this._document.createTextNode(this.title);
+
+      this._bindedElement.nativeElement.replaceChildren(titleNode);
     }
   }
 
-  private _focusToAnchorElement(): void {
-    if (this._bindedElement?.nativeElement && !this._focusService.isIgnored(this.id)) {
-      (this._bindedElement.nativeElement as HTMLAnchorElement).focus();
+  private _setExternalHtml(): void {
+    if (this.external) {
+      // Create Icon Component and define properties
+      this._iconComponentRef = this._viewContainerRef.createComponent(IconComponent);
+      this._iconComponentRef.setInput('icon', 'new-tab');
+      this._iconComponentRef.setInput('color', 'primary-dark');
+
+      // Create HTML semantics
+      const html = String.raw;
+      if (this._parsedTitle.length === 1) {
+        this._bindedElement.nativeElement.innerHTML = html`<span
+          class="fudis-link__external__icon-wrapper"
+          >${this._parsedTitle[0]}<span class="fudis-link__external__icon"></span>
+        </span>`;
+      } else {
+        this._bindedElement.nativeElement.innerHTML = html`${this._parsedTitle[0]}
+          <span class="fudis-link__external__icon-wrapper"
+            >${this._parsedTitle[1]}<span class="fudis-link__external__icon"></span>
+          </span>`;
+      }
+
+      // Append Icon Component to right place in the HTML
+      this._bindedElement.nativeElement
+        .querySelector('.fudis-link__external__icon')
+        ?.append(this._iconComponentRef.instance.elementRef.nativeElement);
+    } else {
+      // If not External, detach Icon Component instance
+
+      this._viewContainerRef.detach();
     }
   }
 
-  private _setTitle(): void {
-    this._bindedElement.nativeElement.innerText = this.title;
+  /**
+   * Used with external links to split the last word of the title to be paired with the Icon, so that on line break, the icon sticks with the last word of the title.
+   */
+  private _parseTitle(): void {
+    const toArray = this.title.split(' ').filter((n) => n);
+
+    if (toArray.length > 1) {
+      const lastWord: string = toArray[toArray.length - 1];
+
+      const titleStart: string = toArray.slice(0, -1).join(' ');
+
+      this._parsedTitle = [titleStart, lastWord];
+    } else {
+      this._parsedTitle = [toArray[0]];
+    }
   }
-
-  //   /**
-  //  * For external links with a title. Used to split the last word of the title to be paired with the Icon, so that on line break, the icon sticks with the last word of the title.
-  //  */
-  //   private _parseExternalLinkTitle(): void {
-  //     if (this.external) {
-  //       if (this.title) {
-  //         const toArray = this.title.split(' ');
-
-  //         if (toArray.length > 1) {
-  //           const lastWord: string = toArray[toArray.length - 1];
-
-  //           const titleStart: string = toArray.slice(0, -1).join(' ');
-
-  //           this._externalLinkTitleParsed.next([titleStart, lastWord]);
-  //         } else {
-  //           this._externalLinkTitleParsed.next(toArray);
-  //         }
-  //       } else {
-  //         this._externalLinkTitleParsed.next([this.title]);
-  //       }
-  //     }
-  //   }
 }
-
-// <!--
-// <ng-container *ngIf="!title">
-//   <span class="fudis-link__external__icon-wrapper"
-//     >{{ external
-//     }}<span class="fudis-link__external__icon">
-//       <fudis-icon [icon]="'new-tab'" [color]="'primary-dark'" />
-//     </span>
-//   </span>
-// </ng-container>
-// <ng-container *ngIf="_externalLinkTitleParsed | async as parsedTitle">
-//   <ng-container *ngIf="title">
-//     <ng-container *ngIf="parsedTitle.length > 1"
-//       >{{ parsedTitle[0] }}
-//       <span class="fudis-link__external__icon-wrapper"
-//         >{{ parsedTitle[1]
-//         }}<span class="fudis-link__external__icon">
-//           <fudis-icon [icon]="'new-tab'" [color]="'primary-dark'" />
-//         </span>
-//       </span>
-//     </ng-container>
-//     <ng-container *ngIf="parsedTitle.length === 1">
-//       <span class="fudis-link__external__icon-wrapper"
-//         >{{ parsedTitle[0]
-//         }}<span class="fudis-link__external__icon">
-//           <fudis-icon [icon]="'new-tab'" [color]="'primary-dark'" />
-//         </span>
-//       </span>
-//     </ng-container>
-//   </ng-container>
-// </ng-container> -->
