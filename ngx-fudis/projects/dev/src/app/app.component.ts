41af47e75ae9b5bef37bfcdca4f4b23034048d3a
Eero Suvanto <eero.suvanto@gmail.com>
2024-10-09 10:08:32 +0300

    DS-288: update packages

--- ngx-fudis/projects/dev/src/app/app.component.ts
+++ ngx-fudis/projects/dev/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, Inject, OnInit } from '@angular/core';
-import { TranslocoService } from '@ngneat/transloco';
+import { TranslocoService } from '@jsverse/transloco';
 import {
   FudisAlertService,
   FudisDialogService,
