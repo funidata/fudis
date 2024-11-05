7def78c3795f22208d9980695de3483c27f1328b
RiinaKuu <riina.kuukso@funidata.fi>
2024-11-05 13:28:46 +0200

    DS-384: Prettier check

--- test/visual-regression/text-area.spec.ts
+++ test/visual-regression/text-area.spec.ts
@@ -19,12 +19,16 @@ test("text-area with validators", async ({ page }) => {
   await page.getByTestId("fudis-text-area-1").focus();
   await page.getByTestId("fudis-text-area-1").fill("hey");
   await expect(page.getByText("Missing a value.")).not.toBeVisible();
-  await expect(page.getByText("Too short input. Minimum length is 5 and maximum length is 20.")).toBeVisible();
+  await expect(
+    page.getByText("Too short input. Minimum length is 5 and maximum length is 20."),
+  ).toBeVisible();
 
   await expect(page).toHaveScreenshot("validators-2-with-errors.png");
 
   await page.getByTestId("fudis-text-area-1").fill("Hello there!");
-  await expect(page.getByText("Too short input. Minimum length is 5 and maximum length is 20.")).not.toBeVisible();
+  await expect(
+    page.getByText("Too short input. Minimum length is 5 and maximum length is 20."),
+  ).not.toBeVisible();
 
   await expect(page).toHaveScreenshot("validators-3-filled.png");
 });
