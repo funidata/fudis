5babca0836b369bed3a549f21963e3b15ef5e499
Eero Suvanto <eero.suvanto@gmail.com>
2024-12-30 17:28:47 +0200

    DS-248: prettier fix

--- ngx-fudis/projects/ngx-fudis/src/lib/components/spinner/spinner.component.spec.ts
+++ ngx-fudis/projects/ngx-fudis/src/lib/components/spinner/spinner.component.spec.ts
@@ -8,9 +8,8 @@ describe('SpinnerComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [SpinnerComponent]
-    })
-    .compileComponents();
+      imports: [SpinnerComponent],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(SpinnerComponent);
     component = fixture.componentInstance;
