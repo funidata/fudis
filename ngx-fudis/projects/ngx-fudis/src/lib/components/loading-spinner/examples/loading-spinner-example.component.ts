9f7bfb089515028c055062ea66f0348812646cdf
Eero Suvanto <eero.suvanto@gmail.com>
2024-12-31 14:04:34 +0200

    DS-248: Enable manual focus for Heading component

--- ngx-fudis/projects/ngx-fudis/src/lib/components/loading-spinner/examples/loading-spinner-example.component.ts
+++ ngx-fudis/projects/ngx-fudis/src/lib/components/loading-spinner/examples/loading-spinner-example.component.ts
@@ -1,8 +1,9 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal, ViewChild } from '@angular/core';
 
 import { CommonModule } from '@angular/common';
 import { LoadingSpinnerComponent } from '../loading-spinner.component';
 import { NgxFudisModule } from '../../../ngx-fudis.module';
+import { HeadingComponent } from '../../typography/heading/heading.component';
 
 @Component({
   standalone: true,
@@ -14,11 +15,17 @@ import { NgxFudisModule } from '../../../ngx-fudis.module';
 export class StorybookExampleLoadingSpinnerComponent {
   loadingState = signal<boolean>(false);
 
+  @ViewChild('headingRef') public headingRef: HeadingComponent;
+
   public toggleLoading(): void {
     this.loadingState.set(true);
 
     setTimeout(() => {
       this.loadingState.set(false);
+
+      setTimeout(() => {
+        this.headingRef.headingRef.nativeElement.focus();
+      }, 100);
     }, 3000);
   }
 }
