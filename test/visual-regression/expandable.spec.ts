289d55f303074628004e888eff82e0dfa5bfdd07
RiinaKuu <riina.kuukso@funidata.fi>
2025-02-18 11:41:25 +0200

    DS-362: Update Playwright test and base images

--- test/visual-regression/expandable.spec.ts
+++ test/visual-regression/expandable.spec.ts
@@ -2,14 +2,22 @@ import test, { expect } from "@playwright/test";
 
 test("expandable", async ({ page }) => {
   await page.goto("/iframe.html?args=&id=components-expandable--all-variants&viewMode=story");
-  await expect(page).toHaveScreenshot("1-init.png");
   await page.getByTestId("fudis-expandable-1-header-button").click();
   await expect(page.getByText("The content of the expandable.")).toBeVisible();
-  await expect(page).toHaveScreenshot("2-regular-opened.png");
-  await page.getByTestId("fudis-expandable-2-header-button").focus();
-  await expect(page).toHaveScreenshot("3-focused-title.png");
-  await page.getByTestId("fudis-expandable-4-header-button").click();
-  await expect(page).toHaveScreenshot("4-lite-opened.png");
+  await expect(page).toHaveScreenshot("1-regular-open.png");
+
+  await page.getByTestId("fudis-expandable-4-header-button").focus();
+  await page.keyboard.press("Tab");
+  await expect(page.getByTestId("fudis-button-1")).toBeFocused();
+
+  await page.getByTestId("fudis-expandable-5-header-button").click();
+  await expect(page.getByText("The content of the expandable with padding small.")).toBeVisible();
+  await expect(page).toHaveScreenshot("2-lite-open.png");
+
   await page.getByTestId("fudis-expandable-1-header-button").click();
-  await expect(page).toHaveScreenshot("5-regular-closed.png");
+  await page.getByTestId("fudis-expandable-5-header-button").click();
+  await expect(page.getByText("The content of the expandable.")).not.toBeVisible();
+  await expect(
+    page.getByText("The content of the expandable with padding small."),
+  ).not.toBeVisible();
 });
