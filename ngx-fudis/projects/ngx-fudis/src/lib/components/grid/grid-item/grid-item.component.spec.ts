98d31633eb9e58183b30648804494f2bfe4e23ac
RiinaKuu <riina.kuukso@funidata.fi>
2025-03-17 17:24:26 +0200

    DS-258: Clean up Grid Item unit test

--- ngx-fudis/projects/ngx-fudis/src/lib/components/grid/grid-item/grid-item.component.spec.ts
+++ ngx-fudis/projects/ngx-fudis/src/lib/components/grid/grid-item/grid-item.component.spec.ts
@@ -1,37 +1,26 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { Component } from '@angular/core';
-import { MockComponent } from 'ng-mocks';
 import { By } from '@angular/platform-browser';
 import { GridItemComponent } from './grid-item.component';
-import { FudisBreakpointService } from '../../../services/breakpoint/breakpoint.service';
-import { FudisBreakpointStyleResponsive } from '../../../types/breakpoints';
-import { FudisGridItemAlignment } from '../../../types/grid';
 import { GridComponent } from '../grid/grid.component';
-import { HeadingComponent } from '../../typography/heading/heading.component';
 import { BodyTextComponent } from '../../typography/body-text/body-text.component';
-import { ButtonComponent } from '../../button/button.component';
+import { FudisBreakpointService } from '../../../services/breakpoint/breakpoint.service';
+
+// NOTE: As most of this component's functionality is visual and it adds inline style properties, testing these with Jest is not feasible.
+// Visual Regression tests (grid-item.spec.ts) should be sufficient to cover testing these.
 
 @Component({
   selector: 'fudis-mock-grid-item-component',
   template: `<fudis-grid [columns]="6">
-    <fudis-heading [level]="2">I am test heading</fudis-heading>
-    <fudis-grid-item [columns]="columns">
-      <fudis-body-text>
-        Paragraph text for testing grid item functionalities. This is so much fun!
-      </fudis-body-text>
+    <fudis-grid-item [columns]="1">
+      <fudis-body-text> Paragraph text for testing grid item existance. </fudis-body-text>
     </fudis-grid-item>
-    <fudis-grid-item [alignSelfY]="alignSelfY" [alignSelfX]="alignSelfX">
-      <fudis-button [label]="'Test button'" />
+    <fudis-grid-item [alignSelfY]="'stretch'" [alignSelfX]="'stretch'">
+      <fudis-body-text> Paragraph text for testing grid item existance. </fudis-body-text>
     </fudis-grid-item>
   </fudis-grid>`,
 })
-class HostComponent {
-  columns: string | FudisBreakpointStyleResponsive = '1';
-
-  alignSelfX: FudisGridItemAlignment = 'stretch';
-
-  alignSelfY: FudisGridItemAlignment = 'stretch';
-}
+class HostComponent {}
 
 describe('GridItemComponent', () => {
   let component: HostComponent;
@@ -43,9 +32,7 @@ describe('GridItemComponent', () => {
         HostComponent,
         GridItemComponent,
         GridComponent,
-        MockComponent(HeadingComponent),
-        MockComponent(BodyTextComponent),
-        MockComponent(ButtonComponent),
+        BodyTextComponent,
       ],
       providers: [FudisBreakpointService],
     }).compileComponents();
@@ -69,6 +56,4 @@ describe('GridItemComponent', () => {
       expect(getGridItemComponent().length).toBe(2);
     });
   });
-
-  // NOTE: as most of components functionality is visual and it adds inline style properties testing these with Jest is not feasible. Visual Regression tests should be sufficient to cover testing these.
 });
