d15bfb1ea3880682d3e59957aaea8ae75d3011a0
marisanity <marion.karlsson@gmail.com>
2025-03-17 09:51:28 +0200

    DS-441 add a test to popover component

--- ngx-fudis/projects/ngx-fudis/src/lib/components/popover/popover.component.spec.ts
+++ ngx-fudis/projects/ngx-fudis/src/lib/components/popover/popover.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PopoverComponent } from './popover.component';
+import { By } from '@angular/platform-browser';
 
 describe('PopoverComponent', () => {
   let component: PopoverComponent;
@@ -13,10 +14,18 @@ describe('PopoverComponent', () => {
 
     fixture = TestBed.createComponent(PopoverComponent);
     component = fixture.componentInstance;
+    fixture.componentRef.setInput('id', 'popover-id');
+    fixture.componentRef.setInput('popoverText', 'Additional information');
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should initialize component successfully', () => {
+    const popoverElement = fixture.debugElement.query(By.css('#popover-id'));
+    expect(popoverElement?.nativeElement?.textContent).toEqual('Additional information');
+    expect(popoverElement?.nativeElement?.id).toEqual('popover-id');
   });
 });
