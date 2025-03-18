dd709be22ba1b59e2e6fe7c561208ae5a4b6d72c
marisanity <marion.karlsson@gmail.com>
2025-03-18 10:28:52 +0200

    DS-441 write tests for the popover directive

--- ngx-fudis/projects/ngx-fudis/setup-jest.ts
+++ ngx-fudis/projects/ngx-fudis/setup-jest.ts
@@ -6,4 +6,17 @@ global.ResizeObserver = jest.fn().mockImplementation(() => ({
   disconnect: jest.fn(),
 }));
 
+/**
+ * Workaround for stylesheet error that comes up in tests using angular material overlays. Hopefully
+ * will be fixed in the future
+ * https://github.com/thymikee/jest-preset-angular/issues/79#issuecomment-1853570270
+ */
+const originalConsoleError = console.error;
+const jsDomCssError = 'Error: Could not parse CSS stylesheet';
+console.error = (...params) => {
+  if (!params.find((p) => p.toString().includes(jsDomCssError))) {
+    originalConsoleError(...params);
+  }
+};
+
 setupZoneTestEnv();
--- ngx-fudis/projects/ngx-fudis/src/lib/directives/popover/popover.directive.spec.ts
+++ ngx-fudis/projects/ngx-fudis/src/lib/directives/popover/popover.directive.spec.ts
@@ -1,3 +1,67 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { PopoverDirective } from './popover.directive';
+import { Component, Input, ViewChild, ViewContainerRef } from '@angular/core';
+import { FudisPopoverPosition } from '../../types/miscellaneous';
+import { OverlayContainer } from '@angular/cdk/overlay';
+import { By } from '@angular/platform-browser';
+
+@Component({
+  standalone: true,
+  imports: [PopoverDirective],
+  selector: 'mock-popover-component',
+  animations: [],
+  template: `<ng-container #container>
+    <button fudisPopover [popoverText]="popoverText" [popoverPosition]="popoverPosition"
+      >Open a popover</button
+    >
+  </ng-container>`,
+})
+class MockPopoverComponent {
+  @ViewChild('container', { read: ViewContainerRef }) container!: ViewContainerRef;
+  @Input() popoverPosition: FudisPopoverPosition = 'below';
+  @Input() popoverText: string;
+}
+
 describe('PopoverDirective', () => {
-  it('should create an instance', () => {});
+  let fixture: ComponentFixture<MockPopoverComponent>;
+  let overlayContainer: OverlayContainer;
+  let overlayContainerElement: HTMLElement;
+
+  beforeAll(() => {});
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [MockPopoverComponent],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(MockPopoverComponent);
+    overlayContainer = TestBed.inject(OverlayContainer);
+    overlayContainerElement = overlayContainer.getContainerElement();
+    fixture.componentRef.setInput('popoverText', 'Popover content here');
+    fixture.detectChanges();
+  });
+
+  it('should set the required attributes for the triggering element', () => {
+    const button = fixture.debugElement.query(By.css('button'));
+    expect(button.nativeElement.getAttribute('aria-controls')).toEqual('fudis-popover-1');
+    expect(button.nativeElement.getAttribute('aria-expanded')).toEqual('false');
+  });
+
+  it('should open and close the popover', () => {
+    const button = fixture.debugElement.query(By.css('button'));
+    button.nativeElement.click();
+    fixture.detectChanges();
+
+    expect(overlayContainerElement).toBeTruthy();
+    let popoverElement = overlayContainerElement.querySelector('#fudis-popover-1');
+    expect(button.nativeElement.getAttribute('aria-expanded')).toEqual('true');
+    expect(popoverElement?.textContent).toEqual('Popover content here');
+
+    button.nativeElement.click();
+    fixture.detectChanges();
+
+    expect(button.nativeElement.getAttribute('aria-expanded')).toEqual('false');
+    popoverElement = overlayContainerElement.querySelector('#fudis-popover-1');
+    expect(popoverElement).toBeFalsy();
+  });
 });
