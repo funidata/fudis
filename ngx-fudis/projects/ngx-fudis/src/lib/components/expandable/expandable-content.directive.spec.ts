3d2b0e55dddab75b968f144cf4ceace30c44682b
marjutkinnari <marjut.kinnari@funidata.fi>
2025-03-28 11:06:18 +0200

    DS-264: Fieldset & Expandable content improvements

---
+++ ngx-fudis/projects/ngx-fudis/src/lib/components/expandable/expandable-content.directive.spec.ts
@@ -0,0 +1,22 @@
+import { TestBed } from '@angular/core/testing';
+import { ExpandableActionsDirective } from './expandable-content.directive';
+
+describe('ExpandableActionsDirective', () => {
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [  
+        ExpandableActionsDirective ]
+    });
+  });
+  it('should find instance', () => {
+    const directive = new ExpandableActionsDirective();
+
+    expect(directive).toBeTruthy();
+  });
+
+  it('should have host class', () => {
+    const directive = new ExpandableActionsDirective();
+
+    expect(directive['_classes']).toBe('fudis-expandable-actions');
+  });
+});
