e695afa7636fc0bdfa6fccac1f61754f24c090b8
marjutkinnari <marjut.kinnari@funidata.fi>
2025-04-10 15:26:18 +0300

    Check body text visibility on screen resize

--- test/visual-regression/description-list-playground.spec.ts
+++ test/visual-regression/description-list-playground.spec.ts
@@ -4,8 +4,9 @@ test("description list playground collection", async ({ page }) => {
   await page.goto(
     "/iframe.html?globals=&args=&id=components-test-playground--description-list-collection&viewMode=story",
   );
-  await expect(page.getByTestId("fudis-description-list-35"))
-    .toBeVisible()
+  await expect(page.getByTestId("fudis-description-list-35")).toBeVisible();
+  await expect(page.getByTestId("fudis-body-text-1"))
+    .toBeVisible() // Prevents flaky paragraph from moving on screen resize
     .then(async () => {
       await expect(page).toHaveScreenshot("1-description-list-collection.png", { fullPage: true });
     });
