7f4965bbf60d7301157c535f73926c8e8f422c1b
marisanity <marion.karlsson@gmail.com>
2025-06-17 18:03:00 +0300

    DS-530 remove label height-service

--- ngx-fudis/projects/ngx-fudis/src/lib/components/form/label/label.component.ts
+++ ngx-fudis/projects/ngx-fudis/src/lib/components/form/label/label.component.ts
@@ -1,19 +1,7 @@
-import {
-  ChangeDetectionStrategy,
-  Component,
-  ElementRef,
-  Input,
-  AfterViewInit,
-  OnDestroy,
-  ViewChild,
-  OnChanges,
-} from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { TooltipApiDirective } from '../../../directives/tooltip/tooltip-api.directive';
 import { FudisTranslationService } from '../../../services/translation/translation.service';
-import { FudisLabelHeightService } from '../../../services/dom/label-height.service';
-import { FudisComponentChanges, FudisLabelData } from '../../../types/miscellaneous';
 import { FudisInputSize } from '../../../types/forms';
-import { throttle } from '../../../utilities/resizeThrottle';
 
 @Component({
   selector: 'fudis-label',
@@ -22,27 +10,10 @@ import { throttle } from '../../../utilities/resizeThrottle';
   changeDetection: ChangeDetectionStrategy.OnPush,
   standalone: false,
 })
-export class LabelComponent
-  extends TooltipApiDirective
-  implements AfterViewInit, OnDestroy, OnChanges
-{
-  constructor(
-    protected _translationService: FudisTranslationService,
-    private _labelHeightService: FudisLabelHeightService,
-  ) {
+export class LabelComponent extends TooltipApiDirective {
+  constructor(protected _translationService: FudisTranslationService) {
     super();
-
-    this._resizeObserver = new ResizeObserver(
-      throttle(() => {
-        //_labelHeightService.triggerLabelHeightSet(this.id);
-      }, 25),
-    );
   }
-
-  /**
-   * Template reference Label HTML element
-   */
-  @ViewChild('labelElementRef') private _labelElementRef: ElementRef<HTMLLabelElement>;
 
   /**
    * Id for label, e. g. used in Dropdown to link ngMaterial mat-select with 'aria-labelledby' to
@@ -74,47 +45,4 @@ export class LabelComponent
   /**
    * To observe size changes of this Label and trigger height calculation as needed
    */
-  private _resizeObserver: ResizeObserver;
-
-  ngOnChanges(changes: FudisComponentChanges<LabelComponent>): void {
-    const requiredChange =
-      changes.required &&
-      changes.required.currentValue !== changes.required.previousValue &&
-      !changes.required.firstChange;
-
-    const textChange =
-      changes.text &&
-      changes.text.currentValue !== changes.text.previousValue &&
-      !changes.text.firstChange;
-
-    const sizeChange =
-      changes.parentSize &&
-      changes.parentSize.currentValue !== changes.parentSize.previousValue &&
-      !changes.parentSize.firstChange;
-
-    if (requiredChange || textChange || sizeChange) {
-      const data: FudisLabelData = {
-        id: this.id,
-        element: this._labelElementRef.nativeElement,
-      };
-
-      //this._labelHeightService.registerNewLabel(data);
-    }
-  }
-
-  ngAfterViewInit(): void {
-    const data: FudisLabelData = {
-      id: this.id,
-      element: this._labelElementRef.nativeElement,
-    };
-
-    //this._labelHeightService.registerNewLabel(data);
-
-    //this._resizeObserver.observe(this._labelElementRef.nativeElement);
-  }
-
-  ngOnDestroy(): void {
-    this._resizeObserver.disconnect();
-    //this._labelHeightService.deleteLabelData(this.id);
-  }
 }
