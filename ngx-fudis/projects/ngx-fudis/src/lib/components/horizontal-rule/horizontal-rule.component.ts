9b7bbecfdd2aaef62232bb572e1060c7f8d2eace
RiinaKuu <riina.kuukso@funidata.fi>
2025-06-19 17:59:12 +0300

    DS-470: Replace HorizontalRule component styles

--- ngx-fudis/projects/ngx-fudis/src/lib/components/horizontal-rule/horizontal-rule.component.ts
+++ ngx-fudis/projects/ngx-fudis/src/lib/components/horizontal-rule/horizontal-rule.component.ts
@@ -3,7 +3,6 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 @Component({
   selector: 'fudis-hr',
   templateUrl: './horizontal-rule.component.html',
-  styleUrls: ['./horizontal-rule.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
   standalone: false,
 })
