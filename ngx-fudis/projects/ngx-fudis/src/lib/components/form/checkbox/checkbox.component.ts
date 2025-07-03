4f549ad86d96e73a5870be42db7b794cca5ac461
marjutkinnari <marjut.kinnari@funidata.fi>
2025-07-03 13:45:28 +0300

    DS-489: Created component

---
+++ ngx-fudis/projects/ngx-fudis/src/lib/components/form/checkbox/checkbox.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'fudis-checkbox',
+  imports: [],
+  templateUrl: './checkbox.component.html',
+  styleUrl: './checkbox.component.scss'
+})
+export class CheckboxComponent {
+
+}
