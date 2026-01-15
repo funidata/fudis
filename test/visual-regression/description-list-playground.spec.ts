82c55d7831922ff6e794fb94a28d09eca5460159
RiinaKuu <riina.kuukso@funidata.fi>
2026-01-15 16:30:11 +0200

    DS-415: Skip description list vr test only in firefox (since it is not cooperative)

--- test/visual-regression/description-list-playground.spec.ts
+++ test/visual-regression/description-list-playground.spec.ts
@@ -1,9 +1,19 @@
 import test, { expect } from "@playwright/test";
 
-test("description list playground collection", async ({ page }) => {
+test("description list playground collection", async ({ page }, testInfo) => {
   await page.goto(
     "/iframe.html?globals=&args=&id=components-test-playground--description-list-collection&viewMode=story",
   );
+
+  /**
+   * TODO: This test is skipped in Firefox since it has some dl+grid issues, see ticket DS-516
+   */
+  const firefoxBrowser = "firefox";
+  test.skip(
+    firefoxBrowser.includes(testInfo.project.name),
+    "Still working on this to work on FireFox",
+  );
+
   await expect(page.getByTestId("fudis-description-list-35"))
     .toBeVisible()
     .then(async () => {
