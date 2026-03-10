969e2cda208677abdb0ea9039055da416886d34b
marjutkinnari <marjut.kinnari@funidata.fi>
2026-03-10 10:27:16 +0200

    Tab navigation

--- ngx-fudis/projects/dev/src/app/app.component.ts
+++ ngx-fudis/projects/dev/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,4 +7,15 @@ import { Component } from '@angular/core';
   standalone: false,
 })
 export class AppComponent {
+
+  constructor(protected router: Router) {}
+
+  routes: { id: string; label: string; path: string }[] = [
+    { id: 'exercise', label: 'Exercise', path: '/exercise' },
+    { id: 'sandbox', label: 'Sandbox', path: '/sandbox' },
+  ];
+
+  change(path: string) {
+    this.router.navigate([path]);
+  }
 }
