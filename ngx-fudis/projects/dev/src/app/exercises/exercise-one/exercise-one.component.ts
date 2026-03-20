492339b778700feb0bb2e81b4ac6856978f3be7e
marjutkinnari <marjut.kinnari@funidata.fi>
2026-03-20 10:10:49 +0200

    DS-520: Sandbox and Exercises

---
+++ ngx-fudis/projects/dev/src/app/exercises/exercise-one/exercise-one.component.ts
@@ -0,0 +1,38 @@
+import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { FudisSelectOption } from 'projects/ngx-fudis/src/lib/types/types';
+
+@Component({
+  selector: 'app-exercise-one',
+  templateUrl: 'exercise-one.component.html',
+  standalone: false,
+})
+export class ExerciseOneComponent {
+  constructor() {
+    this.selectOptions = [
+      {
+        value: 'option1',
+        label: 'Study plan 23.08.2018',
+        subLabel: 'Fashion, Curriculum period: 2017-2018, modified 2.3.2026',
+      },
+      {
+        value: 'option2',
+        label: 'Study plan 13.01.2026',
+        subLabel: 'Fashion, Curriculum period: 2025-2026, modified 13.1.2026',
+      },
+      {
+        value: 'option3',
+        label: 'Study plan 12.11.2023',
+        subLabel: 'Fashion, Curriculum period: 2023-2024, modified 12.11.2023',
+      },
+    ];
+  }
+
+  myControl: FormControl = new FormControl('');
+  testControl: FormControl = new FormControl('');
+
+  selectOptions: FudisSelectOption<string>[] = [];
+
+  selectControl: FormControl<FudisSelectOption<object> | null> =
+    new FormControl<FudisSelectOption<object> | null>(null);
+}
